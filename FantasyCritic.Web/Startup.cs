720be15199756f3667a33c6defec3c630d081101
STEVE-DESKTOP\Stephen <sfallon92@gmail.com>
2018-06-23 01:48:20 -0400

    Dependency Injection setup

--- FantasyCritic.Web/Startup.cs
+++ FantasyCritic.Web/Startup.cs
@@ -8,9 +8,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using FantasyCritic.Web.Data;
+using FantasyCritic.MySQL;
 using FantasyCritic.Web.Models;
 using FantasyCritic.Web.Services;
+using FantasyCritic.Lib.Interfaces;
+using FantasyCritic.Lib.Domain;
 
 namespace FantasyCritic.Web
 {
@@ -26,12 +28,13 @@ namespace FantasyCritic.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddScoped<IFantasyCriticUserStore>(factory => new MySQLFantasyIdentityStore(connectionString));
+            services.AddScoped<IFantasyCriticRepo>(factory => new MySQLFantasyCriticRepo(connectionString));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+            services.AddIdentity<FantasyCriticUser, FantasyCriticRole>()
                 .AddDefaultTokenProviders();
+
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
