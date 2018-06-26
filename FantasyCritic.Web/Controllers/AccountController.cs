39808c3ef92c104ab217e8835ef5611ce5669c71
STEVE-DESKTOP\Stephen <sfallon92@gmail.com>
2018-06-26 01:23:30 -0400

    Signing in with user name.

--- FantasyCritic.Web/Controllers/AccountController.cs
+++ FantasyCritic.Web/Controllers/AccountController.cs
@@ -61,9 +61,8 @@ namespace FantasyCritic.Web.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                FantasyCriticUser user = await _userManager.FindByEmailAsync(model.Email);
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
