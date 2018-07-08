a76f8002eee2de2126c55114696c1ee83ae8f8b2
STEVE-DESKTOP\Stephen <sfallon92@gmail.com>
2018-07-08 19:11:54 -0400

    Routing to the additional pages.

--- FantasyCritic.Web/ClientApp/router.js
+++ FantasyCritic.Web/ClientApp/router.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import HomePage from "components/pages/homePage";
+import Login from "components/pages/login";
+import AboutPage from "components/pages/aboutPage";
+import ContactPage from "components/pages/contactPage";
 
 Vue.use(VueRouter);
 
@@ -10,7 +13,22 @@ let routes = [
         path: "/",
         component: HomePage,
         name: "homePage"
-    }
+    },
+    {
+        path: "/login",
+        component: Login,
+        name: "login"
+    },
+    {
+        path: "/about",
+        component: AboutPage,
+        name: "aboutPage"
+    },
+    {
+        path: "/contact",
+        component: ContactPage,
+        name: "contactPage"
+    },
 ];
 
 let theRouter = new VueRouter({ routes });
