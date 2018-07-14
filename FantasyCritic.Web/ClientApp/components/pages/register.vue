3a3ad2f61e018b36594945c7ca5c7683b7790fd2
Stephen Fallon <sfallon92@gmail.com>
2018-07-14 17:05:21 -0400

    Added real name to registration page.

--- FantasyCritic.Web/ClientApp/components/pages/register.vue
+++ FantasyCritic.Web/ClientApp/components/pages/register.vue
@@ -6,19 +6,23 @@
             <div class="alert alert-danger" v-if="errorInfo">An error has occurred.</div>
             <div class="form-group col-md-10">
                 <label for="userName" class="control-label">User name</label>
-                <input v-model="userName" id="userName" name="userName" type="text" class="form-control input"/>
+                <input v-model="userName" id="userName" name="userName" type="text" class="form-control input" />
+            </div>
+            <div class="form-group col-md-10">
+                <label for="realName" class="control-label">Real name (optional)</label>
+                <input v-model="realName" id="realName" name="realName" type="text" class="form-control input" />
             </div>
             <div class="form-group col-md-10">
                 <label for="emailAddress" class="control-label">Email</label>
-                <input v-model="emailAddress" id="emailAddress" name="emailAddress" type="text" class="form-control input"/>
+                <input v-model="emailAddress" id="emailAddress" name="emailAddress" type="text" class="form-control input" />
             </div>
             <div class="form-group col-md-10">
                 <label for="password" class="control-label">Password</label>
-                <input v-model="password" id="password" name="password" type="password" class="form-control input"/>
+                <input v-model="password" id="password" name="password" type="password" class="form-control input" />
             </div>
             <div class="form-group col-md-10">
                 <label for="cPassword" class="control-label">Confirm Password</label>
-                <input v-model="confirmPassword" id="cPassword" name="cPassword" type="password" class="form-control input"/>
+                <input v-model="confirmPassword" id="cPassword" name="cPassword" type="password" class="form-control input" />
             </div>
             <div class="form-group">
                 <div class="col-md-offset-2 col-md-10">
@@ -37,6 +41,7 @@
         data() {
             return {
                 userName: "",
+                realName: "",
                 emailAddress: "",
                 password: "",
                 confirmPassword: "",
@@ -48,6 +53,7 @@
             register() {
                 var model = {
                     userName: this.userName,
+                    realName: this.realName,
                     emailAddress: this.emailAddress,
                     password: this.password,
                     confirmPassword: this.confirmPassword
