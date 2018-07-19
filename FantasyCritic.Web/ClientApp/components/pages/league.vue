823154624e6eed1ff3ba408ccb51ce0378ba933a
Stephen Fallon <sfallon92@gmail.com>
2018-07-19 01:08:13 -0400

    Sending an invite.

--- FantasyCritic.Web/ClientApp/components/pages/league.vue
+++ FantasyCritic.Web/ClientApp/components/pages/league.vue
@@ -7,6 +7,22 @@
                 {{ player.userName }}
             </li>
         </ul>
+        <div class="col-md-8" v-if="league.isManager">
+            <div v-if="!showInvitePlayer">
+                <b-button variant="info" class="nav-link" v-on:click="showInvite">Invite a Player</b-button>
+            </div>
+            <div v-if="showInvitePlayer">
+                <form method="post" class="form-horizontal" role="form" v-on:submit.prevent="invitePlayer">
+                    <div class="form-group col-md-4">
+                        <label for="inviteEmail" class="control-label">Email Address</label>
+                        <input v-model="inviteEmail" id="inviteEmail" name="inviteEmail" type="text" class="form-control input" />
+                    </div>
+                    <div class="form-group col-md-2">
+                        <input type="submit" class="btn btn-primary" value="Send Invite" />
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,18 +34,39 @@
             return {
                 errorInfo: "",
                 leagueID: "",
-                league: {}
+                league: {},
+                showInvitePlayer: false,
+                inviteEmail: ""
             }
         },
         methods: {
             fetchLeague() {
-                let leagueID = this.$route.params.id;
+                this.leagueID = this.$route.params.id;
                 axios
-                    .get('/api/League/GetLeague/' + leagueID)
+                    .get('/api/League/GetLeague/' + this.leagueID)
                     .then(response => {
                         this.league = response.data;
                     })
                     .catch(returnedError => (this.error = returnedError));
+            },
+            showInvite() {
+                this.showInvitePlayer = true;
+            },
+            invitePlayer() {
+                var model = {
+                    leagueID: this.leagueID,
+                    inviteEmail: this.inviteEmail
+                };
+                axios
+                    .post('/api/league/InvitePlayer', model)
+                    .then(this.responseHandler)
+                    .catch(this.catchHandler);
+            },
+            responseHandler(response) {
+                this.$router.push({ name: "login" });
+            },
+            catchHandler(returnedError) {
+
             }
         },
         mounted() {
