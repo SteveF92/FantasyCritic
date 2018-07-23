7a20f24551fc16c98bdb7652a9f7dbc74d2a2f5a
Stephen Fallon <sfallon92@gmail.com>
2018-07-22 20:51:56 -0400

    Getting score and release date from opencritic.

--- FantasyCritic.Lib/OpenCritic/OpenCriticGame.cs
+++ FantasyCritic.Lib/OpenCritic/OpenCriticGame.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NodaTime;
 
 namespace FantasyCritic.Lib.OpenCritic
 {
     public class OpenCriticGame
     {
-        public OpenCriticGame(OpenCriticScoreResponse scoreResponse)
+
+        public OpenCriticGame(OpenCriticScoreResponse scoreResponse, LocalDate? releaseDate)
         {
             ID = scoreResponse.ID;
             Name = scoreResponse.Name;
             Score = scoreResponse.Score;
             ReviewCount = scoreResponse.ReviewCount;
+            ReleaseDate = releaseDate;
         }
 
         public int ID { get; }
         public string Name { get; }
         public decimal? Score { get; }
         public int? ReviewCount { get; }
+        public LocalDate? ReleaseDate { get; }
     }
 }
--- FantasyCritic.Lib/OpenCritic/OpenCriticService.cs
+++ FantasyCritic.Lib/OpenCritic/OpenCriticService.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
 using NLog;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NodaTime;
+using NodaTime.Text;
 
 namespace FantasyCritic.Lib.OpenCritic
 {
@@ -20,14 +24,36 @@ namespace FantasyCritic.Lib.OpenCritic
 
         public async Task<Maybe<OpenCriticGame>> GetOpenCriticGame(int openCriticGameID)
         {
-            var response = await _client.GetStringAsync($"/api/game/score?id={openCriticGameID}");
-            var parsedResult = JsonConvert.DeserializeObject<OpenCriticScoreResponse>(response);
+            var scoreResponse = await _client.GetStringAsync($"/api/game/score?id={openCriticGameID}");
+            var parsedResult = JsonConvert.DeserializeObject<OpenCriticScoreResponse>(scoreResponse);
             if (parsedResult == null)
             {
                 return Maybe<OpenCriticGame>.None;
             }
 
-            var openCriticGame = new OpenCriticGame(parsedResult);
+            var gameResponse = await _client.GetStringAsync($"/api/game?id={openCriticGameID}");
+            JObject parsedGameResponse = JObject.Parse(gameResponse);
+
+            List<LocalDate> releaseDates = new List<LocalDate>();
+            var platforms = parsedGameResponse.GetValue("Platforms");
+            foreach (var platform in platforms.Children())
+            {
+                var gamePlatforms = platform.SelectToken("GamesPlatforms");
+                var releaseDateToken = gamePlatforms.SelectToken("releaseDate");
+                if (releaseDateToken == null)
+                {
+                    continue;
+                }
+
+                DateTime releaseDateResult = releaseDateToken.Value<DateTime>();
+                LocalDate releaseDate = LocalDate.FromDateTime(releaseDateResult);
+
+                releaseDates.Add(releaseDate);
+            }
+
+            LocalDate? earliestReleaseDate = releaseDates.Min();
+
+            var openCriticGame = new OpenCriticGame(parsedResult, earliestReleaseDate);
             return openCriticGame;
         }
     }
