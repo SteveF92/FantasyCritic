7cf7a7aeb69268e70dd2903711a564fac98a4862
Stephen Fallon <sfallon92@gmail.com>
2018-07-26 00:41:11 -0400

    Made a component for the rows of the player game table.

---
+++ FantasyCritic.Web/ClientApp/components/modules/playerGameRow.vue
@@ -0,0 +1,16 @@
+<template>
+    <tr>
+        <td>{{game.gameName}}</td>
+        <td v-if="game.releaseDate">{{game.releaseDate}}</td>
+        <td v-else>{{game.estimatedReleaseDate}} (Estimated)</td>
+        <td>{{game.criticScore}}</td>
+        <td>{{game.fantasyScore}}</td>
+    </tr>
+</template>
+<script>
+    import Vue from "vue";
+
+    export default {
+        props: ['game']
+    }
+</script>
