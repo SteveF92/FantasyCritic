92474b26b5e4af8d32c759981995a776c00c2a1a
Stephen Fallon <sfallon92@gmail.com>
2018-07-30 00:31:22 -0400

    Started the form for adding a game as a manager.

--- FantasyCritic.Web/ClientApp/components/pages/league.vue
+++ FantasyCritic.Web/ClientApp/components/pages/league.vue
@@ -3,14 +3,17 @@
         <div v-if="errorInfo" class="alert alert-danger" role="alert">
             {{errorInfo}}
         </div>
+
         <div v-if="invitedEmail" class="alert alert-success" role="alert">
             Sucessfully sent invite to {{ invitedEmail }}!
         </div>
+
         <div class="row">
             <h2 class="col-11">{{ league.leagueName }}</h2>
             <b-form-select v-model="activeYear" :options="league.years" class="col-1" />
         </div>
-        <div class="col-md-8" v-if="league.outstandingInvite">
+
+        <div v-if="league.outstandingInvite">
             You have been invited to join this league. Do you wish to join?
             <div class="row">
                 <div class="btn-toolbar">
@@ -18,12 +21,13 @@
                     <b-button variant="secondary" v-on:click="declineInvite" class="mx-2">Decline</b-button>
                 </div>
             </div>
-
         </div>
+
         <div>
             <h3>League Manager</h3>
             {{ league.leagueManager.userName }}
         </div>
+
         <div v-if="leagueYear">
             <h3>Publishers</h3>
             <ul>
@@ -32,7 +36,6 @@
                 </li>
             </ul>
         </div>
-
 
         <div v-if="leagueYear">
             <h3>Summary</h3>
@@ -48,22 +51,59 @@
             </ul>
         </div>
 
-        <div class="col-md-8" v-if="league.isManager">
+        <div v-if="league.isManager" class="col-4">
             <h4>Manager Actions</h4>
-            <div v-if="!showInvitePlayer">
-                <b-button variant="info" class="nav-link" v-on:click="showInvite">Invite a Player</b-button>
+
+            <div>
+                <div v-if="!showInvitePlayer">
+                    <b-button variant="info" class="nav-link" v-on:click="showInvitePlayerForm">Invite a Player</b-button>
+                </div>
+                <div v-if="showInvitePlayer">
+                    <form method="post" class="form-horizontal" role="form" v-on:submit.prevent="invitePlayer">
+                        <div class="form-group">
+                            <label for="inviteEmail" class="control-label">Email Address</label>
+                            <input v-model="inviteEmail" id="inviteEmail" name="inviteEmail" type="text" class="form-control input" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Send Invite" />
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div v-if="showInvitePlayer">
-                <form method="post" class="form-horizontal" role="form" v-on:submit.prevent="invitePlayer">
-                    <div class="form-group col-md-4">
-                        <label for="inviteEmail" class="control-label">Email Address</label>
-                        <input v-model="inviteEmail" id="inviteEmail" name="inviteEmail" type="text" class="form-control input" />
-                    </div>
-                    <div class="form-group col-md-2">
-                        <input type="submit" class="btn btn-primary" value="Send Invite" />
-                    </div>
-                </form>
-                <div v-if="inviteSent">
+            <br />
+            <div>
+                <div v-if="!showAddGame">
+                    <b-button variant="info" class="nav-link" v-on:click="showAddGameForm">Add Publisher Game</b-button>
+                </div>
+                <div v-if="showAddGame">
+                    <form method="post" class="form-horizontal" role="form" v-on:submit.prevent="addGame">
+                        <div class="form-group">
+                            <label for="claimGameName" class="control-label">Game Name</label>
+                            <div class="input-group">
+                                <input v-model="claimGameName" id="claimGameName" name="claimGameName" type="text" class="form-control input" />
+                                <span class="input-group-btn">
+                                    <b-button variant="info" class="nav-link" v-on:click="searchGame">Search Game</b-button>
+                                </span>
+                            </div>
+                        </div>
+                        <br />
+                        <div class="form-group">
+                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                <label class="btn btn-secondary active">
+                                    <input type="radio" name="options" id="draft" autocomplete="off" checked> Draft
+                                </label>
+                                <label class="btn btn-secondary">
+                                    <input type="radio" name="options" id="antiPick" autocomplete="off"> Anti Pick
+                                </label>
+                                <label class="btn btn-secondary">
+                                    <input type="radio" name="options" id="waiver" autocomplete="off"> Waiver
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group col-2">
+                            <input type="submit" class="btn btn-primary" value="Add game to publisher" />
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -82,6 +122,7 @@
                 league: null,
                 leagueYear: null,
                 showInvitePlayer: false,
+                showAddGame: false,
                 inviteEmail: "",
                 invitedEmail: "",
                 activeYear: null
@@ -108,8 +149,11 @@
                     })
                     .catch(returnedError => (this.error = returnedError));
             },
-            showInvite() {
+            showInvitePlayerForm() {
                 this.showInvitePlayer = true;
+            },
+            showAddGameForm() {
+                this.showAddGame = true;
             },
             acceptInvite() {
                 var model = {
