95ad91beff38036dd0692b6d149e7d7e62fe0825
Stephen Fallon <sfallon92@gmail.com>
2018-10-07 03:07:31 -0400

    Created entities that match up with the two new tables related to the acquistion system.

---
+++ FantasyCritic.MySQL/Entities/AcquisitionBidEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyCritic.MySQL.Entities
+{
+    public class AcquisitionBidEntity
+    {
+        public Guid BidID { get; set; }
+        public Guid PublisherID { get; set; }
+        public Guid MasterGameID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Priority { get; set; }
+        public int BidAmount { get; set; }
+        public bool? Sucessful { get; set; }
+    }
+}
---
+++ FantasyCritic.MySQL/Entities/MasterGameRequestEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyCritic.MySQL.Entities
+{
+    public class MasterGameRequestEntity
+    {
+        public Guid RequestID { get; set; }
+        public Guid UserID { get; set; }
+        public string GameName { get; set; }
+        public DateTime RequestTimestamp { get; set; }
+        public string RequestNote { get; set; }
+        public bool Answered { get; set; }
+        public DateTime? ResponseTimestamp { get; set; }
+        public string ResponseNote { get; set; }
+        public Guid? MasterGameID { get; set; }
+    }
+}
