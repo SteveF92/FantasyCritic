168a93202860f2681f6e2f27c59266a596e3e4df
Stephen Fallon <sfallon92@gmail.com>
2018-10-25 23:33:23 -0400

    Added change password UI.

--- FantasyCritic.Web/ClientApp/components/pages/manageUser.vue
+++ FantasyCritic.Web/ClientApp/components/pages/manageUser.vue
@@ -18,35 +18,48 @@
 
       <dt class="col-sm-3">Password</dt>
       <dd class="col-sm-9">
-        <b-button variant="info">Change Password</b-button>
+        <b-button variant="info" v-b-modal="'changePasswordForm'">Change Password</b-button>
       </dd>
     </dl>
+    <changePasswordForm v-on:passwordChanged="passwordChanged"></changePasswordForm>
+
   </div>
 </template>
 <script>
-    import axios from 'axios';
-
-    export default {
-        computed: {
-            userInfo() {
-                return this.$store.getters.userInfo;
-            }
-        },
-        methods: {
-            sendConfirmationEmail() {
-                axios
-                    .post('/api/account/ResendConfirmationEmail')
-                    .then(response => {
-                        let toast = this.$toasted.show('Confirmation Email Sent!', {
-                            theme: "primary",
-                            position: "top-right",
-                            duration: 5000
-                        });
-                    })
-                    .catch(response => {
-
-                    });
-            }
+  import axios from 'axios';
+  import ChangePasswordForm from "components/modules/modals/changePasswordForm";
+  
+  export default {
+    components: {
+      ChangePasswordForm
+    },
+    computed: {
+        userInfo() {
+            return this.$store.getters.userInfo;
         }
+    },
+    methods: {
+      sendConfirmationEmail() {
+          axios
+              .post('/api/account/ResendConfirmationEmail')
+              .then(response => {
+                  let toast = this.$toasted.show('Confirmation Email Sent!', {
+                      theme: "primary",
+                      position: "top-right",
+                      duration: 5000
+                  });
+              })
+              .catch(response => {
+
+              });
+      },
+      passwordChanged() {
+        let toast = this.$toasted.show("Your password has been changed.", {
+          theme: "primary",
+          position: "top-right",
+          duration: 5000
+        });
+      },
     }
+  }
 </script>
