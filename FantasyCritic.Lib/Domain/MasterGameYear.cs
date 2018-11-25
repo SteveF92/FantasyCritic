c38d4451787bea317a4e10aa9f120bda0a4b27e0
Stephen Fallon <sfallon92@gmail.com>
2018-11-25 18:43:03 -0500

    Added mastergameyear class.
    This will describe a master game in the context of one year. The data it requires can be retrieved from the view.

---
+++ FantasyCritic.Lib/Domain/MasterGameYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyCritic.Lib.Domain
+{
+    public class MasterGameYear
+    {
+        public MasterGameYear(MasterGame masterGame, int year, decimal percentStandardGame, decimal percentCounterPick, decimal averageDraftPosition)
+        {
+            MasterGame = masterGame;
+            Year = year;
+            PercentStandardGame = percentStandardGame;
+            PercentCounterPick = percentCounterPick;
+            AverageDraftPosition = averageDraftPosition;
+        }
+
+        public MasterGame MasterGame { get; }
+        public int Year { get; }
+        public decimal PercentStandardGame { get; }
+        public decimal PercentCounterPick { get; }
+        public decimal AverageDraftPosition { get; }
+    }
+}
