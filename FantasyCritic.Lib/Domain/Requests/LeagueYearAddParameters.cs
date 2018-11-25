ab5b07b19800a9a275e4c2e079d121fca63a5638
Stephen Fallon <sfallon92@gmail.com>
2018-11-25 18:24:51 -0500

    Removed estimated critic score from a piece of code that we will need later, but itsn't being used now.

--- FantasyCritic.Lib/Domain/Requests/LeagueYearAddParameters.cs
+++ FantasyCritic.Lib/Domain/Requests/LeagueYearAddParameters.cs
@@ -5,14 +5,13 @@ namespace FantasyCritic.Lib.Domain.Requests
 {
     public class LeagueYearAddParameters
     {
-        public LeagueYearAddParameters(League league, int draftGames, int pickupGames, int counterPicks, decimal estimatedCriticScore, int year,
+        public LeagueYearAddParameters(League league, int draftGames, int pickupGames, int counterPicks, int year,
             int eligibilityLevel, DraftSystem draftSystem, PickupSystem pickupSystem, ScoringSystem scoringSystem)
         {
             League = league;
             DraftGames = draftGames;
             PickupGames = pickupGames;
             CounterPicks = counterPicks;
-            EstimatedCriticScore = estimatedCriticScore;
             Year = year;
             EligibilityLevel = eligibilityLevel;
             DraftSystem = draftSystem;
@@ -24,7 +23,6 @@ namespace FantasyCritic.Lib.Domain.Requests
         public int DraftGames { get; }
         public int PickupGames { get; }
         public int CounterPicks { get; }
-        public decimal EstimatedCriticScore { get; }
         public int Year { get; }
         public int EligibilityLevel { get; }
         public DraftSystem DraftSystem { get; }
