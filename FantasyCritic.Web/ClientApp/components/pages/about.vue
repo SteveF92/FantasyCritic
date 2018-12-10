e87524486d555094dee75ac0da75a238d2ee7a7c
Stephen Fallon <sfallon92@gmail.com>
2018-12-10 00:57:17 -0500

    Updated about page.

--- FantasyCritic.Web/ClientApp/components/pages/about.vue
+++ FantasyCritic.Web/ClientApp/components/pages/about.vue
@@ -1,23 +1,44 @@
 <template>
-    <div>
-        <h2>About</h2>
-        <hr />
-        <div class="alert alert-danger" v-if="error">An error has occurred.</div>
-        <p>{{description}}</p>
+  <div>
+    <h2>About</h2>
+    <hr />
+    <div class="col-lg-6 col-sm-12">
+      <p>
+        Fantasy Critic was created by me, Steve Fallon. The visual design for the site was created by Dale Mahabir.
+        As of now, this site is brand new and there are bound to be bugs, oversights, and design issues. However, I am committed to improving the site for the foreseeable future.
+        I created this site because I wanted to merge my love of video games with my love of fantasy football. I think anyone that likes both of those things will have a great time playing Fantasy Critic.
+      </p>
+      <p>
+        This site could not exist without <a href="https://opencritic.com/">opencritic.com</a>. Review aggregation is time consuming work and I'm very grateful they are allowing me to use their API.
+        Consider supporting them on <a href="https://www.patreon.com/OpenCritic/">Patreon</a>.
+      </p>
+
+      <p>
+        This site is open source. If you want to help work on features or fix bugs, contact me on <a href="https://github.com/SteveF92/FantasyCritic">GitHub</a>.
+      </p>
+      <h3>Technical Credits</h3>
+      <p>
+        This site uses the following software packages heavily, and I wouldn’t be able to create this site without them.
+      </p>
+
+      <ul>
+        <li>ASP.NET Core</li>
+        <li>Vue.JS</li>
+        <li>MySQL</li>
+        <li>Bootstrap</li>
+        <li>Vue-Bootstrap</li>
+        <li>Webpack</li>
+        <li>SendGrid</li>
+        <li>SignalR</li>
+        <li>NodaTime</li>
+        <li>Dapper</li>
+        <li>Newtonsoft.JSON</li>
+        <li>MoreLINQ</li>
+        <li>CSharpFunctionalExtensions</li>
+        <li>NLog</li>
+        <li>NUnit</li>
+      </ul>
     </div>
+    
+  </div>
 </template>
-
-<script>
-    import Vue from 'vue';
-    import axios from 'axios';
-
-    export default {
-        data() {
-            return {
-                description: "Fantasy critic is a thing...",
-                error: ""
-            }
-
-        }
-    }
-</script>
