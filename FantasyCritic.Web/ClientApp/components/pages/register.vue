fbbd32e72fa03587843dbd52eb13a3b3c29788fb
Stephen Fallon <sfallon92@gmail.com>
2018-12-10 00:06:33 -0500

    Moved registration into a module.
    This will let us put it in multiple places.

--- FantasyCritic.Web/ClientApp/components/pages/register.vue
+++ FantasyCritic.Web/ClientApp/components/pages/register.vue
@@ -2,74 +2,18 @@
     <div>
         <h2>Register</h2>
         <hr />
-        <form method="post" class="form-horizontal" role="form" v-on:submit.prevent="register">
-          <div class="alert alert-danger" v-if="errorInfo">An error has occurred.</div>
-          <div class="form-group col-md-10">
-            <label for="userName" class="control-label">Username</label>
-            <input v-model="userName" v-validate="'required'" id="userName" name="userName" type="text" class="form-control input" />
-            <span>{{ errors.first('userName') }}</span>
-          </div>
-          <div class="form-group col-md-10">
-            <label for="emailAddress" class="control-label">Email</label>
-            <input v-model="emailAddress" v-validate="'required|email'" id="emailAddress" name="emailAddress" type="text" class="form-control input" />
-            <span>{{ errors.first('emailAddress') }}</span>
-          </div>
-          <div class="form-group col-md-10">
-            <label for="password" class="control-label">Password</label>
-            <input v-model="password" v-validate="'required|min:8'" name="password" type="password" class="form-control input" ref="password">
-            <span>{{ errors.first('password') }}</span>
-          </div>
-
-          <div class="form-group col-md-10">
-            <label for="confirmPassword" class="control-label">Confirm Password</label>
-            <input v-model="confirmPassword" v-validate="'required|confirmed:password'" name="confirmPassword" type="password" class="form-control input" data-vv-as="password">
-            <span>{{ errors.first('confirmPassword') }}</span>
-          </div>
-
-          <div class="form-group">
-            <div class="col-md-offset-2 col-md-10">
-              <input type="submit" class="btn btn-primary" value="Register" :disabled="!formIsValid" />
-            </div>
-          </div>
-        </form>
+        <registerForm></registerForm>
     </div>
 </template>
 
 <script>
     import Vue from 'vue';
     import axios from 'axios';
+    import RegisterForm from "components/modules/registerForm";
 
     export default {
-        data() {
-            return {
-                userName: "",
-                emailAddress: "",
-                password: "",
-                confirmPassword: "",
-                errorInfo: ""
-            }
-        },
-        computed: {
-          formIsValid() {
-            return !Object.keys(this.fields).some(key => this.fields[key].invalid);
-          }
-        },
-        methods: {
-            register() {
-                var model = {
-                    userName: this.userName,
-                    emailAddress: this.emailAddress,
-                    password: this.password,
-                    confirmPassword: this.confirmPassword
-                };
-                this.$store.dispatch("registerAccount", model)
-                  .then(() => {
-                    this.$router.push({ name: "login", props: { accountCreated: true } });
-                  })
-                  .catch(returnedError => {
-                    this.errorInfo = returnedError;
-                  });
-            }
-        }
+      components: {
+        RegisterForm
+      }
     }
 </script>
