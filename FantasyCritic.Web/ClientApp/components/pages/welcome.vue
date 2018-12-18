32692b062f2c6e4977a925d86b4d98006994e2a1
Stephen Fallon <sfallon92@gmail.com>
2018-12-18 01:05:41 -0500

    Added some new wording to welcome page.

--- FantasyCritic.Web/ClientApp/components/pages/welcome.vue
+++ FantasyCritic.Web/ClientApp/components/pages/welcome.vue
@@ -2,12 +2,9 @@
   <div>
     <div class="row">
       <div class="col-lg-4 offset-lg-1 col-md-12">
-        <h2>Welcome to Fantasy Critic</h2>
+        <h2>Fantasy Football...for video games!</h2>
         <p>
-          FantasyCritic is like fantasy football...but for video games! You and several friends will create your own virtual video game “Publishers”,
-          and then you will assemble a roster of the games you believe will review the best. Similar to fantasy football, the players will alternate
-          picking games in a snake draft style to start the year. At the end of the year, the winner is the team with the best lineup of video games
-          based on scores from <a href="https://opencritic.com/">opencritic.com</a>.
+          Fantasy Critic is a game where you get to play as your own "fantasy" video game publisher. It's your job to choose the best reviewing games to publish!
         </p>
       </div>
       <div class="col-lg-4 offset-lg-2 col-md-12">
@@ -15,6 +12,15 @@
 
         <registerForm></registerForm>
       </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6 welcome-heading">Play free forever!</div>
+        <p class="col-sm-12 col-md-6 welcome-message">
+          FantasyCritic is like fantasy football...but for video games! You and several friends will create your own virtual video game “Publishers”,
+          and then you will assemble a roster of the games you believe will review the best. Similar to fantasy football, the players will alternate
+          picking games in a snake draft style to start the year. At the end of the year, the winner is the team with the best lineup of video games
+          based on scores from <a href="https://opencritic.com/">opencritic.com</a>.
+        </p>
     </div>
   </div>
 </template>
@@ -32,5 +38,9 @@
 <style scoped>
   .registration-area{
     float:right;
+  }
+  .welcome-heading {
+  }
+  .welcome-message {
   }
 </style>
