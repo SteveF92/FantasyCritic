a276bb1d82cebde789eb51af4f644c20bb7c35d0
Stephen Fallon <sfallon92@gmail.com>
2018-12-20 01:04:03 -0500

    Removing bootswatch.

--- FantasyCritic.Web/ClientApp/app.js
+++ FantasyCritic.Web/ClientApp/app.js
@@ -13,7 +13,6 @@ import "./filters";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import 'bootswatch/dist/superhero/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 Vue.use(BootstrapVue);
