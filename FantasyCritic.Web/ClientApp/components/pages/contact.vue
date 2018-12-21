76585bb4ddafe704660fa16ef29a9db3517d60ce
Stephen Fallon <sfallon92@gmail.com>
2018-12-21 01:38:48 -0500

    Changed the contact page.

--- FantasyCritic.Web/ClientApp/components/pages/contact.vue
+++ FantasyCritic.Web/ClientApp/components/pages/contact.vue
@@ -3,9 +3,9 @@
         <h2>Contact</h2>
         <hr />
 
-        <address>
-            <strong>Support:</strong>   <a href="mailto:elitefusion92@gmail.com">elitefusion92@gmail.com</a><br />
-        </address>
+        <p>
+          The best way to contact me is on Twitter <a href="https://twitter.com/fantasy_critic">@fantasy_critic</a> our on our <a href="https://www.reddit.com/r/fantasycritic/">subreddit</a>.
+        </p>
     </div>
 </template>
 
