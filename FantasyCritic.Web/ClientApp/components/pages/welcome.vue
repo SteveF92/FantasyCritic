d992c1c3c92ff3c9dd0b9b51d2cd760a4293326f
Stephen Fallon <sfallon92@gmail.com>
2018-12-20 21:18:48 -0500

    Centering hero message vertically.

--- FantasyCritic.Web/ClientApp/components/pages/welcome.vue
+++ FantasyCritic.Web/ClientApp/components/pages/welcome.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <div class="row hero-area">
-      <div class="col-lg-6 col-md-12 hero-message">
-        <h1 class="hero-message-header">
-          Fantasy Football...
-          <br />
-          for video games!
-        </h1>
-        <p class="shadowed hero-message-details">
-          Fantasy Critic is a game where you get to play as your own "fantasy" video game publisher. It's your job to choose the best reviewing games to publish!
-        </p>
+      <div class="col-lg-6 col-md-12 hero-message-area">
+        <div class="hero-message">
+          <h1 class="hero-message-header">
+            Fantasy Football...
+            <br />
+            for video games!
+          </h1>
+          <p class="shadowed hero-message-details">
+            Fantasy Critic is a game where you get to play as your own "fantasy" video game publisher. It's your job to choose the best reviewing games to publish!
+          </p>
+        </div>
       </div>
       <div class="col-lg-6 col-md-12">
         <h2>Sign up for free</h2>
@@ -105,11 +107,13 @@
   .hero-area {
     background-color: #414141;
   }
-  .hero-message {
+  .hero-message-area {
     text-align: center
   }
+  .hero-message {
+    padding-top: 70px;
+  }
   .hero-message-header {
-    
     display: inline-block;
   }
   .hero-message-details {
