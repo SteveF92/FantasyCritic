5ea1badc700b1613176de8c68ec5d987ece9f966
Stephen Fallon <sfallon92@gmail.com>
2018-12-22 03:25:08 -0500

    Making the full size player table match the minimal one.

--- FantasyCritic.Web/ClientApp/components/modules/gameTables/playerGameTable.vue
+++ FantasyCritic.Web/ClientApp/components/modules/gameTables/playerGameTable.vue
@@ -22,7 +22,7 @@
                 </td>
                 <td></td>
                 <td id="average-critic-column">{{publisher.averageCriticScore | score(2)}} (Average)</td>
-                <td id="total-column" class="table-success">{{publisher.totalFantasyPoints | score(2)}}</td>
+                <td id="total-column" class="success">{{publisher.totalFantasyPoints | score(2)}}</td>
               </tr>
             </tbody>
         </table>
@@ -63,10 +63,6 @@
   .player-table {
     margin-left: 3px;
     margin-right: 7px;
-    border-radius: 2px;
-    border-color: #4E5D6C;
-    border-width: 5px;
-    border-style: solid;
     margin-bottom: 10px;
   }
   .player-table table {
