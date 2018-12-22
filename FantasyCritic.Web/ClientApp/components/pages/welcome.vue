9a6fb703cffa225fc81b8c8343dcdb86a08d3b7c
Stephen Fallon <sfallon92@gmail.com>
2018-12-22 01:24:05 -0500

    Made margins around computer image slightly better.

--- FantasyCritic.Web/ClientApp/components/pages/welcome.vue
+++ FantasyCritic.Web/ClientApp/components/pages/welcome.vue
@@ -211,6 +211,8 @@
     flex-direction: column; /* make main axis vertical */
     justify-content: center; /* center items vertically, in this case */
     align-items: center; /* center items horizontally, in this case */
+    margin-top: 20px;
+    margin-bottom: 10px;
   }
 
   .computer-image {
