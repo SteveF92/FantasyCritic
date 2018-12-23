dbdfa9c70e30b27c692a64354426c60210d05037
Stephen Fallon <sfallon92@gmail.com>
2018-12-22 22:40:52 -0500

    Font awesome is half working.

--- FantasyCritic.Web/ClientApp/icons.js
+++ FantasyCritic.Web/ClientApp/icons.js
@@ -1,44 +1,44 @@
-import fontawesome from '@fortawesome/fontawesome'
-// Official documentation available at: https://github.com/FortAwesome/vue-fontawesome
-import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
+//import fontawesome from '@fortawesome/fontawesome'
+//// Official documentation available at: https://github.com/FortAwesome/vue-fontawesome
+//import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
 
-// If not present, it won't be visible within the application. Considering that you
-// don't want all the icons for no reason. This is a good way to avoid importing too many
-// unnecessary things.
-fontawesome.library.add(
-  require('@fortawesome/fontawesome-free-solid/faEnvelope'),
-  require('@fortawesome/fontawesome-free-solid/faGraduationCap'),
-  require('@fortawesome/fontawesome-free-solid/faHome'),
-  require('@fortawesome/fontawesome-free-solid/faList'),
-  require('@fortawesome/fontawesome-free-solid/faSpinner'),
-  require('@fortawesome/fontawesome-free-solid/faBell'),
-  require('@fortawesome/fontawesome-free-solid/faExternalLinkAlt'),
-  require('@fortawesome/fontawesome-free-solid/faBars'),
-  require('@fortawesome/fontawesome-free-solid/faTrophy'),
-  require('@fortawesome/fontawesome-free-solid/faUserFriends'),
-  require('@fortawesome/fontawesome-free-solid/faListOl'),
-  require('@fortawesome/fontawesome-free-solid/faDollarSign'),
-  require('@fortawesome/fontawesome-free-solid/faCircle'),
-  require('@fortawesome/fontawesome-free-solid/faCheck'),
-  require('@fortawesome/fontawesome-free-solid/faInfoCircle'),
-  require('@fortawesome/fontawesome-free-solid/faQuestionCircle'),
-  require('@fortawesome/fontawesome-free-solid/faUserPlus'),
-  require('@fortawesome/fontawesome-free-solid/faSignInAlt'),
-  require('@fortawesome/fontawesome-free-solid/faPlusCircle'),
-  require('@fortawesome/fontawesome-free-solid/faMinusCircle'),
-  //Regular
-  require('@fortawesome/fontawesome-free-regular/faCircle'),
-  // Brands
-  require('@fortawesome/fontawesome-free-brands/faFontAwesome'),
-  require('@fortawesome/fontawesome-free-brands/faMicrosoft'),
-  require('@fortawesome/fontawesome-free-brands/faVuejs'),
-  require('@fortawesome/fontawesome-free-brands/faTwitterSquare'),
-  require('@fortawesome/fontawesome-free-brands/faRedditSquare')
-)
+//// If not present, it won't be visible within the application. Considering that you
+//// don't want all the icons for no reason. This is a good way to avoid importing too many
+//// unnecessary things.
+//fontawesome.library.add(
+//  require('@fortawesome/free-solid-svg-icons/faEnvelope'),
+//  require('@fortawesome/free-solid-svg-icons/faGraduationCap'),
+//  require('@fortawesome/free-solid-svg-icons/faHome'),
+//  require('@fortawesome/free-solid-svg-icons/faList'),
+//  require('@fortawesome/free-solid-svg-icons/faSpinner'),
+//  require('@fortawesome/free-solid-svg-icons/faBell'),
+//  require('@fortawesome/free-solid-svg-icons/faExternalLinkAlt'),
+//  require('@fortawesome/free-solid-svg-icons/faBars'),
+//  require('@fortawesome/free-solid-svg-icons/faTrophy'),
+//  require('@fortawesome/free-solid-svg-icons/faUserFriends'),
+//  require('@fortawesome/free-solid-svg-icons/faListOl'),
+//  require('@fortawesome/free-solid-svg-icons/faDollarSign'),
+//  require('@fortawesome/free-solid-svg-icons/faCircle'),
+//  require('@fortawesome/free-solid-svg-icons/faCheck'),
+//  require('@fortawesome/free-solid-svg-icons/faInfoCircle'),
+//  require('@fortawesome/free-solid-svg-icons/faQuestionCircle'),
+//  require('@fortawesome/free-solid-svg-icons/faUserPlus'),
+//  require('@fortawesome/free-solid-svg-icons/faSignInAlt'),
+//  require('@fortawesome/free-solid-svg-icons/faPlusCircle'),
+//  require('@fortawesome/free-solid-svg-icons/faMinusCircle'),
+//  //Regular
+//  require('@fortawesome/free-regular-svg-icons/faCircle'),
+//  // Brands
+//  require('@fortawesome/free-brands-svg-icons/faFontAwesome'),
+//  require('@fortawesome/free-brands-svg-icons/faMicrosoft'),
+//  require('@fortawesome/free-brands-svg-icons/faVuejs'),
+//  require('@fortawesome/free-brands-svg-icons/faTwitterSquare'),
+//  require('@fortawesome/free-brands-svg-icons/faRedditSquare')
+//)
 
-export {
-  FontAwesomeIcon,
-  FontAwesomeLayers,
-  FontAwesomeLayersText
-}
+//export {
+//  FontAwesomeIcon,
+//  FontAwesomeLayers,
+//  FontAwesomeLayersText
+//}
