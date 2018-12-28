076278f4aa3700ffe38b072cd4da5e5c3a7011d9
Stephen Fallon <sfallon92@gmail.com>
2018-12-27 19:03:22 -0500

    Made footer a component.

--- FantasyCritic.Web/ClientApp/components/app-root.vue
+++ FantasyCritic.Web/ClientApp/components/app-root.vue
@@ -10,67 +10,19 @@
         </main>
       </div>
     </div>
-    <footer class="footer">
-      <div class="container">
-        <div class="row justify-content-between">
-          <div col-md-4>
-            <img class="full-logo" src="/images/logo-white.png" />
-            <img class="small-logo" src="/images/small-logo-white.png" />
-          </div>
-          <div col-md-4>
-            <label>Navigation</label>
-            <div>
-              <router-link :to="{ name: 'about' }" title="About">
-                <font-awesome-icon icon="info-circle" size="lg" />
-                About
-              </router-link>
-            </div>
-            <div>
-              <router-link :to="{ name: 'faq' }" title="FAQ">
-                <font-awesome-icon icon="question-circle" size="lg" />
-                FAQ
-              </router-link>
-            </div>
-          </div>
-          <div col-md-4>
-            <label>Contact</label>
-            <div>
-              <a href="https://twitter.com/fantasy_critic" target="_blank">
-                <font-awesome-icon :icon="['fab', 'twitter-square']" size="lg" :style="{ color: '00acee' }" />
-                <span class="full-contact">@fantasy_critic</span>
-              </a>
-            </div>
-            <div>
-              <a href="https://www.reddit.com/r/fantasycritic/" target="_blank">
-                <font-awesome-icon :icon="['fab', 'reddit-square']" size="lg" :style="{ color: 'ff4500' }" />
-                <span class="full-contact">r/fantasycritic</span>
-              </a>
-            </div>
-            <div>
-              <a href="https://www.facebook.com/fantasycritic" target="_blank">
-                <font-awesome-icon :icon="['fab', 'facebook-square']" size="lg" :style="{ color: '3b5998' }" />
-                <span class="full-contact">Facebook</span>
-              </a>
-            </div>
-            <div>
-              <a href="https://github.com/SteveF92/FantasyCritic" target="_blank">
-                <font-awesome-icon :icon="['fab', 'github-square']" size="lg" :style="{ color: 'ffffff' }" />
-                <span class="full-contact">GitHub</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <footerModule>
+    </footerModule>
   </div>
 </template>
 
 <script>
   import TopNavbar from "components/modules/topNavbar";
+  import FooterModule from "components/modules/footerModule";
 
   export default {
     components: {
-      TopNavbar
+      TopNavbar,
+      FooterModule
     },
     mounted() {
       if (this.$store.getters.tokenIsCurrent()) {
@@ -80,84 +32,73 @@
   };
 </script>
 
-<style>
-  [v-cloak] {
-    display: none;
-  }
-
-  html {
-    position: relative;
-    min-height: 100%;
-  }
-</style>
-
-<style scoped>
-  @media only screen and (max-width: 343px) {
-    .full-contact {
-      display: none;
-    }
-  }
-
-  @media only screen and (max-width: 679px) {
-    .full-logo {
-      display: none;
-    }
-
-    #vue-app {
-      margin-bottom: 75px; /* Margin bottom by footer height */
-    }
-
-    .footer {
-      height: 75px; /* Set the fixed height of the footer here */
-    }
-
-    .container {
-      padding-bottom: 25px;
-    }
-  }
-
-  @media only screen and (min-width: 680px) {
-    .small-logo {
-      display: none;
-    }
-
-    #vue-app {
-      margin-bottom: 175px; /* Margin bottom by footer height */
-    }
-
-    .footer {
-      height: 175px; /* Set the fixed height of the footer here */
-    }
-  }
-
-  .footer {
-    position: absolute;
-    background-color: black;
-    bottom: 0;
-    width: 100%;
-    line-height: 30px; /* Vertically center the text there */
-  }
-
-  /* Custom page CSS
--------------------------------------------------- */
-  /* Not required for template or sticky footer method. */
-
-  .container {
-    width: auto;
-    max-width: 1200px;
-    padding-left: 45px;
-    padding-right: 45px;
-    background-color: black;
-  }
-
-</style>
-
 <style lang="scss">
   @import '../scss/custom-bootstrap.scss';
   @import '../../node_modules/bootstrap/scss/bootstrap.scss';
 </style>
 
 <style>
-@import '../css/site.css';
-@import '../css/modal.css';
+  @import '../css/site.css';
+  @import '../css/modal.css';
+</style>
+
+
+<style>
+    @media only screen and (max-width: 343px) {
+      .full-contact {
+        display: none;
+      }
+    }
+
+    @media only screen and (max-width: 679px) {
+      .full-logo {
+        display: none;
+      }
+
+      #vue-app {
+        margin-bottom: 75px; /* Margin bottom by footer height */
+      }
+
+      .footer {
+        height: 75px; /* Set the fixed height of the footer here */
+      }
+
+      .container {
+        padding-bottom: 25px;
+      }
+    }
+
+    @media only screen and (min-width: 680px) {
+      .small-logo {
+        display: none;
+      }
+
+      #vue-app {
+        margin-bottom: 175px; /* Margin bottom by footer height */
+      }
+
+      .footer {
+        height: 175px; /* Set the fixed height of the footer here */
+      }
+    }
+
+    .footer {
+      position: absolute;
+      background-color: black;
+      bottom: 0;
+      width: 100%;
+      line-height: 30px; /* Vertically center the text there */
+    }
+
+    /* Custom page CSS
+  -------------------------------------------------- */
+    /* Not required for template or sticky footer method. */
+
+    .container {
+      width: auto;
+      max-width: 1200px;
+      padding-left: 45px;
+      padding-right: 45px;
+      background-color: black;
+    }
 </style>
---
+++ FantasyCritic.Web/ClientApp/components/modules/footerModule.vue
@@ -0,0 +1,54 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div col-md-4>
+          <img class="full-logo" src="/images/logo-white.png" />
+          <img class="small-logo" src="/images/small-logo-white.png" />
+        </div>
+        <div col-md-4>
+          <label>Navigation</label>
+          <div>
+            <router-link :to="{ name: 'about' }" title="About">
+              <font-awesome-icon icon="info-circle" size="lg" />
+              About
+            </router-link>
+          </div>
+          <div>
+            <router-link :to="{ name: 'faq' }" title="FAQ">
+              <font-awesome-icon icon="question-circle" size="lg" />
+              FAQ
+            </router-link>
+          </div>
+        </div>
+        <div col-md-4>
+          <label>Contact</label>
+          <div>
+            <a href="https://twitter.com/fantasy_critic" target="_blank">
+              <font-awesome-icon :icon="['fab', 'twitter-square']" size="lg" :style="{ color: '00acee' }" />
+              <span class="full-contact">@fantasy_critic</span>
+            </a>
+          </div>
+          <div>
+            <a href="https://www.reddit.com/r/fantasycritic/" target="_blank">
+              <font-awesome-icon :icon="['fab', 'reddit-square']" size="lg" :style="{ color: 'ff4500' }" />
+              <span class="full-contact">r/fantasycritic</span>
+            </a>
+          </div>
+          <div>
+            <a href="https://www.facebook.com/fantasycritic" target="_blank">
+              <font-awesome-icon :icon="['fab', 'facebook-square']" size="lg" :style="{ color: '3b5998' }" />
+              <span class="full-contact">Facebook</span>
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/SteveF92/FantasyCritic" target="_blank">
+              <font-awesome-icon :icon="['fab', 'github-square']" size="lg" :style="{ color: 'ffffff' }" />
+              <span class="full-contact">GitHub</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
