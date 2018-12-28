1130427723a472904248d95e9723961a207c0e38
Stephen Fallon <sfallon92@gmail.com>
2018-12-28 18:43:34 -0500

    Fixed the forgot password form.

--- FantasyCritic.Web/ClientApp/components/pages/forgotPassword.vue
+++ FantasyCritic.Web/ClientApp/components/pages/forgotPassword.vue
@@ -1,44 +1,50 @@
 <template>
     <div>
-        <h2>Forgot Password</h2>
-        <div v-if="showSent" class="alert alert-success">Forgot password email has been sent. Please check your email.</div>
-        <form v-on:submit.prevent="sendForgotPasswordRequest">
-            <div class="form-group col-md-4">
-                <label for="emailAddress" class="control-label">Email Address</label>
-                <input v-model="emailAddress" type="text" class="form-control input" />
-            </div>
-            <div class="form-group">
-                <div class="col-md-offset-2 col-md-4">
-                    <input type="submit" class="btn btn-default" value="Submit" />
-                </div>
-            </div>
-        </form>
+      <h2>Forgot Password</h2>
+      <div v-if="showSent" class="alert alert-success">Forgot password email has been sent. Please check your email.</div>
+      <form v-on:submit.prevent="sendForgotPasswordRequest">
+        <div class="form-group col-md-4">
+          <label for="emailAddress" class="control-label">Email Address</label>
+          <input v-model="emailAddress" v-validate="'required|email'" type="text" class="form-control input" />
+          <span class="text-danger">{{ errors.first('emailAddress') }}</span>
+        </div>
+        <div class="form-group">
+          <div class="col-md-offset-2 col-md-4">
+            <input type="submit" class="btn btn-primary" value="Submit" :disabled="!formIsValid" />
+          </div>
+        </div>
+      </form>
     </div>
 </template>
 <script>
-    import axios from 'axios';
+  import axios from 'axios';
 
-    export default {
-        data() {
-            return {
-                emailAddress: "",
-                showSent: false
-            }
-        },
-        methods: {
-            sendForgotPasswordRequest() {
-                let request = {
-                    emailAddress: this.emailAddress
-                };
-                axios
-                    .post('/api/account/ForgotPassword', request)
-                    .then(response => {
-                        this.showSent = true;
-                    })
-                    .catch(response => {
-
-                    });
-            }
-        }
+  export default {
+    data() {
+      return {
+        emailAddress: "",
+        showSent: false
+      }
+  },
+  computed: {
+    formIsValid() {
+      return !Object.keys(this.fields).some(key => this.fields[key].invalid);
     }
+  },
+  methods: {
+    sendForgotPasswordRequest() {
+      let request = {
+          emailAddress: this.emailAddress
+      };
+      axios
+        .post('/api/account/ForgotPassword', request)
+        .then(response => {
+            this.showSent = true;
+        })
+        .catch(response => {
+
+        });
+    }
+  }
+}
 </script>
