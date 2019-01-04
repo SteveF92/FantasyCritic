cc9d4a2f269a0b34837052409de8370f42586aab
Stephen Fallon <sfallon92@gmail.com>
2019-01-03 21:35:10 -0500

    Added a link to Dale's website.

--- FantasyCritic.Web/ClientApp/components/pages/about.vue
+++ FantasyCritic.Web/ClientApp/components/pages/about.vue
@@ -4,7 +4,7 @@
     <hr />
     <div class="col-lg-6 col-sm-12">
       <p>
-        Fantasy Critic was created by me, Steve Fallon. The visual design for the site was created by Dale Mahabir.
+        Fantasy Critic was created by me, Steve Fallon. The visual design for the site was created by <a href="http://dalemahabir.rocks/" target="_blank">Dale Mahabir <font-awesome-icon icon="external-link-alt" size="sm" /></a>.
         As of now, this site is brand new and there are bound to be bugs, oversights, and design issues. However, I am committed to improving the site for the foreseeable future.
         I created this site because I wanted to merge my love of video games with my love of fantasy football. I think anyone that likes both of those things will have a great time playing Fantasy Critic.
       </p>
