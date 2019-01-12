f0425ea756ac79a2c80be1edfd6b9314b3c2d0c6
Stephen Fallon <sfallon92@gmail.com>
2019-01-11 19:38:45 -0500

    Minor text fixes.

--- FantasyCritic.Web/ClientApp/components/pages/contact.vue
+++ FantasyCritic.Web/ClientApp/components/pages/contact.vue
@@ -5,7 +5,7 @@
 
         <p class="text-well">
           The best way to contact me is on Twitter <a href="https://twitter.com/fantasy_critic">@fantasy_critic <font-awesome-icon icon="external-link-alt" size="sm" /></a>
-          our on our <a href="https://www.reddit.com/r/fantasycritic/">subreddit <font-awesome-icon icon="external-link-alt" size="sm" /></a>.
+          or on our <a href="https://www.reddit.com/r/fantasycritic/">subreddit <font-awesome-icon icon="external-link-alt" size="sm" /></a>.
         </p>
     </div>
 </template>
