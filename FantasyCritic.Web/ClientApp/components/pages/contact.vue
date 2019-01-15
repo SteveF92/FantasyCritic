2a5b6cda6dacf1df402db14cdeb47ef5944268da
Stephen Fallon <sfallon92@gmail.com>
2019-01-14 19:18:45 -0500

    Fixed text using too much width on contact page.

--- FantasyCritic.Web/ClientApp/components/pages/contact.vue
+++ FantasyCritic.Web/ClientApp/components/pages/contact.vue
@@ -2,11 +2,12 @@
     <div>
         <h1>Contact</h1>
         <hr />
-
-        <p class="text-well">
-          The best way to contact me is on Twitter <a href="https://twitter.com/fantasy_critic">@fantasy_critic <font-awesome-icon icon="external-link-alt" size="sm" /></a>
-          or on our <a href="https://www.reddit.com/r/fantasycritic/">subreddit <font-awesome-icon icon="external-link-alt" size="sm" /></a>.
-        </p>
+        <div class="col-lg-8 col-md-10 col-sm-12">
+          <p class="text-well">
+            The best way to contact me is on Twitter <a href="https://twitter.com/fantasy_critic">@fantasy_critic <font-awesome-icon icon="external-link-alt" size="sm" /></a>
+            or on our <a href="https://www.reddit.com/r/fantasycritic/">subreddit <font-awesome-icon icon="external-link-alt" size="sm" /></a>.
+          </p>
+        </div>
     </div>
 </template>
 
