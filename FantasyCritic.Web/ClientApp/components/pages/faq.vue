900ab280faf0cbef5b9da5e8d96e07f491bfa07f
Stephen Fallon <sfallon92@gmail.com>
2019-01-19 01:22:51 -0500

    Changed the FAQ about messaging me.

--- FantasyCritic.Web/ClientApp/components/pages/faq.vue
+++ FantasyCritic.Web/ClientApp/components/pages/faq.vue
@@ -167,7 +167,9 @@
       <h3>Can I bid on a game that doesn't come up when I search for games?</h3>
       <p class="text-well">
         Unfortunately no. To bid on a game, it must be in the master database. I try to stay on top of upcoming releases, but if a game that you want is missing,
-        DM me on <a href="https://twitter.com/fantasy_critic" target="_blank">twitter <font-awesome-icon icon="external-link-alt" size="sm" /></a> and I'll make sure to get it added for you quickly.
+        <a href="https://twitter.com/fantasy_critic" target="_blank">tweet at me <font-awesome-icon icon="external-link-alt" size="sm" /></a> or message me on
+        <a href="https://www.reddit.com/r/fantasycritic/" target="_blank">Reddit <font-awesome-icon icon="external-link-alt" size="sm" /></a>
+        and I'll make sure to get it added for you quickly.
       </p>
       <h3>When are bids processed?</h3>
       <p class="text-well">
