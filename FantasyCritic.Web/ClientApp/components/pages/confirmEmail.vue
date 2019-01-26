228d1211ff417c4095ddac1e738b85575c30737d
Stephen Fallon <sfallon92@gmail.com>
2019-01-26 18:59:26 -0500

    Chaning the confirm email page to be more manual.

--- FantasyCritic.Web/ClientApp/components/pages/confirmEmail.vue
+++ FantasyCritic.Web/ClientApp/components/pages/confirmEmail.vue
@@ -1,19 +1,33 @@
 <template>
-    <div>
-        <div v-if="!accountConfirmed" class="alert alert-info">
-          <div>Confirming Email...</div>
-          <div>If you are having issues, check out our <a href="/faq#bidding-system" target="_blank">FAQ</a> page.</div>
-        </div>
-        <div v-if="accountConfirmed" class="alert alert-success">
-          <span>Email Confirmation Successful!</span>
-          <span>You will be redirected in a few seconds...</span>
-        </div>
+  <div>
+    <h1>Confirm Your Email</h1>
+    <div class="alert alert-danger" v-show="errorInfo">
+      {{errorInfo}}
     </div>
+    <div v-if="isBusy" class="alert alert-info">
+      <div>Confirming Email...</div>
+    </div>
+    <div v-if="attemptFailed" class="alert alert-warning">
+      <div>If you are having issues, check out our <a href="/faq#bidding-system" target="_blank" class="text-secondary">FAQ</a> page.</div>
+    </div>
+    <div v-if="accountConfirmed" class="alert alert-success">
+      <span>Email Confirmation Successful!</span>
+      <span>You will be redirected in a few seconds...</span>
+    </div>
+    <b-button variant="primary" v-on:click="confirmFromURL" :disabled="isBusy">Click here to confirm your email.</b-button>
+  </div>
 </template>
 <script>
   import axios from 'axios';
 
   export default {
+    data() {
+      return {
+        isBusy: false,
+        attemptFailed: false,
+        errorInfo: ""
+      }
+    },
     computed: {
       accountConfirmed() {
         if (this.$store.getters.userInfo) {
@@ -25,28 +39,39 @@
         return this.$store.getters.tokenIsCurrent();
       }
     },
-    mounted() {
-      let request = {
+    methods: {
+      confirmFromURL() {
+        this.attemptFailed = false;
+        this.errorInfo = "";
+        let request = {
           userID: this.$route.query.UserID,
           code: this.$route.query.Code
-      };
-      axios
-        .post('/api/account/ConfirmEmail', request)
-        .then(response => {
-          if (this.isAuth) {
-            this.$store.dispatch("getUserInfo")
-              .then(() => {
-                setTimeout(() => this.$router.push({ name: "home" }), 3000);
-              })
-              .catch(returnedError => {
-              });
-          } else {
-            setTimeout(() => this.$router.push({ name: "login" }), 3000); 
-          }    
-        })
-        .catch(response => {
-
-        });
+        };
+        this.confirmEmail(request);
+      },
+      confirmEmail(request) {
+        this.isBusy = true;
+        axios
+          .post('/api/account/ConfirmEmail', request)
+          .then(response => {
+            this.isBusy = false;
+            if (this.isAuth) {
+              this.$store.dispatch("getUserInfo")
+                .then(() => {
+                  setTimeout(() => this.$router.push({ name: "home" }), 3000);
+                })
+                .catch(returnedError => {
+                });
+            } else {
+              setTimeout(() => this.$router.push({ name: "login" }), 3000);
+            }
+          })
+          .catch(response => {
+            this.isBusy = false;
+            this.errorInfo = "Something went wrong with automatic confirmation. Try confirming manually.";
+            this.attemptFailed = true;
+          });
+      },
     }
   }
 </script>
