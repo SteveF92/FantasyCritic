0250e9012accb8caed9780d3603bd398eedbe1ed
Stephen Fallon <sfallon92@gmail.com>
2019-02-14 00:42:28 -0500

    Handling the site counts better on mobile.

--- FantasyCritic.Web/ClientApp/components/modules/siteCounts.vue
+++ FantasyCritic.Web/ClientApp/components/modules/siteCounts.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <div v-if="siteCounts" class="row counts-area">
-      <div class="col-3">
+      <div class="col-6 col-sm-3">
         <h3>{{siteCounts.userCount | thousands}}</h3>
         <h4>Users Joined</h4>
       </div>
-      <div class="col-3">
+      <div class="col-6 col-sm-3">
         <h3>{{siteCounts.leagueCount | thousands}}</h3>
         <h4>Leagues Created</h4>
       </div>
-      <div class="col-3">
+      <div class="col-6 col-sm-3">
         <h3>{{siteCounts.masterGameCount | thousands}}</h3>
         <h4>Unique Games</h4>
       </div>
-      <div class="col-3">
+      <div class="col-6 col-sm-3">
         <h3>{{siteCounts.publisherGameCount | thousands}}</h3>
-        <h4>Games drafted</h4>
+        <h4>Games Drafted</h4>
       </div>
     </div>
   </div>
