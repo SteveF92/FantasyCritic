da4d9f29fbce35d97565695becd172d27d5dd332
Stephen Fallon <sfallon92@gmail.com>
2019-03-03 20:07:41 -0500

    Added the refresh critic scores button.

--- FantasyCritic.Web/ClientApp/components/pages/adminConsole.vue
+++ FantasyCritic.Web/ClientApp/components/pages/adminConsole.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1>Admin Console</h1>
-    
+    <div class="alert alert-danger" v-show="errorInfo">{{errorInfo}}</div>
+    <div class="alert alert-info" v-show="isBusy">Request is processing...</div>
+    <div class="alert alert-success" v-show="jobSuccess">{{jobSuccess}} sucessfully run.</div>
+    <b-button variant="info" v-on:click="refreshCriticScores">Refresh Critic Scores</b-button>
   </div>
 </template>
 <script>
@@ -10,14 +13,27 @@
   export default {
     data() {
       return {
-        isBusy: false
+        isBusy: false,
+        errorInfo: "",
+        jobSuccess: ""
       }
     },
     computed: {
 
     },
     methods: {
-     
+      refreshCriticScores() {
+        this.isBusy = true;
+        axios
+          .post('/api/admin/RefreshCriticInfo')
+          .then(response => {
+            this.isBusy = false;
+            this.jobSuccess = "Refresh Critic Scores";
+          })
+          .catch(returnedError => {
+            this.errorInfo = returnedError.response.data;
+          });
       }
     }
+  }
 </script>
