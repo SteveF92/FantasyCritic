64e3f538749b2a8387555cd23632e6c72d997024
Stephen Fallon <sfallon92@gmail.com>
2019-04-07 19:53:13 -0400

    Created a task to be scheduled.
    For testing it is not going to update opencritic data, and it's going to be every other minute.

---
+++ FantasyCritic.Lib/Scheduling/RefreshDataTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FantasyCritic.Lib.Scheduling.Lib;
+using FantasyCritic.Lib.Services;
+
+namespace FantasyCritic.Lib.Scheduling
+{
+    public class RefreshDataTask : IScheduledTask
+    {
+        private readonly AdminService _adminService;
+        //public string Schedule => "0 */2 * * *";
+        public string Schedule => "*/2 * * * *";
+
+        public RefreshDataTask(AdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return _adminService.UpdateFantasyPoints();
+        }
+    }
+}
