d24332656bb1bc08cdc33411801baf4bd622d9ce
Stephen Fallon <sfallon92@gmail.com>
2019-04-12 23:37:28 -0400

    Created a bunch of fake repos to be able to use the project without a database.

---
+++ FantasyCritic.FakeRepo/FakeFantasyCriticRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasyCritic.Lib.Interfaces;
+
+namespace FantasyCritic.FakeRepo
+{
+    public class FakeFantasyCriticRepo : IFantasyCriticRepo
+    {
+        
+    }
+}
