5d9570c3f32e24957990e0431e6363253e9d7e44
Stephen Fallon <sfallon92@gmail.com>
2019-04-13 00:27:52 -0400

    Making eligibility levels equatable.

--- FantasyCritic.Lib/Domain/EligibilityLevel.cs
+++ FantasyCritic.Lib/Domain/EligibilityLevel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FantasyCritic.Lib.Domain
 {
-    public class EligibilityLevel
+    public class EligibilityLevel : IEquatable<EligibilityLevel>
     {
         public EligibilityLevel(int level, string name, string description, IEnumerable<string> examples)
         {
@@ -20,5 +20,25 @@ namespace FantasyCritic.Lib.Domain
         public string Name { get; }
         public string Description { get; }
         public IReadOnlyList<string> Examples { get; }
+
+        public bool Equals(EligibilityLevel other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Level == other.Level;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((EligibilityLevel) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Level;
+        }
     }
 }
