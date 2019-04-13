d37894341d53b467543d37509a9236cd0ec83225
Stephen Fallon <sfallon92@gmail.com>
2019-04-13 00:25:19 -0400

    Getting box art working.

--- FantasyCritic.FakeRepo/MasterGameFactory.cs
+++ FantasyCritic.FakeRepo/MasterGameFactory.cs
@@ -17,20 +17,20 @@ namespace FantasyCritic.FakeRepo
             List<MasterGame> games = new List<MasterGame>();
 
             var sekiro = CreateMasterGame("96f5e8e3-672b-4626-b47e-4bff3a6c4430", "Sekiro: Shadows Die Twice", "2019-03-22", new LocalDate(2019, 3, 22), 6630,
-                89.9200m, 2019, 0, false, false, "2019-03-20T15:30:00Z", false, "2019-01-22T15:30:00Z");
+                89.9200m, 2019, 0, false, false, "Sekiro_art.jpg", "2019-03-20T15:30:00Z", false, "2019-01-22T15:30:00Z");
             games.Add(sekiro);
 
             return games;
         }
 
         private static MasterGame CreateMasterGame(string guid, string name, string estimatedReleaseDate, LocalDate? releaseDate, int? openCriticID, decimal? criticScore,
-            int minimumReleaseYear, int eligibilityLevelID, bool yearlyInstallment, bool earlyAccess, string firstCriticScoreTimestamp, bool doNotRefresh, string addedTimestamp)
+            int minimumReleaseYear, int eligibilityLevelID, bool yearlyInstallment, bool earlyAccess, string boxartFileName, string firstCriticScoreTimestamp, bool doNotRefresh, string addedTimestamp)
         {
             EligibilityLevel eligibilityLevel = EligibilityLevelFactory.GetEligibilityLevels().Single(x => x.Level == eligibilityLevelID);
 
             var game = new MasterGame(Guid.Parse(guid), name, estimatedReleaseDate, releaseDate, openCriticID,
                 criticScore, minimumReleaseYear, eligibilityLevel, yearlyInstallment,
-                earlyAccess, guid, InstantPattern.ExtendedIso.Parse(firstCriticScoreTimestamp).GetValueOrThrow(), doNotRefresh,
+                earlyAccess, boxartFileName, InstantPattern.ExtendedIso.Parse(firstCriticScoreTimestamp).GetValueOrThrow(), doNotRefresh,
                 InstantPattern.ExtendedIso.Parse(addedTimestamp).GetValueOrThrow());
 
             return game;
