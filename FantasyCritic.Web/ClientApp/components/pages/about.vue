513c608aa08931cd777cfa376eaf686bc77e00ad
Stephen Fallon <sfallon92@gmail.com>
2019-05-30 00:19:26 -0400

    Fixed a typo

--- FantasyCritic.Web/ClientApp/components/pages/about.vue
+++ FantasyCritic.Web/ClientApp/components/pages/about.vue
@@ -73,7 +73,7 @@
 
         <h3>Other Credits</h3>
         <ul>
-          <li><a href="https://wildermuth.com/">Shawn Wildermuth</a>, who's ASP.NET Core + Vue.JS gave me a great introduction to Vue.JS.</li>
+          <li><a href="https://wildermuth.com/">Shawn Wildermuth</a>, whose ASP.NET Core + Vue.JS gave me a great introduction to Vue.JS.</li>
           <li><a href="https://github.com/TrilonIO/aspnetcore-Vue-starter">Mark Piesak</a>, for his excellent ASP.NET Core + Vue.JS template, which forms the foundation of this website.</li>
         </ul>
         
