8fe7068f785a3a693ed98925b7ba20a04fe03691
Stephen Fallon <sfallon92@gmail.com>
2019-06-01 19:41:44 -0400

    Simplfied test code.

--- FantasyCritic.Test/MasterGameSearchingTests.cs
+++ FantasyCritic.Test/MasterGameSearchingTests.cs
@@ -17,14 +17,18 @@ namespace FantasyCritic.Test
     [TestFixture]
     public class MasterGameSearchingTests
     {
+        private static async Task<IReadOnlyList<MasterGame>> SearchGames(string searchString)
+        {
+            var fakeRepo = new FakeMasterGameRepo(new FakeFantasyCriticUserStore(FakeClock.FromUtc(2019, 1, 1)));
+            var masterGames = await fakeRepo.GetMasterGames();
+            var searchResults = MasterGameSearching.SearchMasterGames(searchString, masterGames);
+            return searchResults;
+        }
+
         [Test]
         public async Task BasicSearchTest()
         {
-            var fakeRepo = new FakeMasterGameRepo(new FakeFantasyCriticUserStore(FakeClock.FromUtc(2019, 1, 1)));
-
-            var masterGames = await fakeRepo.GetMasterGames();
-            string searchString = "Sekiro: Shadows Die Twice";
-            var searchResults = MasterGameSearching.SearchMasterGames(searchString, masterGames);
+            var searchResults = await SearchGames("Sekiro: Shadows Die Twice");
 
             Assert.AreEqual(1, searchResults.Count);
             Assert.AreEqual("96f5e8e3-672b-4626-b47e-4bff3a6c4430", searchResults[0].MasterGameID.ToString());
