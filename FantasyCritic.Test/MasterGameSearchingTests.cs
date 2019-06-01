1c6c14e633ab9386c0a5b8840a3062f4e3057696
Stephen Fallon <sfallon92@gmail.com>
2019-06-01 19:39:47 -0400

    Introducing string searching unit tests.

---
+++ FantasyCritic.Test/MasterGameSearchingTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FantasyCritic.FakeRepo;
+using FantasyCritic.Lib.Domain;
+using FantasyCritic.Lib.Domain.ScoringSystems;
+using FantasyCritic.Lib.Utilities;
+using NodaTime;
+using NodaTime.Testing;
+using NodaTime.Text;
+using NUnit.Framework;
+
+namespace FantasyCritic.Test
+{
+    [TestFixture]
+    public class MasterGameSearchingTests
+    {
+        [Test]
+        public async Task BasicSearchTest()
+        {
+            var fakeRepo = new FakeMasterGameRepo(new FakeFantasyCriticUserStore(FakeClock.FromUtc(2019, 1, 1)));
+
+            var masterGames = await fakeRepo.GetMasterGames();
+            string searchString = "Sekiro: Shadows Die Twice";
+            var searchResults = MasterGameSearching.SearchMasterGames(searchString, masterGames);
+
+            Assert.AreEqual(1, searchResults.Count);
+            Assert.AreEqual("96f5e8e3-672b-4626-b47e-4bff3a6c4430", searchResults[0].MasterGameID.ToString());
+        }
+    }
+}
