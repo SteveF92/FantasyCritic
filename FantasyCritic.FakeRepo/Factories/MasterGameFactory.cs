77afe4ccdff8491a3509f078801de40ed5818832
Stephen Fallon <sfallon92@gmail.com>
2019-06-01 20:17:10 -0400

    Unit tests focused on different ways of writing the same thing.

--- FantasyCritic.FakeRepo/Factories/MasterGameFactory.cs
+++ FantasyCritic.FakeRepo/Factories/MasterGameFactory.cs
@@ -15,7 +15,13 @@ namespace FantasyCritic.FakeRepo.Factories
 
             var sekiro = CreateMasterGame("96f5e8e3-672b-4626-b47e-4bff3a6c4430", "Sekiro: Shadows Die Twice", "2019-03-22", new LocalDate(2019, 3, 22), 6630,
                 89.9200m, 2019, 0, false, false, false, false, false, "Sekiro_art.jpg", "2019-03-20T15:30:00Z", false, "2019-01-22T15:30:00Z");
+            var metroidPrime = CreateMasterGame("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", "Metroid Prime 4", "TBA", null, null,
+                null, 2020, 0, false, false, false, false, false, null, "2019-03-20T15:30:00Z", false, "2019-01-22T15:30:00Z");
+            var civSix = CreateMasterGame("3729b47d-35b6-48c3-8acd-a57364617b8e", "Sid Meier's Civilization VI: Rise and Fall", "2018-02-08", new LocalDate(2018, 2, 8), 5405,
+                82.6250m, 2019, 0, false, false, false, false, true, null, "2019-03-20T15:30:00Z", false, "2019-01-22T15:30:00Z");
             games.Add(sekiro);
+            games.Add(metroidPrime);
+            games.Add(civSix);
 
             return games;
         }
--- FantasyCritic.Test/MasterGameSearchingTests.cs
+++ FantasyCritic.Test/MasterGameSearchingTests.cs
@@ -26,39 +26,111 @@ namespace FantasyCritic.Test
         }
 
         [Test]
-        public async Task ExactTest()
+        public async Task ExactSekiroTest()
         {
             var searchResults = await SearchGames("Sekiro: Shadows Die Twice");
 
-            Assert.GreaterOrEqual(1, searchResults.Count);
+            Assert.GreaterOrEqual(searchResults.Count, 1);
             Assert.AreEqual("96f5e8e3-672b-4626-b47e-4bff3a6c4430", searchResults[0].MasterGameID.ToString());
         }
 
         [Test]
-        public async Task PartOfStringTest()
+        public async Task PartOfStringSekiroTest()
         {
             var searchResults = await SearchGames("Sekiro");
 
-            Assert.GreaterOrEqual(1, searchResults.Count);
+            Assert.GreaterOrEqual(searchResults.Count, 1);
             Assert.AreEqual("96f5e8e3-672b-4626-b47e-4bff3a6c4430", searchResults[0].MasterGameID.ToString());
         }
 
         [Test]
-        public async Task MispelledStringTest()
+        public async Task MispelledStringSekiroTest()
         {
             var searchResults = await SearchGames("Sakiro: Shadows Die Twice");
 
-            Assert.GreaterOrEqual(1, searchResults.Count);
+            Assert.GreaterOrEqual(searchResults.Count, 1);
             Assert.AreEqual("96f5e8e3-672b-4626-b47e-4bff3a6c4430", searchResults[0].MasterGameID.ToString());
         }
 
         [Test]
-        public async Task MispelledPartStringTest()
+        public async Task MispelledPartStringSekiroTest()
         {
             var searchResults = await SearchGames("Sakiro");
 
-            Assert.GreaterOrEqual(1, searchResults.Count);
+            Assert.GreaterOrEqual(searchResults.Count, 1);
             Assert.AreEqual("96f5e8e3-672b-4626-b47e-4bff3a6c4430", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task ExactMetroidTest()
+        {
+            var searchResults = await SearchGames("Metroid Prime 4");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task PartOfStringMetroidTest()
+        {
+            var searchResults = await SearchGames("Metroid");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task MispelledStringMetroidTest()
+        {
+            var searchResults = await SearchGames("Matroid Prime 4");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task MispelledPartStringMetroidTest()
+        {
+            var searchResults = await SearchGames("Matroid");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task SkippedWordMetroidTest()
+        {
+            var searchResults = await SearchGames("Metroid 4");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task SkippedWordMispelledMetroidTest()
+        {
+            var searchResults = await SearchGames("Matroid 4");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task ArabicNumeralsCivilizationTest()
+        {
+            var searchResults = await SearchGames("Civ 6");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("3729b47d-35b6-48c3-8acd-a57364617b8e", searchResults[0].MasterGameID.ToString());
+        }
+
+        [Test]
+        public async Task WordNumbersCivilizationTest()
+        {
+            var searchResults = await SearchGames("Civ Six");
+
+            Assert.GreaterOrEqual(searchResults.Count, 1);
+            Assert.AreEqual("3729b47d-35b6-48c3-8acd-a57364617b8e", searchResults[0].MasterGameID.ToString());
         }
     }
 }
