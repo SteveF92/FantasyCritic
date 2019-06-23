407101867afe9362b369c3e72a1033d3fb56c7c2
Stephen Fallon <sfallon92@gmail.com>
2019-06-23 18:17:50 -0400

    Created an FAQ collapse module to simplfying FAQs

---
+++ FantasyCritic.Web/ClientApp/components/modules/faqCollapse.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-card no-body class="mb-1">
+    <b-card-header header-tag="header" class="p-1" role="tab">
+      <b-btn block href="#" v-b-toggle="'accordion-' + collapseID" variant="primary">
+        <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
+        <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
+        <slot name="question"></slot>
+      </b-btn>
+    </b-card-header>
+    <b-collapse :id="'accordion-' + collapseID" :visible="defaultVisible" :accordion="accordionID" role="tabpanel">
+      <b-card-body>
+        <p class="card-text">
+          <slot name="answer"></slot>
+        </p>
+      </b-card-body>
+    </b-collapse>
+  </b-card>
+</template>
+
+<script>
+  export default {
+    props: ['accordionID','collapseID', 'defaultVisible']
+  }
+</script>
+
+<style scoped>
+  .faq-icon {
+    float: left;
+  }
+
+  .collapsed > .when-opened,
+  :not(.collapsed) > .when-closed {
+    display: none;
+  }
+</style>
--- FantasyCritic.Web/ClientApp/components/modules/homeFAQ.vue
+++ FantasyCritic.Web/ClientApp/components/modules/homeFAQ.vue
@@ -1,80 +1,49 @@
 <template>
-  <div role="tablist">
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-btn block href="#" v-b-toggle.accordion1 variant="primary">
-          <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
-          <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
-          Why should I play FantasyCritic?
-        </b-btn>
-      </b-card-header>
-      <b-collapse id="accordion1" visible accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <p class="card-text">
-            It’s a great catalyst for discussing video games with your friends, and gets everyone invested in checking out the year’s best video games.
-          </p>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-btn block href="#" v-b-toggle.accordion2 variant="primary">
-          <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
-          <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
-          Does this cost money?
-        </b-btn>
-      </b-card-header>
-      <b-collapse id="accordion2" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <p class="card-text">
-            No. It’s totally free. No catch. For now, this is a passion project for me and I’m comfortable eating the cost of servers and advertising.
-            In the future, it is possible that I will consider adding advertising or opening a Patreon, but I’m never going to charge people to play.
-          </p>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-btn block href="#" v-b-toggle.accordion3 variant="primary">
-          <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
-          <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
-          How many players can be in a league?
-        </b-btn>
-      </b-card-header>
-      <b-collapse id="accordion3" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <p class="card-text">
-            Anywhere from 2 to 14. Somewhere between 4 and 8 is probably optimal, but you’ll do fine anywhere in that range.
-          </p>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-btn block href="#" v-b-toggle.accordion4 variant="primary">
-          <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
-          <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
-          How long does a league last?
-        </b-btn>
-      </b-card-header>
-      <b-collapse id="accordion4" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <p class="card-text">
-            Each calendar year is one “game” of Fantasy Critic. Everyone starts fresh with a new roster at the start of each new year,
-            but you are free to keep playing in the same league with the same people year after year.
-          </p>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
+  <div>
+    <faqCollapse :accordianID="faqAccordian" :collapseID="1" defaultVisible>
+      <div slot="question">
+        Why should I play FantasyCritic?
+      </div>
+      <div slot="answer">
+        It’s a great catalyst for discussing video games with your friends, and gets everyone invested in checking out the year’s best video games.
+      </div>
+    </faqCollapse>
+    <faqCollapse :accordianID="faqAccordian" :collapseID="2">
+      <div slot="question">
+        Does this cost money?
+      </div>
+      <div slot="answer">
+        No. It’s totally free. No catch. For now, this is a passion project for me and I’m comfortable eating the cost of servers and advertising.
+        In the future, it is possible that I will consider adding advertising or opening a Patreon, but I’m never going to charge people to play.
+      </div>
+    </faqCollapse>
+    <faqCollapse :accordianID="faqAccordian" :collapseID="3">
+      <div slot="question">
+        How many players can be in a league?
+      </div>
+      <div slot="answer">
+        Anywhere from 2 to 14. Somewhere between 4 and 8 is probably optimal, but you’ll do fine anywhere in that range.
+      </div>
+    </faqCollapse>
+    <faqCollapse :accordianID="faqAccordian" :collapseID="4">
+      <div slot="question">
+        How long does a league last?
+      </div>
+      <div slot="answer">
+        Each calendar year is one “game” of Fantasy Critic. Everyone starts fresh with a new roster at the start of each new year,
+        but you are free to keep playing in the same league with the same people year after year.
+      </div>
+    </faqCollapse>
   </div>
 </template>
-<style scoped>
-  .faq-icon{
-    float: left;
-  }
 
-  .collapsed > .when-opened,
-  :not(.collapsed) > .when-closed {
-    display: none;
+
+<script>
+  import FaqCollapse from "components/modules/faqCollapse";
+
+  export default {
+    components: {
+      FaqCollapse
+    }
   }
-</style>
+</script>
