39bbc3821d4130d3166ab50f331b7dcb5d52cb99
Stephen Fallon <sfallon92@gmail.com>
2019-06-23 15:04:06 -0400

    Getting the table looking pretty nice.

--- FantasyCritic.Web/ClientApp/components/modules/leagueTable.vue
+++ FantasyCritic.Web/ClientApp/components/modules/leagueTable.vue
@@ -3,9 +3,11 @@
            :fields="leagueFields"
            thead-class="hidden_header"
            bordered
-           striped>
+           striped
+           >
     <template slot="leagueName" slot-scope="data">
-      <router-link :to="{ name: 'league', params: { leagueid: data.item.leagueID, year: data.item.activeYear }}">{{data.item.leagueName}}</router-link>
+      <router-link :to="{ name: 'league', params: { leagueid: data.item.leagueID, year: data.item.activeYear }}" class="league-link">{{data.item.leagueName}}</router-link>
+      <div class="manager">Manager: {{data.item.leagueManager.displayName}}</div>
     </template>
   </b-table>
 </template>
@@ -24,5 +26,17 @@
 <style scoped>
 table >>> .hidden_header {
   display: none;
+}
+
+.league-link {
+  font-weight: bold;
+  margin-bottom: 0;
+  padding-bottom: 0;
+}
+
+.manager {
+  margin-top: 0;
+  padding-top: 0;
+  font-size: 14px;
 }
 </style>
