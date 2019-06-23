534d60786b8e79a65cc7e186a34a0b1c83dc15c7
Stephen Fallon <sfallon92@gmail.com>
2019-06-22 20:47:41 -0400

    Gutting the home page to rebuild it with the new layout.
    The header is working.

--- FantasyCritic.Web/ClientApp/components/pages/home.vue
+++ FantasyCritic.Web/ClientApp/components/pages/home.vue
@@ -1,83 +1,22 @@
 <template>
   <div>
-    <h1>Welcome to Fantasy Critic!</h1>
     <div class="alert alert-warning" v-show="userInfo && !userInfo.emailConfirmed">
       <div>Your email address has not been confirmed. You cannot accept league invites until you do so.</div>
       <div>Check your email account for an email from us.</div>
       <span>If you are having issues, check out our <a href="/faq#technical" class="text-secondary" target="_blank">FAQ</a> page.</span>
     </div>
 
-    <div class="row">
-      <div class="col-lg-6 col-md-12">
-        <b-button variant="info" :to="{ name: 'howtoplay' }" v-if="shouldShowLearnToPlay" class="learn-to-play-button">Learn to Play</b-button>
-        <b-button variant="primary" :to="{ name: 'createLeague' }" class="create-league-button">Create a League</b-button>
-        <div v-if="anyInvitedLeagues">
-          <h2>League Invites</h2>
-          <ul>
-            <li v-for="league in invitedLeagues">
-              <router-link v-show="userInfo.emailConfirmed" :to="{ name: 'league', params: { leagueid: league.leagueID, year: league.activeYear }}">{{league.leagueName}}</router-link>
-              <div v-show="!userInfo.emailConfirmed">{{league.leagueName}}</div>
-            </li>
-          </ul>
-          <hr />
-        </div>
-
-        <div v-if="anyManagedLeagues">
-          <h2>Leagues I Manage</h2>
-          <ul>
-            <li v-for="league in myLeagues" v-if="league.isManager && !league.testLeague">
-              <router-link :to="{ name: 'league', params: { leagueid: league.leagueID, year: league.activeYear }}">{{league.leagueName}}</router-link>
-            </li>
-          </ul>
-          <hr />
-        </div>
-
-        <div v-if="anyPlayerLeagues">
-          <h2>Leagues I Play In</h2>
-          <ul>
-            <li v-for="league in myLeagues" v-if="!league.isManager && !league.testLeague">
-              <router-link :to="{ name: 'league', params: { leagueid: league.leagueID, year: league.activeYear }}">{{league.leagueName}}</router-link>
-            </li>
-          </ul>
-          <hr />
-        </div>
-        <div>
-          <h2>Leagues I'm Following</h2>
-          <h5><router-link :to="{ name: 'publicLeagues' }">Full List of Public Leagues</router-link></h5>
-          <div v-if="anyFollowedLeagues">
-            <ul>
-              <li v-for="league in myFollowedLeagues">
-                <router-link :to="{ name: 'league', params: { leagueid: league.leagueID, year: league.activeYear }}">{{league.leagueName}}</router-link>
-              </li>
-            </ul>
-          </div>
-          <div v-else>
-            <label>You are not following any public leagues!</label>
-          </div>
-          <hr />
-        </div>
-
-        <div v-if="anyTestLeagues">
-          <h2>Test Leagues</h2>
-          <ul>
-            <li v-for="league in myLeagues" v-if="league.testLeague">
-              <router-link :to="{ name: 'league', params: { leagueid: league.leagueID, year: league.activeYear }}">{{league.leagueName}}</router-link>
-            </li>
-          </ul>
-          <hr />
-        </div>
-
-        <div v-if="!fetchingLeagues && noLeagues">
-          <h3>You are not part of any leagues! Why not start one?</h3>
-          <hr />
-        </div>
-        <h5 v-if="isAdmin"><router-link :to="{ name: 'adminConsole' }">Admin Console</router-link></h5>
-
+    <div class="col-md-10 offset-md-1 col-sm-12 text-well welcome-area">
+      <div class="row welcome-header">
+        <h1>Welcome {{userInfo.displayName}}!</h1>
       </div>
-      <div class="col-lg-6 col-md-12">
-        <tweets></tweets>
+      <div class="row main-buttons">
+        <b-button variant="primary" :to="{ name: 'createLeague' }" class="main-button">Create a League</b-button>
+        <b-button variant="info" :to="{ name: 'howtoplay' }" class="main-button">Learn to Play</b-button>
       </div>
     </div>
+
+
   </div>
 </template>
 
@@ -170,11 +109,21 @@
   }
 </script>
 <style scoped>
-  .learn-to-play-button {
-    width: 100%;
-    margin-bottom: 8px;
+  .welcome-area{
+    margin-top: 10px;
   }
-  .create-league-button {
-    width: 100%;
+  .welcome-header {
+    justify-content: center;
+    text-align: center;
+  }
+  .main-buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+
+  .main-button{
+    margin-top: 5px;
+    min-width: 200px;
   }
 </style>
