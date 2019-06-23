fe3089f04d98ec610a77866adce931f43fcdf626
Stephen Fallon <sfallon92@gmail.com>
2019-06-23 19:27:05 -0400

    Getting the buttons sized properly.

--- FantasyCritic.Web/ClientApp/components/modules/faqCollapse.vue
+++ FantasyCritic.Web/ClientApp/components/modules/faqCollapse.vue
@@ -1,11 +1,11 @@
 <template>
   <b-card no-body class="mb-1">
     <b-card-header header-tag="header" class="p-1" role="tab">
-      <b-btn block href="#" v-b-toggle="'accordion-' + errors.vmId" variant="secondary">
+      <div v-b-toggle="'accordion-' + errors.vmId" class="btn btn-secondary faq-button">
         <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
         <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
         <slot name="question"></slot>
-      </b-btn>
+      </div>
     </b-card-header>
     <b-collapse :id="'accordion-' + errors.vmId" :visible="defaultVisible" accordion="my-accordion" role="tabpanel">
       <b-card-body>
@@ -31,5 +31,11 @@
   .collapsed > .when-opened,
   :not(.collapsed) > .when-closed {
     display: none;
+  }
+
+  .faq-button {
+    width: 100%;
+    cursor: pointer;
+    font-weight: bold;
   }
 </style>
