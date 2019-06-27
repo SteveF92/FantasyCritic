4389058f65daa4d855a196820b9bc2659b093ab6
Stephen Fallon <sfallon92@gmail.com>
2019-06-27 00:46:49 -0400

    Changing language to reflect that green means "Eligible", but not necessarily "available"

--- FantasyCritic.Web/ClientApp/components/modules/statusBadge.vue
+++ FantasyCritic.Web/ClientApp/components/modules/statusBadge.vue
@@ -1,7 +1,7 @@
 <template>
   <span>
     <span class="badge badge-pill badge-primary badge-danger" v-show="taken">Taken</span>
-    <span class="badge badge-pill badge-primary badge-success" v-show="!taken && isEligible">Available</span>
+    <span class="badge badge-pill badge-primary badge-success" v-show="!taken && isEligible">Eligible</span>
     <span class="badge badge-pill badge-primary badge-warning" v-show="!taken && !isEligible">Ineligible</span>
   </span>
 </template>
