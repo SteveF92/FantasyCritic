42ca3210e87df028a367ae1c6a8d5abef2b445a4
Stephen Fallon <sfallon92@gmail.com>
2019-08-07 22:46:39 -0400

    Getting R test program working.

---
+++ FantasyCritic.Stats/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Xml.XPath;
+using RDotNet;
+
+namespace FantasyCritic.Stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            REngine.SetEnvironmentVariables();
+            var engine = REngine.GetInstance();
+            var rscriptData = Resources.critic_score_4;
+            string rscript = System.Text.Encoding.UTF8.GetString(rscriptData);
+            CharacterVector vector = engine.Evaluate(rscript).AsCharacter();
+            string result = vector[0];
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ FantasyCritic.Stats/critic_score_4.R
@@ -0,0 +1,41 @@
+library(dplyr)
+#library(tidytext)
+library(tidyr)
+#library(ggplot2)
+
+# read CSV file
+dat <- read.csv('./CleanMasterGames2.csv')
+
+# select meaningful + response vars
+ds <- dat %>%
+  select(EligiblePercentCounterPick, TotalBidAmount, DateAdjustedHypeFactor, CriticScore)
+
+# Replacing all zeroes to NA:
+ds[ds == 0] <- NA
+
+# summary(ds)
+
+# all games in 2020 have a preset score of 70
+# table(ds$CriticScore==70)
+
+# take out score 70 from response variable
+ds <- ds %>% filter(!CriticScore==70)
+
+# replace NAs with column Means
+ds <- replace_na(ds,as.list(colMeans(ds,na.rm=T)))
+
+# simple regression model
+simple.lm = lm(CriticScore ~ ., data = ds)
+
+# summary(simple.lm)
+
+
+# output the formula with updated coefficients
+#as.formula(
+  paste0("CriticScoreFormula = ", round(coefficients(simple.lm)[1],6), " + ", 
+         paste(sprintf("%.6f * %s", 
+                       coefficients(simple.lm)[-1],  
+                       names(coefficients(simple.lm)[-1])), 
+               collapse=" + ")
+  )
+#)
