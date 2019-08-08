6e8bd5e5d258716663340e30bdf93b2ddd4d01e0
Stephen Fallon <sfallon92@gmail.com>
2019-08-07 23:29:23 -0400

    Getting master games to send to R.

--- FantasyCritic.Stats/Program.cs
+++ FantasyCritic.Stats/Program.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Xml.XPath;
+using FantasyCritic.Lib.Interfaces;
+using FantasyCritic.MySQL;
+using NodaTime;
 using RDotNet;
 
 namespace FantasyCritic.Stats
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             REngine.SetEnvironmentVariables();
             var engine = REngine.GetInstance();
             string rscript = File.ReadAllText("critic_score_4.R");
+
+            string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            IMasterGameRepo masterGameRepo = new MySQLMasterGameRepo(connectionString, new MySQLFantasyCriticUserStore(connectionString, SystemClock.Instance));
+
+            var masterGames = await masterGameRepo.GetMasterGameYears(2019, true);
+
+            var gamesToOutput = masterGames
+                .Where(x => x.Year == 2019)
+                .Where(x => x.DateAdjustedHypeFactor > 0)
+                .Where(x => !x.WillRelease() || x.MasterGame.CriticScore.HasValue);
+
+            var outputModels = masterGames.Select(x => new MasterGameYearRInput(x));
 
             var args_r = new string[1] { "CleanMasterGames2.csv" };
             engine.SetCommandLineArguments(args_r);
