437479c8c65ca2c476b407cf65d926a88eba4dd6
Stephen Fallon <sfallon92@gmail.com>
2019-09-02 19:12:25 -0400

    Defining service actions.

--- FantasyCritic.Lib/Interfaces/IRoyaleRepo.cs
+++ FantasyCritic.Lib/Interfaces/IRoyaleRepo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FantasyCritic.Lib.Interfaces
 {
     public interface IRoyaleRepo
     {
+
     }
 }
--- FantasyCritic.Lib/Services/RoyaleService.cs
+++ FantasyCritic.Lib/Services/RoyaleService.cs
@@ -2,6 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using FantasyCritic.Lib.Domain;
+using FantasyCritic.Lib.Royale;
 
 namespace FantasyCritic.Lib.Services
 {
@@ -12,6 +16,31 @@ namespace FantasyCritic.Lib.Services
         public RoyaleService(IRoyaleRepo royaleRepo)
         {
             _royaleRepo = royaleRepo;
+        }
+
+        public Task<RoyalePublisher> CreatePublisher()
+        {
+
+        }
+
+        public Task<Maybe<RoyalePublisher>> GetPublisher(RoyaleYearQuarter yearQuarter, FantasyCriticUser user)
+        {
+
+        }
+
+        public Task<RoyalePublisherGame> PurchaseGame(RoyalePublisher publisher, MasterGame masterGame)
+        {
+
+        }
+
+        public Task SellGame(RoyalePublisher publisher, RoyalePublisherGame game)
+        {
+
+        }
+
+        public Task SetAdvertising(RoyalePublisher publisher, RoyalePublisherGame game, decimal advertisingMoney)
+        {
+
         }
     }
 }
