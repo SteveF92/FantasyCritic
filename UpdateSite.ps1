37fe9e3e32870976ed213ebd20c5b0fcd82b3525
Stephen Fallon <sfallon92@gmail.com>
2019-10-07 23:24:13 -0400

    Getting script to delete reliably.

--- UpdateSite.ps1
+++ UpdateSite.ps1
@@ -2,7 +2,12 @@ dotnet publish -c Release -o C:\FantasyCritic\BuildArea\
 Write-Output "Stopping site"
 Stop-Website "FantasyCritic"
 Write-Output "Deleting files"
-Get-ChildItem C:\FantasyCritic\Binary\ -Recurse | Remove-Item -Recurse -Force
+while((Get-ChildItem "C:\FantasyCritic\Binary\" | Measure-Object).Count -gt 0)
+{
+    Get-ChildItem C:\FantasyCritic\Binary\ -Recurse | Remove-Item -Recurse -Force
+}
+$numFiles = (Get-ChildItem "C:\FantasyCritic\Binary\" | Measure-Object).Count
+Write-Output "Delete done, $numFiles in folder."
 Write-Output "Copying files"
 Copy-Item -Path "C:\FantasyCritic\BuildArea\*" -Destination "C:\FantasyCritic\Binary\" -Force -Recurse
 Write-Output "Copying Items"
