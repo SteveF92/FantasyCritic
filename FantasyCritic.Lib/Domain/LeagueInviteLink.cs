d19b7137e3f4513382aab0d1dcb8277ae48482ab
Stephen Fallon <sfallon92@gmail.com>
2019-11-23 01:34:31 -0500

    Defining league invite link objects.

---
+++ FantasyCritic.Lib/Domain/LeagueInviteLink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+
+namespace FantasyCritic.Lib.Domain
+{
+    public class LeagueInviteLink
+    {
+        public LeagueInviteLink(Guid inviteID, League league, Guid inviteCode, bool active)
+        {
+            InviteID = inviteID;
+            League = league;
+            InviteCode = inviteCode;
+            Active = active;
+        }
+
+        public Guid InviteID { get; }
+        public League League { get; }
+        public Guid InviteCode { get; }
+        public bool Active { get; }
+    }
+}
