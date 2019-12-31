7791ef107d8a9be9034210030a1ede0082b49219
Stephen Fallon <sfallon92@gmail.com>
2019-12-31 00:03:11 -0500

    Removed a function not used anymore.

--- FantasyCritic.Lib/Utilities/MasterGameSearching.cs
+++ FantasyCritic.Lib/Utilities/MasterGameSearching.cs
@@ -58,39 +58,5 @@ namespace FantasyCritic.Lib.Utilities
             double percent = (double)longestCommon.Length / target.Length;
             return percent;
         }
-
-        //https://stackoverflow.com/a/9453762
-        private static int CalculateLevenshteinDistance(this string a, string b)
-        {
-            if (string.IsNullOrEmpty(a) && String.IsNullOrEmpty(b))
-            {
-                return 0;
-            }
-            if (string.IsNullOrEmpty(a))
-            {
-                return b.Length;
-            }
-            if (String.IsNullOrEmpty(b))
-            {
-                return a.Length;
-            }
-            int lengthA = a.Length;
-            int lengthB = b.Length;
-            var distances = new int[lengthA + 1, lengthB + 1];
-            for (int i = 0; i <= lengthA; distances[i, 0] = i++) ;
-            for (int j = 0; j <= lengthB; distances[0, j] = j++) ;
-
-            for (int i = 1; i <= lengthA; i++)
-            for (int j = 1; j <= lengthB; j++)
-            {
-                int cost = b[j - 1] == a[i - 1] ? 0 : 1;
-                distances[i, j] = Math.Min
-                (
-                    Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1),
-                    distances[i - 1, j - 1] + cost
-                );
-            }
-            return distances[lengthA, lengthB];
-        }
     }
 }
