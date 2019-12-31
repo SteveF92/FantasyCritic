471c9a8cc3bf47097adb67daa0b25260f2a25b1c
Stephen Fallon <sfallon92@gmail.com>
2019-12-31 00:21:03 -0500

    Further improved the searching algo.
    Using hype factor to break ties.

--- FantasyCritic.Lib/Utilities/MasterGameSearching.cs
+++ FantasyCritic.Lib/Utilities/MasterGameSearching.cs
@@ -12,17 +12,14 @@ namespace FantasyCritic.Lib.Utilities
     {
         public static IReadOnlyList<MasterGame> SearchMasterGames(string gameName, IEnumerable<MasterGame> masterGames) 
         {
-            var subsequenceMatches = masterGames
-                .Select(x => new Tuple<MasterGame, double>(x, GetDistance(gameName, x.GameName)));
-
             var substringMatches = masterGames
-                .Select(x => new Tuple<MasterGame, double>(x, GetPercentInCommon(gameName, x.GameName)));
+                .Select(x => new Tuple<MasterGame, double>(x, GetAbsoluteSubstringInCommon(gameName, x.GameName)));
 
-            var perfectMatches = substringMatches.Where(x => Math.Abs(x.Item2 - 1.0) < .01);
-            var filteredSubsequenceMatches = subsequenceMatches
-                .OrderByDescending(x => x.Item2);
+            var perfectMatches = substringMatches.Where(x => string.Equals(gameName, x.Item1.GameName, StringComparison.InvariantCultureIgnoreCase));
+            var filteredSubstringMatches = substringMatches.OrderByDescending(x => x.Item2);
+
             var combinedSequences = perfectMatches
-                .Concat(filteredSubsequenceMatches)
+                .Concat(filteredSubstringMatches)
                 .Select(x => x.Item1);
 
             return combinedSequences.Distinct().ToList();
@@ -30,33 +27,24 @@ namespace FantasyCritic.Lib.Utilities
 
         public static IReadOnlyList<MasterGameYear> SearchMasterGameYears(string gameName, IEnumerable<MasterGameYear> masterGames)
         {
-            var subsequenceMatches = masterGames
-                .Select(x => new Tuple<MasterGameYear, double>(x, GetDistance(gameName, x.MasterGame.GameName)));
-
             var substringMatches = masterGames
-                .Select(x => new Tuple<MasterGameYear, double>(x, GetPercentInCommon(gameName, x.MasterGame.GameName)));
+                .Select(x => new Tuple<MasterGameYear, double>(x, GetAbsoluteSubstringInCommon(gameName, x.MasterGame.GameName)));
 
-            var perfectMatches = substringMatches.Where(x => Math.Abs(x.Item2 - 1.0) < .01);
-            var filteredSubsequenceMatches = subsequenceMatches
-                .OrderByDescending(x => x.Item2);
+            var perfectMatches = substringMatches.Where(x => string.Equals(gameName, x.Item1.MasterGame.GameName, StringComparison.InvariantCultureIgnoreCase));
+            var filteredSubstringMatches = substringMatches.OrderByDescending(x => x.Item2).ThenByDescending(x => x.Item1.DateAdjustedHypeFactor);
+
             var combinedSequences = perfectMatches
-                .Concat(filteredSubsequenceMatches)
+                .Concat(filteredSubstringMatches)
                 .Select(x => x.Item1);
 
             return combinedSequences.Distinct().ToList();
         }
 
-        private static double GetDistance(string source, string target)
-        {
-            var longestCommon = source.ToLowerInvariant().LongestCommonSubsequence(target.ToLowerInvariant());
-            return longestCommon.Length;
-        }
-
-        private static double GetPercentInCommon(string source, string target)
+        private static double GetAbsoluteSubstringInCommon(string source, string target)
         {
             var longestCommon = source.ToLowerInvariant().LongestCommonSubstring(target.ToLowerInvariant());
-            double percent = (double)longestCommon.Length / target.Length;
-            return percent;
+            double result = longestCommon.Length;
+            return result;
         }
     }
 }
