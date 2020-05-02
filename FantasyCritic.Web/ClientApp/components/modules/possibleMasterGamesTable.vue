fa3c503ebafa8dad320478ed53b2cec34c21f068
Stephen Fallon <sfallon92@gmail.com>
2020-05-02 18:47:16 -0400

    Changing a heading to be more accurate.

--- FantasyCritic.Web/ClientApp/components/modules/possibleMasterGamesTable.vue
+++ FantasyCritic.Web/ClientApp/components/modules/possibleMasterGamesTable.vue
@@ -45,7 +45,7 @@
           { key: 'masterGame.gameName', label: 'Name', sortable: true, thClass:'bg-primary' },
           { key: 'masterGame.sortableEstimatedReleaseDate', label: 'Release Date', sortable: true, thClass: 'bg-primary' },
           { key: 'masterGame.dateAdjustedHypeFactor', label: 'Hype Factor', sortable: true, thClass: ['bg-primary','lg-screen-minimum'], tdClass: 'lg-screen-minimum' },
-          { key: 'masterGame.eligibilityLevel', label: 'Eligibility Level', thClass: ['bg-primary','lg-screen-minimum'], tdClass: 'lg-screen-minimum' },
+          { key: 'masterGame.eligibilityLevel', label: 'Status', thClass: ['bg-primary','lg-screen-minimum'], tdClass: 'lg-screen-minimum' },
           { key: 'select', label: '', thClass: 'bg-primary' }
         ],
         sortBy: 'dateAdjustedHypeFactor',
