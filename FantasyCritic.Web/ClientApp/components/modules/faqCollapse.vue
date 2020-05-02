b01e97840687d238c8a87479b29a7f3aca459a15
Stephen Fallon <sfallon92@gmail.com>
2020-05-02 18:07:36 -0400

    Fixing the FAQ page.

--- FantasyCritic.Web/ClientApp/components/modules/faqCollapse.vue
+++ FantasyCritic.Web/ClientApp/components/modules/faqCollapse.vue
@@ -1,13 +1,13 @@
-<template>
+..<template>
   <b-card no-body class="mb-1">
     <b-card-header header-tag="header" class="p-1" role="tab">
-      <div v-b-toggle="'accordion-' + errors.vmId" class="btn btn-secondary faq-button">
+      <div v-b-toggle="'accordion-' + _uid" class="btn btn-secondary faq-button">
         <font-awesome-icon icon="plus-circle" size="lg" transform="down-2" class="when-closed faq-icon" />
         <font-awesome-icon icon="minus-circle" size="lg" transform="down-2" class="when-opened faq-icon" />
         <slot name="question"></slot>
       </div>
     </b-card-header>
-    <b-collapse :id="'accordion-' + errors.vmId" :visible="defaultVisible" accordion="my-accordion" role="tabpanel">
+    <b-collapse :id="'accordion-' + _uid" :visible="defaultVisible" accordion="my-accordion" role="tabpanel">
       <b-card-body>
         <p class="card-text">
           <slot name="answer"></slot>
