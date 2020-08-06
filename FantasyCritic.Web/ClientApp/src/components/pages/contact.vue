469f50444ff341a107d850ebedbc1f5d6a6b432b
Stephen Fallon <sfallon92@gmail.com>
2020-08-06 01:16:28 -0400

    Adding more details to contact page.

--- FantasyCritic.Web/ClientApp/src/components/pages/contact.vue
+++ FantasyCritic.Web/ClientApp/src/components/pages/contact.vue
@@ -3,6 +3,16 @@
       <div class="col-md-10 offset-md-1 col-sm-12">
         <h1>Contact</h1>
         <hr />
+        <h2>Adding new Games/Corrections</h2>
+        <p class="text-well">
+          <ul>
+            <li>Please use the <router-link :to="{ name: 'masterGameRequest' }">Master Game Request form</router-link> for adding new games.</li>
+            <li>Please use the "Suggest Correction" form, which can be accessed by clicking on a master game link and clicking the "Suggest a Correction" option for correcting existing games.</li>
+          </ul>
+          
+          
+        </p>
+        <h2>Other questions/comments</h2>
         <p class="text-well">
           The best way to contact me is on Twitter <a href="https://twitter.com/fantasy_critic">@fantasy_critic <font-awesome-icon icon="external-link-alt" size="sm" /></a>,
           <a href="https://discord.gg/dNa7DD3">Discord <font-awesome-icon icon="external-link-alt" size="sm" /></a>,
