5796f48a4a02929c8e85edbc9951c9e4e954e5e3
AndrewKralovec <akrala@yahoo.com>
2020-09-07 09:58:18 -0500

    add api redirect for client env

--- FantasyCritic.Web/ClientApp/vue.config.js
+++ FantasyCritic.Web/ClientApp/vue.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  lintOnSave: false
+let config = {
+  lintOnSave: false,
+};
+
+if (process.env.NODE_ENV === "client") {
+  config = {
+    ...config,
+    devServer: {
+      proxy: {
+        // Route the api calls to the acutal site
+        "/api/**": {
+          target: "https://www.fantasycritic.games",
+          secure: false,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 }
+
+module.exports = config;
