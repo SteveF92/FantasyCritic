83f19a093e63160a956aa17114b2bf9996a42a74
Stephen Fallon <sfallon92@gmail.com>
2020-12-01 00:38:51 -0500

    Getting the fake repo building.

--- FantasyCritic.FakeRepo/Factories/MasterGameFactory.cs
+++ FantasyCritic.FakeRepo/Factories/MasterGameFactory.cs
@@ -13,14 +13,17 @@ namespace FantasyCritic.FakeRepo.Factories
         {
             List<MasterGame> games = new List<MasterGame>();
 
-            var sekiro = CreateMasterGame("96f5e8e3-672b-4626-b47e-4bff3a6c4430", "Sekiro: Shadows Die Twice", "2019-03-22", new LocalDate(2019, 3, 22), 
-                new LocalDate(2019, 3, 22), 6630, 89.9200m, new LocalDate(2019, 1, 1), 0, false, false, false, false, false, false, "Sekiro_art.jpg", 
-                "2019-03-20T15:30:00Z", false, false, "2019-01-22T15:30:00Z");
-            var metroidPrime = CreateMasterGame("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", "Metroid Prime 4", "TBA", LocalDate.MaxIsoValue, null, null,
-                null, new LocalDate(2020, 1, 1), 0, false, false, false, false, false, false, null, "2019-03-20T15:30:00Z", false, false, "2019-01-22T15:30:00Z");
-            var civSix = CreateMasterGame("3729b47d-35b6-48c3-8acd-a57364617b8e", "Sid Meier's Civilization VI: Rise and Fall", "2018-02-08", new LocalDate(2018, 2, 8), 
-                new LocalDate(2018, 2, 8), 5405, 82.6250m, new LocalDate(2019, 1, 1), 0, false, false, false, false, true, false,
-                null, "2019-03-20T15:30:00Z", false, false, "2019-01-22T15:30:00Z");
+            var sekiro = CreateMasterGame("96f5e8e3-672b-4626-b47e-4bff3a6c4430", "Sekiro: Shadows Die Twice", 
+                "2019-03-22", new LocalDate(2019, 1, 1), new LocalDate(2019, 3, 22), null, null, new LocalDate(2019, 3, 22), 
+                6630, 89.9200m, 0, false, false, false, false, false, false, "Sekiro_art.jpg", "2019-03-20T15:30:00Z", false, false, "2019-01-22T15:30:00Z");
+
+            var metroidPrime = CreateMasterGame("914fc4e8-1013-46a4-b7e1-c1ca0d60ab96", "Metroid Prime 4", 
+                "TBA", new LocalDate(2020, 1, 1), null, null, null, null,
+                null, null, 0, false, false, false, false, false, false, "", "2019-03-20T15:30:00Z", false, false, "2019-01-22T15:30:00Z");
+
+            var civSix = CreateMasterGame("3729b47d-35b6-48c3-8acd-a57364617b8e", "Sid Meier's Civilization VI: Rise and Fall",
+                "2018-02-08", new LocalDate(2019, 1, 1), new LocalDate(2018, 2, 8), null, null, new LocalDate(2018, 2, 8),
+                5405, 82.6250m, 0, false, false, false, false, true, false, "", "2019-03-20T15:30:00Z", false, false, "2019-01-22T15:30:00Z");
             games.Add(sekiro);
             games.Add(metroidPrime);
             games.Add(civSix);
@@ -28,19 +31,19 @@ namespace FantasyCritic.FakeRepo.Factories
             return games;
         }
 
-        private static MasterGame CreateMasterGame(string guid, string name, string estimatedReleaseDate, LocalDate sortableEstimatedReleaseDate, LocalDate? releaseDate, int? openCriticID, decimal? criticScore,
-            LocalDate minimumReleaseDate, int eligibilityLevelID, bool yearlyInstallment, bool earlyAccess, bool freeToPlay, bool releasedInternationally, bool expansionPack,
-            bool unannouncedGame, string boxartFileName, string firstCriticScoreTimestamp, bool doNotRefreshDate, bool doNotRefreshAnything, string addedTimestamp)
+        private static MasterGame CreateMasterGame(string guid, string name, string estimatedReleaseDate, LocalDate minimumReleaseDate, LocalDate? maximumReleaseDate, LocalDate? internationalReleaseDate, LocalDate? earlyAccessReleaseDate,
+            LocalDate? releaseDate, int? openCriticID, decimal? criticScore, int eligibilityLevelID, bool yearlyInstallment, bool earlyAccess, bool freeToPlay, 
+            bool releasedInternationally, bool expansionPack, bool unannouncedGame, string boxartFileName, string firstCriticScoreTimestamp, bool doNotRefreshDate, bool doNotRefreshAnything, string addedTimestamp)
         {
             EligibilityLevel eligibilityLevel = EligibilityLevelFactory.GetEligibilityLevels().Single(x => x.Level == eligibilityLevelID);
 
             var eligibilitySettings = new EligibilitySettings(eligibilityLevel, yearlyInstallment, earlyAccess,
                 freeToPlay, releasedInternationally, expansionPack, unannouncedGame);
 
-            var game = new MasterGame(Guid.Parse(guid), name, estimatedReleaseDate, sortableEstimatedReleaseDate, releaseDate, openCriticID,
-                criticScore, minimumReleaseDate, eligibilitySettings, "", boxartFileName, 
-                InstantPattern.ExtendedIso.Parse(firstCriticScoreTimestamp).GetValueOrThrow(), doNotRefreshDate, doNotRefreshAnything, false,
-                InstantPattern.ExtendedIso.Parse(addedTimestamp).GetValueOrThrow());
+            var game = new MasterGame(Guid.Parse(guid), name, estimatedReleaseDate, minimumReleaseDate, maximumReleaseDate, internationalReleaseDate, earlyAccessReleaseDate, releaseDate,
+                openCriticID, criticScore, eligibilitySettings, "", boxartFileName, InstantPattern.ExtendedIso.Parse(firstCriticScoreTimestamp).GetValueOrThrow(), doNotRefreshDate,
+                doNotRefreshAnything, false, InstantPattern.ExtendedIso.Parse(addedTimestamp).GetValueOrThrow(), new List<MasterSubGame>());
+
 
             return game;
         }
