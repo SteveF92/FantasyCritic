5322164adc86783208c0c0f0e8e2ef667387d4e8
Stephen Fallon <sfallon92@gmail.com>
2020-12-04 01:44:00 -0500

    Making the tag badge look nicer.

--- FantasyCritic.Web/ClientApp/src/components/modules/masterGameTagBadge.vue
+++ FantasyCritic.Web/ClientApp/src/components/modules/masterGameTagBadge.vue
@@ -1,5 +1,19 @@
 <template>
-  <span class="badge badge-pill badge-primary">{{value.readableName}}</span>
+  <div>
+    <span class="badge badge-pill badge-primary" id="popover-target-1">{{value.readableName}}</span>
+    <b-popover target="popover-target-1" triggers="hover" placement="top">
+      <template #title>
+        {{value.readableName}}
+      </template>
+      {{value.description}}
+      <div v-if="value.examples && value.examples.length > 0">
+        <h6>Examples</h6>
+        <ul>
+          <li v-for="example in value.examples">{{example}}</li>
+        </ul>
+      </div>
+    </b-popover>
+  </div>
 </template>
 <script>
 
