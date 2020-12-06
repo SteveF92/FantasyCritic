90a9be8d908c0662ad015cef23a9319cfaefab1e
Stephen Fallon <sfallon92@gmail.com>
2020-12-06 17:11:28 -0500

    Getting hot reload fixed?

--- FantasyCritic.Web/Startup.cs
+++ FantasyCritic.Web/Startup.cs
@@ -271,7 +271,7 @@ namespace FantasyCritic.Web
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseVueCli(npmScript: "serve");
+                    spa.UseVueCli(npmScript: "serve", port: 8080);
                 }
 
             });
