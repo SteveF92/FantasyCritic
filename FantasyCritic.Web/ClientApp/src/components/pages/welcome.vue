955542fbdce644982d9526c60f02728360006c31
Stephen Fallon <sfallon92@gmail.com>
2020-12-19 19:13:58 -0500

    Changing text on the front page.

--- FantasyCritic.Web/ClientApp/src/components/pages/welcome.vue
+++ FantasyCritic.Web/ClientApp/src/components/pages/welcome.vue
@@ -45,7 +45,7 @@
       </div>
       <div class="col-md-6 col-sm-12">
         <p class="welcome-message">
-          <h1 class="welcome-heading">New Mode: Critics Royale!</h1>
+          <h1 class="welcome-heading">Critics Royale!</h1>
           Don't have a league to join? Want to start playing in the middle of the year? Our new "Critics Royale" mode is structured similar to Daily Fantasy Sports, allowing you to compete on your own against
           the entire site for a leaderboard position. Instead of drafting, you're given a 'budget' which you use to select your games.
           Your goal is to spend that money wisely and put together the best lineup of games that you can.
