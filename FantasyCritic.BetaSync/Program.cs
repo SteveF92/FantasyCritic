8851b326a8c67224bd130e6b95397da7398452fb
Stephen Fallon <sfallon92@gmail.com>
2021-10-26 15:44:28 -0400

    Creating a program to clean/sync the beta server with the production server.

---
+++ FantasyCritic.BetaSync/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FantasyCritic.BetaSync
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new Exception("Unknown mode. Pass --clean or --sync.");
+            }
+
+            if (args[0] == "--clean")
+            {
+                return CleanBetaServer();
+            }
+            if (args[0] == "--sync")
+            {
+                return SyncProductionAndBeta();
+            }
+
+            throw new Exception("Unknown mode. Pass --clean or --sync.");
+        }
+
+        static async Task CleanBetaServer()
+        {
+            return;
+        }
+
+        static async Task SyncProductionAndBeta()
+        {
+            return;
+        }
+    }
+}
