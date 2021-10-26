c77fb2ba9faea11446c98c7900a1fbfe5acfc2d7
Stephen Fallon <sfallon92@gmail.com>
2021-10-26 16:30:57 -0400

    Cleaning emails and passwords for users that are not beta users.

---
+++ FantasyCritic.BetaSync/MySQLBetaCleaner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using FantasyCritic.Lib.Domain;
+using MoreLinq;
+using MySqlConnector;
+using NLog;
+
+namespace FantasyCritic.BetaSync
+{
+    public class MySQLBetaCleaner
+    {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private readonly string _connectionString;
+
+        public MySQLBetaCleaner(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task CleanEmailsAndPasswords(IEnumerable<FantasyCriticUser> allUsers, IEnumerable<FantasyCriticUser> betaUsers)
+        {
+            var nonBetaUsers = allUsers.Except(betaUsers).ToList();
+            List<string> updateStatements = new List<string>();
+            foreach (var nonBetaUser in nonBetaUsers)
+            {
+                var fakedEmailAddress = Guid.NewGuid() + "@example.com";
+                var fakedNormalizedEmailAddress = fakedEmailAddress.ToUpper();
+                string sql = $"UPDATE tbl_user SET EmailAddress = '{fakedEmailAddress}', NormalizedEmailAddress = '{fakedNormalizedEmailAddress}', PasswordHash = '', IsDeleted = 1 WHERE UserID = '{nonBetaUser.UserID}';";
+                updateStatements.Add(sql);
+            }
+
+            var batches = updateStatements.Batch(500).ToList();
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                using (var transaction = await connection.BeginTransactionAsync())
+                {
+                    for (var index = 0; index < batches.Count; index++)
+                    {
+                        _logger.Info($"Running user clean batch {index + 1}/{batches.Count}");
+                        var batch = batches[index];
+                        var joinedSQL = string.Join('\n', batch);
+                        await connection.ExecuteAsync(joinedSQL, transaction: transaction);
+                    }
+
+                    await transaction.CommitAsync();
+                }
+            }
+        }
+    }
+}
--- FantasyCritic.BetaSync/Program.cs
+++ FantasyCritic.BetaSync/Program.cs
@@ -1,44 +1,37 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Threading;
+using Dapper.NodaTime;
+using FantasyCritic.MySQL;
+using NLog;
+using NodaTime;
 
 namespace FantasyCritic.BetaSync
 {
     class Program
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
         private static string _productionReadOnlyConnectionString;
         private static string _betaConnectionString;
+        private static IClock _clock;
 
-        static Task Main(string[] args)
+        static async Task Main(string[] args)
         {
-            if (args.Length != 1)
-            {
-                throw new Exception("Unknown mode. Pass --clean or --sync.");
-            }
-
             _productionReadOnlyConnectionString = ConfigurationManager.AppSettings["productionConnectionString"];
             _betaConnectionString = ConfigurationManager.AppSettings["betaConnectionString"];
+            _clock = SystemClock.Instance;
+            DapperNodaTimeSetup.Register();
 
-            if (args[0] == "--clean")
-            {
-                return CleanBetaServer();
-            }
-            if (args[0] == "--sync")
-            {
-                return SyncProductionAndBeta();
-            }
+            MySQLFantasyCriticUserStore userStore = new MySQLFantasyCriticUserStore(_betaConnectionString, _clock);
+            MySQLBetaCleaner cleaner = new MySQLBetaCleaner(_betaConnectionString);
 
-            throw new Exception("Unknown mode. Pass --clean or --sync.");
-        }
+            _logger.Info("Cleaning emails/passwords for non-beta users");
+            var allUsers = await userStore.GetAllUsers();
+            var betaUsers = await userStore.GetUsersInRoleAsync("BetaTester", CancellationToken.None);
+            await cleaner.CleanEmailsAndPasswords(allUsers, betaUsers);
 
-        static async Task CleanBetaServer()
-        {
-            return;
-        }
-
-        static async Task SyncProductionAndBeta()
-        {
-            return;
         }
     }
 }
