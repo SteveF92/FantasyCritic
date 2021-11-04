261cba0fe16c74f0f308dcdfbd617d3c99de4e3a
Stephen Fallon <sfallon92@gmail.com>
2021-11-04 15:02:29 -0400

    Updated the SPA middleware.

--- FantasyCritic.Web/Startup.cs
+++ FantasyCritic.Web/Startup.cs
@@ -213,6 +213,8 @@ namespace FantasyCritic.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            _ = CommandLine.Arguments.TryGetOptions(System.Environment.GetCommandLineArgs(), false, out string mode, out ushort port, out bool https);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -222,6 +224,10 @@ namespace FantasyCritic.Web
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            if (https)
+            {
                 app.UseHttpsRedirection();
             }
 
@@ -242,7 +248,10 @@ namespace FantasyCritic.Web
                     };
                 }
             });
-            app.UseSpaStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
 
             app.UseRouting();
             app.UseAuthentication();
@@ -254,28 +263,45 @@ namespace FantasyCritic.Web
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
 
-                if (env.IsDevelopment())
-                {
-                    endpoints.MapToVueCliProxy(
-                        "{*path}",
-                        new SpaOptions { SourcePath = "ClientApp" },
-                        npmScript: "serve",
-                        regex: "Compiled successfully");
-                }
+                // Add MapRazorPages if the app uses Razor Pages. Since Endpoint Routing includes support for many frameworks, adding Razor Pages is now opt -in.
+                // endpoints.MapRazorPages();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
 
                 endpoints.MapHub<UpdateHub>("/updatehub");
             });
 
             app.UseSpa(spa =>
             {
-                if (env.IsDevelopment())
-                    spa.Options.SourcePath = "ClientApp/";
-                else
-                    spa.Options.SourcePath = "dist";
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseVueCli(npmScript: "serve", forceKill: true);
+                    // run npm process with client app
+                    if (mode == "start")
+                    {
+                        spa.UseVueCli(npmScript: "serve", port: port, forceKill: true, https: https);
+                    }
+
+                    // if you just prefer to proxy requests from client app, use proxy to SPA dev server instead,
+                    // app should be already running before starting a .NET client:
+                    // run npm process with client app
+                    if (mode == "attach")
+                    {
+                        spa.UseProxyToSpaDevelopmentServer($"{(https ? "https" : "http")}://localhost:{port}"); // your Vue app port
+                    }
+                }
+                else
+                {
+                    spa.Options.SourcePath = "dist";
                 }
             });
         }
