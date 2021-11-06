617740448c40907307ee9b9b148078407183f01c
Stephen Fallon <sfallon92@gmail.com>
2021-11-05 20:44:49 -0400

    Used the "identity scaffolding" to take control of Identity pages.

---
+++ FantasyCritic.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FantasyCritic.Lib.Identity;
+using FantasyCritic.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FantasyCritic.Web.Areas.Identity.IdentityHostingStartup))]
+namespace FantasyCritic.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
