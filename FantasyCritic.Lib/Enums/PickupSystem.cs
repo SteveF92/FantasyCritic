64a3600aa545a96f7f43e34c897b513f3318602a
Stephen Fallon <sfallon92@gmail.com>
2021-11-23 22:23:26 -0500

    Removed an old pickup system we don't use.

--- FantasyCritic.Lib/Enums/PickupSystem.cs
+++ FantasyCritic.Lib/Enums/PickupSystem.cs
@@ -7,7 +7,6 @@ namespace FantasyCritic.Lib.Enums
     public class PickupSystem : TypeSafeEnum<PickupSystem>
     {
         // Define values here.
-        public static readonly PickupSystem Manual = new PickupSystem("Manual");
         public static readonly PickupSystem Budget = new PickupSystem("Budget");
 
         // Constructor is private: values are defined within this class only!
