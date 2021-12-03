b2ec7532acf8fe52b15a398381ea38f07be11892
Stephen Fallon <sfallon92@gmail.com>
2021-12-03 00:26:57 -0500

    More cookie work

--- FantasyCritic.Web/Startup.cs
+++ FantasyCritic.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using VueCliMiddleware;
@@ -196,7 +197,8 @@ namespace FantasyCritic.Web
                 })
                 .AddIdentityServerJwt();
 
-            services.AddDataProtection();
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(@"C:\FantasyCritic\Keys\"));
 
             services.AddHsts(options =>
             {
