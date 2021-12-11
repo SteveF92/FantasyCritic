b162e387d6a93b037a929727198d209ad33948b1
Stephen Fallon <sfallon92@gmail.com>
2021-12-11 12:13:16 -0500

    Logging signalR failures.

--- FantasyCritic.Web/Hubs/UpdateHub.cs
+++ FantasyCritic.Web/Hubs/UpdateHub.cs
@@ -7,11 +7,14 @@ using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NLog;
 
 namespace FantasyCritic.Web.Hubs
 {
     public class UpdateHub : Hub
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
         private readonly FantasyCriticService _fantasyCriticService;
 
         public UpdateHub(FantasyCriticService fantasyCriticService)
@@ -21,15 +24,25 @@ namespace FantasyCritic.Web.Hubs
 
         public async Task Subscribe(string leagueID, string year)
         {
-            Guid leagueGUID = Guid.Parse(leagueID);
-            int yearInt = int.Parse(year);
-            var leagueYear = await _fantasyCriticService.GetLeagueYear(leagueGUID, yearInt);
-            if (leagueYear.HasNoValue)
+            try
             {
-                return;
-            }
+                Guid leagueGUID = Guid.Parse(leagueID);
+                int yearInt = int.Parse(year);
+                var leagueYear = await _fantasyCriticService.GetLeagueYear(leagueGUID, yearInt);
+                if (leagueYear.HasNoValue)
+                {
+                    return;
+                }
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, leagueYear.Value.GetGroupName);
+                await Groups.AddToGroupAsync(Context.ConnectionId, leagueYear.Value.GetGroupName);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("SignalR fail!");
+                _logger.Error(e);
+                throw;
+            }
+            
         }
     }
 }
