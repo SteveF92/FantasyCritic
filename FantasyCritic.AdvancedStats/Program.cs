40fcccd6705e0abbd662954d99fa37bc9a0019f7
Stephen Fallon <sfallon92@gmail.com>
2021-12-27 00:23:03 -0500

    Calculating most common winning games.

--- FantasyCritic.AdvancedStats/Program.cs
+++ FantasyCritic.AdvancedStats/Program.cs
@@ -42,7 +42,43 @@ namespace FantasyCritic.AdvancedStats
             MySQLMasterGameRepo masterGameRepo = new MySQLMasterGameRepo(_connectionString, userStore);
             MySQLFantasyCriticRepo fantasyCriticRepo = new MySQLFantasyCriticRepo(_connectionString, userStore, masterGameRepo);
 
+            var supportedYears = await fantasyCriticRepo.GetSupportedYears();
+            foreach (var supportedYear in supportedYears)
+            {
+                Console.WriteLine($"Stats for {supportedYear.Year}");
+                var allLeagueYears = await fantasyCriticRepo.GetLeagueYears(supportedYear.Year);
+                var publishers = await fantasyCriticRepo.GetAllPublishersForYear(supportedYear.Year, allLeagueYears);
+                var publishersByLeagueYear = publishers.ToLookup(x => x.LeagueYear.Key);
+                var winningPublishers = new List<Publisher>();
 
+                var leaguesToCount = allLeagueYears
+                    .Where(x => x.PlayStatus.DraftFinished)
+                    .Where(x => !x.League.TestLeague)
+                    .ToList();
+                foreach (var leagueYear in leaguesToCount)
+                {
+                    var publishersInLeagueYear = publishersByLeagueYear[leagueYear.Key];
+                    var winningPublisher = publishersInLeagueYear.MaxBy(x => x.TotalFantasyPoints).FirstOrDefault();
+                    winningPublishers.Add(winningPublisher);
+                }
+
+                var allPublisherGamesToCount = winningPublishers
+                    .SelectMany(x => x.PublisherGames)
+                    .Where(x => x.MasterGame.HasValue)
+                    .Where(x => !x.CounterPick)
+                    .ToList();
+
+                var grouped = allPublisherGamesToCount
+                    .GroupBy(x => x.MasterGame.Value.MasterGame)
+                    .Select(x => (x.Key, x.Count()))
+                    .OrderByDescending(x => x.Item2)
+                    .Take(10);
+
+                foreach (var game in grouped)
+                {
+                    Console.WriteLine($"{game.Key} was in {game.Item2}/{winningPublishers.Count} ({((double) game.Item2 / (double) winningPublishers.Count) * 100}%) of winning publishers in {supportedYear.Year}");
+                }
+            }
         }
     }
 }
