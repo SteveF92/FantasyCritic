c939cb32350bac9ab0e7f1578a3a36db74be3107
Stephen Fallon <sfallon92@gmail.com>
2022-01-09 14:12:22 -0500

    Bug fix SQL

--- Database/SchemaUpdates/FC_1.10.2-1.10.3_bugFixes.sql
+++ Database/SchemaUpdates/FC_1.10.2-1.10.3_bugFixes.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `tbl_user`
 	CHANGE COLUMN `SecurityStamp` `SecurityStamp` VARCHAR(255) NULL COLLATE 'utf8mb4_general_ci' AFTER `PasswordHash`;
+
+ALTER TABLE `tbl_mastergame`
+	DROP INDEX `UniqueBoxArt`;
