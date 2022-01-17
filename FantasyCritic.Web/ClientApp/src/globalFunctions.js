d88c195c22de65f99671188543f8517deed50614
Stephen Fallon <sfallon92@gmail.com>
2022-01-17 16:08:00 -0500

    Fixing publisher icon detection.

--- FantasyCritic.Web/ClientApp/src/globalFunctions.js
+++ FantasyCritic.Web/ClientApp/src/globalFunctions.js
@@ -2,6 +2,9 @@ import GraphemeBreaker from 'grapheme-breaker-mjs';
 
 export default {
   publisherIconIsValid(publisherIcon) {
+    if (!publisherIcon) {
+      return false;
+    }
     let length = GraphemeBreaker.countBreaks(publisherIcon);
     return length === 1;
   }
