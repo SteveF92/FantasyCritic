f102acbe6b6ca1047a115630c0293321af1f3ffe
Stephen Fallon <sfallon92@gmail.com>
2022-02-11 02:05:00 -0500

    Further refactors.

--- FantasyCritic.PublisherGameFixer/PublisherGameEntities.cs
+++ FantasyCritic.PublisherGameFixer/PublisherGameEntities.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 
 namespace FantasyCritic.PublisherGameFixer
 {
-    public record PublisherGameBidAmountUpdateEntity(Guid PublisherGameID, uint? BidAmount);
-    public record PublisherGameOverallDraftPositionUpdateEntity(Guid PublisherGameID, int? OverallDraftPosition);
-    public record PublisherGameDraftPositionUpdateEntity(Guid PublisherGameID, int? DraftPosition);
-    public record PublisherGameCombinedUpdateEntity(Guid PublisherGameID, int? OverallDraftPosition, int? DraftPosition);
+    public record PublisherGameBidAmountUpdateEntity(Guid PublisherGameID, uint BidAmount);
+    public record PublisherGameDraftPositionUpdateEntity(Guid PublisherGameID, int OverallDraftPosition, int DraftPosition);
 }
