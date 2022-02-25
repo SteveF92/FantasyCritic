5d5814435a99308180b1626d0cd3fdb98b3d4d24
Stephen Fallon <sfallon92@gmail.com>
2022-02-25 12:05:16 -0500

    Getting most of the propose trade UI in place.

--- FantasyCritic.Web/ClientApp/src/components/modules/modals/proposeTradeForm.vue
+++ FantasyCritic.Web/ClientApp/src/components/modules/modals/proposeTradeForm.vue
@@ -2,14 +2,78 @@
   <div>
     <form class="form-horizontal" hide-footer>
       <b-modal id="proposeTradeForm" size="lg" ref="proposeTradeFormRef" title="Propose Trade" @hidden="clearData">
-        <div class="alert alert-info">You can use this form to propose a trade.</div>
         <div class="form-group">
           <label for="counterParty" class="control-label">Publisher to trade with</label>
           <b-form-select v-model="counterParty">
-            <option v-for="publisher in leagueYear.publishers" v-bind:value="publisher">
+            <option v-for="publisher in otherPublishers" v-bind:value="publisher">
               {{ publisher.publisherName }}
             </option>
           </b-form-select>
+        </div>
+
+        <div v-show="counterParty">
+          <div class="row">
+            <div class="col-6">
+              <h4 class="text-black">Offer</h4>
+
+              <div>
+                <div v-for="(item, index) in proposerPublisherGames" :key="item.publisherGameID">
+                  <div class="trade-game-row">
+                    <label>{{index}}</label>
+                    <b-form-select v-model="item.game">
+                      <option v-for="publisherGame in publisher.games" v-bind:value="publisherGame">
+                        {{ publisherGame.gameName }}
+                      </option>
+                    </b-form-select>
+
+                    <div class="close-button fake-link" v-on:click="removeProposerGame(item.id)">
+                      <font-awesome-icon icon="times" size="lg" :style="{ color: '#414141' }" />
+                    </div>
+                  </div>
+                  
+                </div>
+                <b-button variant="secondary" class="add-game-button" v-on:click="addGame(proposerPublisherGames)">Add Game</b-button>
+              </div>
+            </div>
+            <div class="col-6">
+              <h4 class="text-black">Receive</h4>
+
+              <div>
+                <div v-for="(item, index) in counterPartyPublisherGames" :key="item.publisherGameID">
+                  <div class="trade-game-row">
+                    <label>{{index}}</label>
+                    <b-form-select v-model="item.game">
+                      <option v-for="publisherGame in publisher.games" v-bind:value="publisherGame">
+                        {{ publisherGame.gameName }}
+                      </option>
+                    </b-form-select>
+
+                    <div class="close-button fake-link" v-on:click="removeCounterPartyGame(item.id)">
+                      <font-awesome-icon icon="times" size="lg" :style="{ color: '#414141' }" />
+                    </div>
+                  </div>
+
+                </div>
+                <b-button variant="secondary" class="add-game-button" v-on:click="addGame(counterPartyPublisherGames)">Add Game</b-button>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <ValidationProvider rules="required|integer" v-slot="{ errors }">
+                <label>Budget</label>
+                <input v-model="proposerBudgetSendAmount" id="proposerBudgetSendAmount" name="proposerBudgetSendAmount" type="number" class="form-control input" />
+                <span class="text-danger">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </div>
+            <div class="col-6">
+              <ValidationProvider rules="required|integer" v-slot="{ errors }">
+                <label>Budget</label>
+                <input v-model="counterPartyBudgetSendAmount" id="counterPartyBudgetSendAmount" name="counterPartyBudgetSendAmount" type="number" class="form-control input" />
+                <span class="text-danger">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </div>
+          </div>
         </div>
 
         <div slot="modal-footer">
@@ -27,16 +91,37 @@ export default {
   data() {
     return {
       counterParty: null,
-      errorInfo: ''
+      proposerPublisherGames: [],
+      counterPartyPublisherGames: [],
+      proposerBudgetSendAmount: 0,
+      counterPartyBudgetSendAmount: 0,
+      errorInfo: '',
+      indexer: 0
     };
   },
   props: ['leagueYear', 'publisher'],
   computed: {
     tradeIsValid() {
       return true;
+    },
+    otherPublishers() {
+      return _.filter(this.leagueYear.publishers, x => x.publisherID !== this.publisher.publisherID);
     }
   },
   methods: {
+    addGame(list) {
+      const element = {
+        id: this.indexer++,
+        game: null
+      }
+      list.push(element);
+    },
+    removeProposerGame(id) {
+      this.proposerPublisherGames = _.filter(this.proposerPublisherGames, x => x.id !== id);
+    },
+    removeCounterPartyGame(id) {
+      this.counterPartyPublisherGames = _.filter(this.proposerPublisherGames, x => x.id !== id);
+    },
     proposeTrade() {
       var request = {
 
@@ -54,25 +139,25 @@ export default {
     },
     clearData() {
       this.counterParty = null;
+      this.proposerPublisherGames = [];
+      this.counterPartyPublisherGames = [];
+      this.proposerBudgetSendAmount = 0;
+      this.counterPartyBudgetSendAmount = 0;
     }
   }
 };
 </script>
 <style scoped>
+  .trade-game-row {
+    width: 100%;
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    margin-bottom: 5px;
+  }
 
   .add-game-button {
     width: 100%;
-  }
-
-  .claim-error {
-    margin-top: 10px;
-  }
-
-  .game-search-input {
-    margin-bottom: 15px;
-  }
-
-  .remove-error {
-    margin-top: 15px;
   }
 </style>
