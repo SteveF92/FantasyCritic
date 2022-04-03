1c551c5d71c48d8453be829e91ffda716fba0617
Stephen Fallon <sfallon92@gmail.com>
2022-04-03 18:58:40 -0400

    Getting the layout looking good.

--- src/FantasyCritic.Web/ClientApp/src/components/gameTables/publisherCoverView.vue
+++ src/FantasyCritic.Web/ClientApp/src/components/gameTables/publisherCoverView.vue
@@ -2,18 +2,10 @@
   <div>
     <div class="cover-art-header">
       <a href="https://ggapp.io/" target="_blank">
-        <strong>
-          All Images Provided by GG|
-          <font-awesome-icon icon="external-link-alt" />
-        </strong>
+        <strong>All Images Provided by GG|</strong>
       </a>
-
-      <b-button v-if="userIsPublisher" variant="secondary" size="sm" @click="sharePublisher">
-        <font-awesome-icon icon="share-alt" size="lg" class="share-button" />
-        <span>Share</span>
-      </b-button>
     </div>
-    <div id="cover-art-container">
+    <div class="cover-art-container">
       <div v-for="gameSlot in gameSlots" :key="gameSlot.overallSlotNumber">
         <publisherCoverViewSlot :game-slot="gameSlot"></publisherCoverViewSlot>
       </div>
@@ -22,8 +14,6 @@
 </template>
 
 <script>
-import html2canvas from 'html2canvas';
-
 import PublisherMixin from '@/mixins/publisherMixin';
 import PublisherCoverViewSlot from '@/components/gameTables/publisherCoverViewSlot';
 
@@ -31,28 +21,7 @@ export default {
   components: {
     PublisherCoverViewSlot
   },
-  mixins: [PublisherMixin],
-  methods: {
-    sharePublisher() {
-      let elementID = '#cover-art-container';
-      const options = { allowTaint: false, useCORS: true, scale: 2 };
-      html2canvas(document.querySelector(elementID), options).then(async (canvas) => {
-        const dataUrl = canvas.toDataURL('png');
-        const blob = await (await fetch(dataUrl)).blob();
-        const filesArray = [
-          new File([blob], 'myPublisher.png', {
-            type: blob.type,
-            lastModified: new Date().getTime()
-          })
-        ];
-        const shareData = {
-          files: filesArray,
-          title: 'My Fantasy Critic Publisher'
-        };
-        navigator.share(shareData);
-      });
-    }
-  }
+  mixins: [PublisherMixin]
 };
 </script>
 <style scoped>
@@ -61,15 +30,14 @@ export default {
   justify-content: space-between;
   align-items: center;
   margin-bottom: 10px;
+  padding-left: 10px;
 }
 
-#cover-art-container {
+.cover-art-container {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   gap: 6px;
-  background: #222222;
-  border-radius: 5px;
   max-width: 1500px;
   padding: 10px;
 }
--- src/FantasyCritic.Web/ClientApp/src/views/publisher.vue
+++ src/FantasyCritic.Web/ClientApp/src/views/publisher.vue
@@ -2,90 +2,115 @@
   <div v-if="publisher && leagueYear">
     <div class="col-md-10 offset-md-1 col-sm-12">
       <div class="cover-art-mode-options">
+        <b-button v-if="userIsPublisher" v-show="coverArtMode" variant="secondary" size="sm" @click="sharePublisher">
+          <font-awesome-icon icon="share-alt" size="lg" class="share-button" />
+          <span>Share</span>
+        </b-button>
         <label>Cover Art Mode</label>
         <toggle-button v-model="editableCoverArtMode" class="toggle" :sync="true" :labels="{ checked: 'On', unchecked: 'Off' }" :css-colors="true" :font-size="13" :width="60" :height="28" />
       </div>
-      <div class="publisher-header">
-        <div class="publisher-details">
-          <div class="publisher-name-and-icon">
-            <div v-if="publisher.publisherIcon && iconIsValid" class="publisher-icon">
-              {{ publisher.publisherIcon }}
+      <div v-show="!coverArtMode">
+        <div class="publisher-header">
+          <div class="publisher-details">
+            <div class="publisher-name-and-icon">
+              <div v-if="publisher.publisherIcon && iconIsValid" class="publisher-icon">
+                {{ publisher.publisherIcon }}
+              </div>
+              <div class="publisher-name">
+                <h1>{{ publisher.publisherName }}</h1>
+              </div>
             </div>
-            <div class="publisher-name">
-              <h1>{{ publisher.publisherName }}</h1>
+            <h4>Player Name: {{ publisher.playerName }}</h4>
+            <h4>
+              <router-link :to="{ name: 'league', params: { leagueid: publisher.leagueID, year: publisher.year } }">League: {{ publisher.leagueName }}</router-link>
+            </h4>
+            <ul>
+              <li>Budget: {{ publisher.budget | money }}</li>
+              <li>Will Release Games Dropped: {{ getDropStatus(publisher.willReleaseGamesDropped, publisher.willReleaseDroppableGames) }}</li>
+              <li>Will Not Release Games Dropped: {{ getDropStatus(publisher.willNotReleaseGamesDropped, publisher.willNotReleaseDroppableGames) }}</li>
+              <li>"Any Unreleased" Games Dropped: {{ getDropStatus(publisher.freeGamesDropped, publisher.freeDroppableGames) }}</li>
+            </ul>
+          </div>
+        </div>
+
+        <div v-if="!leagueYear.league.publicLeague && !(publisher.userIsInLeague || publisher.outstandingInvite)" class="alert alert-warning">You are viewing a private league.</div>
+
+        <div v-show="moveGameError" class="alert alert-danger">
+          {{ moveGameError }}
+        </div>
+
+        <div v-if="hasIneligibleGame" class="alert alert-warning">
+          {{ referToPlayer('You have', 'This player has') }} games in ineligible slots. There are a few reasons this could happen.
+          <ul>
+            <li>A game's tags changed after a correction was made.</li>
+            <li>{{ referToPlayer('You', 'They') }} picked up a game and there were no eligible slots it could fit in.</li>
+            <li v-if="leagueYear.hasSpecialSlots">{{ referToPlayer('You', 'They') }} intentionally moved a game into a slot it's not eligble for. This used to be possible but no longer is.</li>
+          </ul>
+
+          The options going forward are:
+          <ul>
+            <li v-if="leagueYear.hasSpecialSlots">{{ referToPlayer('You', 'They') }} can reorganize {{ referToPlayer('your', 'their') }} games so everything is eligible.</li>
+            <li>The league manager can override a game's tags if the league disagrees with the tags the site decided.</li>
+            <li>{{ referToPlayer('You', 'They') }} can drop the game (depending on the league settings).</li>
+            <li>The league could decide to give {{ referToPlayer('you', 'they') }} a "free drop" regardless of the league settings if they decide that this game should not be eligible.</li>
+            <li>{{ referToPlayer('You', 'They') }} could trade the game to another player.</li>
+          </ul>
+
+          <template v-if="leagueYear.hasSpecialSlots">
+            Generally speaking, players should always be putting their games into slots they are eligible for. Intentionally keeping games in ineligible slots in order to free up 'more valuable' slots
+            is outside of the spirit of Fantasy Critic.
+          </template>
+        </div>
+
+        <div v-show="!coverArtMode" class="table-options">
+          <b-button v-if="!sortOrderMode && leagueYear.hasSpecialSlots && userIsPublisher && !moveMode" variant="info" @click="enterMoveMode">Move Games</b-button>
+          <b-button v-if="!sortOrderMode && leagueYear.hasSpecialSlots && moveMode" variant="secondary" @click="cancelMoveMode">Cancel Movement</b-button>
+          <b-button v-if="!sortOrderMode && leagueYear.hasSpecialSlots && moveMode" variant="success" @click="confirmPositions">Confirm Positions</b-button>
+          <template v-if="!moveMode && isPlusUser">
+            <b-form-checkbox v-show="sortOrderMode && hasFormerGames" v-model="editableIncludeRemovedInSorted">
+              <span class="checkbox-label">Include Dropped Games</span>
+            </b-form-checkbox>
+            <toggle-button
+              v-model="editableSortOrderMode"
+              class="toggle"
+              :sync="true"
+              :labels="{ checked: 'Sort Mode', unchecked: 'Slot Mode' }"
+              :css-colors="true"
+              :font-size="13"
+              :width="107"
+              :height="28" />
+          </template>
+        </div>
+
+        <playerGameTable></playerGameTable>
+      </div>
+
+      <div v-show="coverArtMode" class="cover-art-container">
+        <div id="full-cover-art-view">
+          <div class="cover-art-view-publisher-info">
+            <div class="publisher-name-and-icon">
+              <div v-if="publisher.publisherIcon && iconIsValid" class="publisher-icon">
+                {{ publisher.publisherIcon }}
+              </div>
+              <div class="publisher-name">
+                <h1>{{ publisher.publisherName }}</h1>
+              </div>
+            </div>
+            <div>
+              <h4>Player Name: {{ publisher.playerName }}</h4>
+              <h4>League: {{ publisher.leagueName }}</h4>
             </div>
           </div>
-          <h4>Player Name: {{ publisher.playerName }}</h4>
-          <h4>
-            <router-link :to="{ name: 'league', params: { leagueid: publisher.leagueID, year: publisher.year } }">League: {{ publisher.leagueName }}</router-link>
-          </h4>
-          <ul>
-            <li>Budget: {{ publisher.budget | money }}</li>
-            <li>Will Release Games Dropped: {{ getDropStatus(publisher.willReleaseGamesDropped, publisher.willReleaseDroppableGames) }}</li>
-            <li>Will Not Release Games Dropped: {{ getDropStatus(publisher.willNotReleaseGamesDropped, publisher.willNotReleaseDroppableGames) }}</li>
-            <li>"Any Unreleased" Games Dropped: {{ getDropStatus(publisher.freeGamesDropped, publisher.freeDroppableGames) }}</li>
-          </ul>
+          <publisherCoverView></publisherCoverView>
         </div>
-      </div>
-
-      <div v-if="!leagueYear.league.publicLeague && !(publisher.userIsInLeague || publisher.outstandingInvite)" class="alert alert-warning">You are viewing a private league.</div>
-
-      <div v-show="moveGameError" class="alert alert-danger">
-        {{ moveGameError }}
-      </div>
-
-      <div v-if="hasIneligibleGame" class="alert alert-warning">
-        {{ referToPlayer('You have', 'This player has') }} games in ineligible slots. There are a few reasons this could happen.
-        <ul>
-          <li>A game's tags changed after a correction was made.</li>
-          <li>{{ referToPlayer('You', 'They') }} picked up a game and there were no eligible slots it could fit in.</li>
-          <li v-if="leagueYear.hasSpecialSlots">{{ referToPlayer('You', 'They') }} intentionally moved a game into a slot it's not eligble for. This used to be possible but no longer is.</li>
-        </ul>
-
-        The options going forward are:
-        <ul>
-          <li v-if="leagueYear.hasSpecialSlots">{{ referToPlayer('You', 'They') }} can reorganize {{ referToPlayer('your', 'their') }} games so everything is eligible.</li>
-          <li>The league manager can override a game's tags if the league disagrees with the tags the site decided.</li>
-          <li>{{ referToPlayer('You', 'They') }} can drop the game (depending on the league settings).</li>
-          <li>The league could decide to give {{ referToPlayer('you', 'they') }} a "free drop" regardless of the league settings if they decide that this game should not be eligible.</li>
-          <li>{{ referToPlayer('You', 'They') }} could trade the game to another player.</li>
-        </ul>
-
-        <template v-if="leagueYear.hasSpecialSlots">
-          Generally speaking, players should always be putting their games into slots they are eligible for. Intentionally keeping games in ineligible slots in order to free up 'more valuable' slots
-          is outside of the spirit of Fantasy Critic.
-        </template>
-      </div>
-
-      <div v-show="!coverArtMode" class="table-options">
-        <b-button v-if="!sortOrderMode && leagueYear.hasSpecialSlots && userIsPublisher && !moveMode" variant="info" @click="enterMoveMode">Move Games</b-button>
-        <b-button v-if="!sortOrderMode && leagueYear.hasSpecialSlots && moveMode" variant="secondary" @click="cancelMoveMode">Cancel Movement</b-button>
-        <b-button v-if="!sortOrderMode && leagueYear.hasSpecialSlots && moveMode" variant="success" @click="confirmPositions">Confirm Positions</b-button>
-        <template v-if="!moveMode && isPlusUser">
-          <b-form-checkbox v-show="sortOrderMode && hasFormerGames" v-model="editableIncludeRemovedInSorted">
-            <span class="checkbox-label">Include Dropped Games</span>
-          </b-form-checkbox>
-          <toggle-button
-            v-model="editableSortOrderMode"
-            class="toggle"
-            :sync="true"
-            :labels="{ checked: 'Sort Mode', unchecked: 'Slot Mode' }"
-            :css-colors="true"
-            :font-size="13"
-            :width="107"
-            :height="28" />
-        </template>
-      </div>
-
-      <playerGameTable v-show="!coverArtMode"></playerGameTable>
-      <div class="cover-view">
-        <publisherCoverView v-show="coverArtMode"></publisherCoverView>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import html2canvas from 'html2canvas';
+
 import PlayerGameTable from '@/components/gameTables/playerGameTable';
 import PublisherCoverView from '@/components/gameTables/publisherCoverView';
 import GlobalFunctions from '@/globalFunctions';
@@ -182,13 +207,31 @@ export default {
       this.$store.dispatch('confirmPositions').then(() => {
         this.$emit('gamesMoved');
       });
+    },
+    sharePublisher() {
+      let elementID = '#full-cover-art-view';
+      const options = { allowTaint: false, useCORS: true, scale: 2 };
+      html2canvas(document.querySelector(elementID), options).then(async (canvas) => {
+        const dataUrl = canvas.toDataURL('png');
+        const blob = await (await fetch(dataUrl)).blob();
+        const filesArray = [
+          new File([blob], 'myPublisher.png', {
+            type: blob.type,
+            lastModified: new Date().getTime()
+          })
+        ];
+        const shareData = {
+          files: filesArray,
+          title: 'My Fantasy Critic Publisher'
+        };
+        navigator.share(shareData);
+      });
     }
   }
 };
 </script>
 <style scoped>
 .publisher-header {
-  margin-top: 10px;
   margin-bottom: 10px;
   display: flex;
 }
@@ -222,6 +265,7 @@ export default {
 
 .cover-art-mode-options {
   margin-top: 10px;
+  margin-bottom: 10px;
   display: flex;
   justify-content: end;
   align-items: center;
@@ -248,8 +292,20 @@ export default {
   margin: 0;
 }
 
-.cover-view {
+.cover-art-container {
   display: flex;
   justify-content: center;
+}
+
+#full-cover-art-view {
+  background: #222222;
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.cover-art-view-publisher-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
