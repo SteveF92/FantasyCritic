41ab37bb4cb65501158f04f71f7734c64d72604a
Stephen Fallon <sfallon92@gmail.com>
2022-05-03 19:35:11 -0400

    SQL statement to create special auction table.

---
+++ Database/SchemaUpdates/FC_2.3.2-2.3.3_specialAuctions.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `tbl_league_specialauction` (
+	`LeagueID` CHAR(36) NOT NULL,
+	`Year` YEAR NOT NULL,
+	`MasterGameID` CHAR(36) NOT NULL,
+	`CreationTime` DATETIME NOT NULL,
+	`ScheduledEndTime` DATETIME NOT NULL,
+	`Processed` BIT(1) NOT NULL,
+	PRIMARY KEY (`LeagueID`, `Year`, `MasterGameID`) USING BTREE,
+	INDEX `FK_tbl_league_specialauction_tbl_mastergame` (`MasterGameID`) USING BTREE,
+	CONSTRAINT `FK_tbl_league_specialauction_tbl_league_year` FOREIGN KEY (`LeagueID`, `Year`) REFERENCES `tbl_league_year` (`LeagueID`, `Year`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `FK_tbl_league_specialauction_tbl_mastergame` FOREIGN KEY (`MasterGameID`) REFERENCES `tbl_mastergame` (`MasterGameID`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+ENGINE=InnoDB
+;
