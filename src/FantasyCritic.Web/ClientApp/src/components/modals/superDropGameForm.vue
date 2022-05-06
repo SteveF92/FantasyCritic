cf106946bb6ec7ae7fb76c05a10a28cf825c2066
Stephen Fallon <sfallon92@gmail.com>
2022-05-06 17:16:55 -0400

    Finishing the super drop modal.

--- src/FantasyCritic.Web/ClientApp/src/components/modals/superDropGameForm.vue
+++ src/FantasyCritic.Web/ClientApp/src/components/modals/superDropGameForm.vue
@@ -1,24 +1,31 @@
 <template>
-  <b-modal id="superDropGameForm" ref="superDropGameFormRef" size="lg" title="Drop a Game" hide-footer @hidden="clearData">
+  <b-modal id="superDropGameForm" ref="superDropGameFormRef" size="lg" title="Super Drop a Game" hide-footer @hidden="clearData">
     <div v-if="errorInfo" class="alert alert-danger" role="alert">
       {{ errorInfo }}
     </div>
-    <p>You can use this form to request to super drop a game.</p>
+    <div class="alert alert-info">
+      Super drops work differently from normal drops. You can drop any game you wish, and drops happen immediately, rather than having to wait until Saturday.
+      <br />
+      <br />
+      <template v-if="userPublisher.superDropsAvailable > 1">You currently have {{ userPublisher.superDropsAvailable }} super drops.</template>
+      <template v-if="userPublisher.superDropsAvailable === 1">You currently have 1 super drop.</template>
+      <template v-if="!userPublisher.superDropsAvailable">You do not currently have any super drops.</template>
+    </div>
     <form class="form-horizontal" hide-footer @submit.prevent="dropGame">
       <div class="form-group">
         <label for="gameToDrop" class="control-label">Game</label>
         <b-form-select v-model="gameToDrop">
-          <option v-for="publisherGame in droppableGames" :key="publisherGame.publisherGameID" :value="publisherGame">
+          <option v-for="publisherGame in userPublisher.games" :key="publisherGame.publisherGameID" :value="publisherGame">
             {{ publisherGame.gameName }}
           </option>
         </b-form-select>
       </div>
 
       <div v-if="gameToDrop">
-        <input type="submit" class="btn btn-danger add-game-button" value="Super Drop Game" :disabled="isBusy" />
+        <input type="submit" class="btn btn-danger full-width-button" value="Super Drop Game" :disabled="isBusy" />
       </div>
-      <hr />
       <div v-if="dropResult && !dropResult.success" class="alert bid-error alert-danger">
+        <hr />
         <h3 class="alert-heading">Error!</h3>
         <ul>
           <li v-for="error in dropResult.errors" :key="error">{{ error }}</li>
@@ -45,35 +52,31 @@ export default {
   computed: {
     formIsValid() {
       return this.dropMasterGame;
-    },
-    droppableGames() {
-      return _.filter(this.userPublisher.games, { counterPick: false });
     }
   },
   methods: {
-    dropGame() {
+    async dropGame() {
       var request = {
         publisherID: this.userPublisher.publisherID,
         publisherGameID: this.gameToDrop.publisherGameID
       };
-      this.isBusy = true;
-      axios
-        .post('/api/league/MakeDropRequest', request)
-        .then((response) => {
-          this.isBusy = false;
-          this.dropResult = response.data;
-          if (!this.dropResult.success) {
-            return;
-          }
 
-          this.notifyAction('Drop Request for ' + this.gameToDrop.gameName + ' was made.');
-          this.$refs.superDropGameFormRef.hide();
-          this.clearData();
-        })
-        .catch((response) => {
-          this.isBusy = false;
-          this.errorInfo = response.response.data;
-        });
+      this.isBusy = true;
+      try {
+        const response = await axios.post('/api/league/UseSuperDrop', request);
+        this.isBusy = false;
+        this.dropResult = response.data;
+        if (!this.dropResult.success) {
+          return;
+        }
+
+        this.notifyAction(this.gameToDrop.gameName + ' has been super dropped.');
+        this.$refs.superDropGameFormRef.hide();
+        this.clearData();
+      } catch (error) {
+        this.isBusy = false;
+        this.errorInfo = error.response.data;
+      }
     },
     clearData() {
       this.dropResult = null;
