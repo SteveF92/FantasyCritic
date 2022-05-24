18bb44f94a9540ee93e3a64272b88262eb02b52e
Stephen Fallon <sfallon92@gmail.com>
2022-05-24 17:40:56 -0400

    Beginning to set up AWS secrets manager.

---
+++ src/FantasyCritic.Web/Utilities/ConfigurationFileStore.cs
@@ -0,0 +1,24 @@
+using FantasyCritic.Lib.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace FantasyCritic.Web.Utilities;
+public class ConfigurationFileStore : IConfigurationStore
+{
+    private readonly ConfigurationManager _configurationManager;
+
+    public ConfigurationFileStore(ConfigurationManager configurationManager)
+    {
+        _configurationManager = configurationManager;
+    }
+
+    public string GetConfigValue(string name)
+    {
+        var value = _configurationManager[name];
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new Exception($"Config value: {name} not found.");
+        }
+
+        return value;
+    }
+}
