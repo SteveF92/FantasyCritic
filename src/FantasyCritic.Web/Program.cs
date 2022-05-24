0e45210704ee748f688e0fce0fce8f674df55b03
Stephen Fallon <sfallon92@gmail.com>
2022-05-24 13:53:12 -0400

    Getting closer to the logging I want.

--- src/FantasyCritic.Web/Program.cs
+++ src/FantasyCritic.Web/Program.cs
@@ -7,12 +7,42 @@ using Serilog;
 using Serilog.Events;
 
 string allLogPath = @"C:\FantasyCritic\Logs\log-all";
+string myLogPath = @"C:\FantasyCritic\Logs\log-my";
 string warnLogPath = @"C:\FantasyCritic\Logs\log-warning";
+string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({SourceContext}.{Method}) {Message}{NewLine}{Exception}";
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
     .Enrich.FromLogContext()
-    .WriteTo.File(allLogPath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3)
-    .WriteTo.File(warnLogPath, rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Warning, retainedFileCountLimit: 10)
+    .WriteTo.File(allLogPath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3, outputTemplate: outputTemplate)
+    .WriteTo.File(warnLogPath, rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Warning, retainedFileCountLimit: 10, outputTemplate: outputTemplate)
+
+    .WriteTo.Logger(config =>
+    {
+        config.Filter
+            .ByExcluding(logEvent =>
+            {
+                if (logEvent.Properties.TryGetValue("SourceContext", out var sourceContext))
+                {
+                    var sourceContextString = sourceContext?.ToString();
+                    if (sourceContextString is not null && sourceContextString.StartsWith("\"FantasyCritic"))
+                    {
+                        return false;
+                    }
+                }
+
+                if (logEvent.Properties.TryGetValue("ActionName", out var actionName))
+                {
+                    var actionNameString = actionName?.ToString();
+                    if (actionNameString is not null && actionNameString.StartsWith("\"FantasyCritic"))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            })
+            .WriteTo.File(myLogPath, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 5, outputTemplate: outputTemplate);
+    })
     .CreateLogger();
 
 try
