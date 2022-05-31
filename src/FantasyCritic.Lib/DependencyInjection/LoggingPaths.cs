092a28585de60c809c93159cafefe5c305ac190c
Stephen Fallon <sfallon92@gmail.com>
2022-05-31 14:16:34 -0400

    Added linux logging paths.

---
+++ src/FantasyCritic.Lib/DependencyInjection/LoggingPaths.cs
@@ -0,0 +1,23 @@
+using System.Runtime.InteropServices;
+
+namespace FantasyCritic.Lib.DependencyInjection;
+
+public class LoggingPaths
+{
+    private readonly string _windowsBase = @"C:\FantasyCritic\Logs\";
+    private readonly string _linuxBase = @"/var/log/fantasyCritic/";
+
+    public string AllLogPath => $"{GetBase()}log-all.txt";
+    public string MyLogPath => $"{GetBase()}log-my.txt";
+    public string WarnLogPath => $"{GetBase()}log-warning.txt";
+
+    private string GetBase()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return _windowsBase;
+        }
+
+        return _linuxBase;
+    }
+}
