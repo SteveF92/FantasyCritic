93454042b08d888e60c39335cf1af3a83dfa33b8
Stephen Fallon <sfallon92@gmail.com>
2022-06-18 16:05:04 -0400

    Removed references to Critic's Royale being a "beta". It's been almost three years...

--- src/FantasyCritic.Web/ClientApp/src/views/criticsRoyale.vue
+++ src/FantasyCritic.Web/ClientApp/src/views/criticsRoyale.vue
@@ -72,13 +72,9 @@
       <h3>What is Critics Royale?</h3>
       <div class="text-well">
         <p>
-          Critics Royale is a new way to play Fantasy Critic - no league required. Every quarter, you will create a publisher that will compete against the entire site. Instead of drafting, you're
-          given a 'budget' which you will use to buy games that you believe will score well during that quarter. Your goal is to spend that money wisely and put together the best lineup of games that
-          you can.
-        </p>
-        <p>
-          For now, I'm calling this a beta because I will continue to expand upon this as time goes on. One of the advantages of running a new game every quarter instead of every year is that it will
-          allow me to iterate and improve the game design faster than the main site.
+          Critics Royale is an alternate way to play Fantasy Critic - no league required. Every quarter, you can create a publisher that will compete against the entire site. Instead of drafting,
+          you're given a 'budget' which you will use to buy games that you believe will score well during that quarter. Your goal is to spend that money wisely and put together the best lineup of
+          games that you can.
         </p>
       </div>
       <h3>How's it work?</h3>
