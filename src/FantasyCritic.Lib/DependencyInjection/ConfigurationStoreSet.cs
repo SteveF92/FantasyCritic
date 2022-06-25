6f85aac04b98abac4051eee4285ac52e66e29ce3
Stephen Fallon <sfallon92@gmail.com>
2022-06-25 14:30:25 -0400

    Being more clear about which store is the secret store.

--- src/FantasyCritic.Lib/DependencyInjection/ConfigurationStoreSet.cs
+++ src/FantasyCritic.Lib/DependencyInjection/ConfigurationStoreSet.cs
@@ -2,45 +2,45 @@ namespace FantasyCritic.Lib.DependencyInjection;
 
 public class ConfigurationStoreSet : IConfigurationStore
 {
-    private readonly IConfigurationStore _primaryStore;
-    private readonly IConfigurationStore _secondaryStore;
+    private readonly IConfigurationStore _normalStore;
+    private readonly IConfigurationStore _secretStore;
 
-    public ConfigurationStoreSet(IConfigurationStore primaryStore, IConfigurationStore secondaryStore)
+    public ConfigurationStoreSet(IConfigurationStore normalStore, IConfigurationStore secretStore)
     {
-        _primaryStore = primaryStore;
-        _secondaryStore = secondaryStore;
+        _normalStore = normalStore;
+        _secretStore = secretStore;
     }
 
     public string GetConfigValue(string name)
     {
-        var primaryStoreValue = _primaryStore.GetConfigValue(name);
+        var primaryStoreValue = _normalStore.GetConfigValue(name);
         if (primaryStoreValue != "secret")
         {
             return primaryStoreValue;
         }
 
-        return _secondaryStore.GetConfigValue(name);
+        return _secretStore.GetConfigValue(name);
     }
 
     public string GetConnectionString(string name)
     {
-        var primaryStoreValue = _primaryStore.GetConnectionString(name);
+        var primaryStoreValue = _normalStore.GetConnectionString(name);
         if (primaryStoreValue != "secret")
         {
             return primaryStoreValue;
         }
 
-        return _secondaryStore.GetConnectionString(name);
+        return _secretStore.GetConnectionString(name);
     }
 
     public string GetAWSRegion()
     {
-        var primaryStoreValue = _primaryStore.GetAWSRegion();
+        var primaryStoreValue = _normalStore.GetAWSRegion();
         if (primaryStoreValue != "secret")
         {
             return primaryStoreValue;
         }
 
-        return _secondaryStore.GetAWSRegion();
+        return _secretStore.GetAWSRegion();
     }
 }
