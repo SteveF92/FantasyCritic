11d803f0bf28d73b3c1fca2c32ecf2f6392a5c01
Stephen Fallon <sfallon92@gmail.com>
2022-07-30 21:14:28 -0400

    Update script.

--- linuxUpdateSite.sh
+++ linuxUpdateSite.sh
@@ -1,1 +1,14 @@
-dotnet publish src/FantasyCritic.Web/FantasyCritic.Web.csproj -c Release -o /var/www/fantasy-critic
+echo Building
+dotnet publish src/FantasyCritic.Web/FantasyCritic.Web.csproj -c Release -o BuildArea
+
+echo Build done, stopping service
+sudo systemctl stop fantasy-critic.service
+
+echo Deleting and moving files
+rm -r /var/www/fantasy-critic/*
+mv  -v BuildArea/* /var/www/fantasy-critic/
+rm -rf folderName BuildArea
+
+echo Starting site
+sudo systemctl start fantasy-critic.service
+echo Site started
