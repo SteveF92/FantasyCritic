d3b53546449740953a61fc831a08ea44b791ff1b
Robert <4060573+rarDevelopment@users.noreply.github.com>
2022-10-01 21:30:57 -0400

    refactored to take code out of the DiscordBot project

---
+++ src/FantasyCritic.Discord/Bot/DiscordBotService.cs
@@ -0,0 +1,88 @@
+using Discord.WebSocket;
+using Discord;
+using FantasyCritic.Discord.Commands;
+using NodaTime;
+using Discord.Net;
+using Newtonsoft.Json;
+using FantasyCritic.Lib.Interfaces;
+
+namespace FantasyCritic.Discord.Bot;
+public class DiscordBotService
+{
+    private readonly string _botToken;
+    private readonly IFantasyCriticRepo _mySQLFantasyCriticRepo;
+    private readonly IDiscordRepo _mySQLDiscordRepo;
+    private readonly SystemClock _systemClock;
+    private DiscordSocketClient _client = null!;
+    private List<ICommand> _commandsList = new();
+
+    public DiscordBotService(string botToken,
+        IFantasyCriticRepo mySQLFantasyCriticRepo,
+        IDiscordRepo mySQLDiscordRepo,
+        SystemClock systemClock)
+    {
+        _botToken = botToken;
+        _mySQLFantasyCriticRepo = mySQLFantasyCriticRepo;
+        _mySQLDiscordRepo = mySQLDiscordRepo;
+        _systemClock = systemClock;
+    }
+
+    public async Task InitializeBot()
+    {
+        _commandsList = new List<ICommand>
+        {
+            new GetLeagueCommand(_mySQLDiscordRepo, _mySQLFantasyCriticRepo, _systemClock),
+            new GetLeagueLinkCommand(_mySQLDiscordRepo, _systemClock)
+        };
+
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        _client.Ready += Client_Ready;
+        _client.SlashCommandExecuted += SlashCommandHandler;
+
+        await _client.LoginAsync(TokenType.Bot, _botToken);
+        await _client.StartAsync();
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    public async Task Client_Ready()
+    {
+        foreach (var command in _commandsList)
+        {
+            var globalCommand = new SlashCommandBuilder()
+                .WithName(command.Name)
+                .WithDescription(command.Description);
+
+            if (command.Options.Any())
+            {
+                foreach (var option in command.Options)
+                {
+                    globalCommand.AddOption(option);
+                }
+            }
+
+            try
+            {
+                await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+            }
+            catch (HttpException exception)
+            {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+    }
+    private async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        var commandToHandle = _commandsList.FirstOrDefault(c => c.Name == command.Data.Name);
+        if (commandToHandle != null)
+        {
+            await commandToHandle.HandleCommand(command);
+        }
+    }
+}
--- src/FantasyCritic.DiscordBot/Program.cs
+++ src/FantasyCritic.DiscordBot/Program.cs
@@ -1,23 +1,15 @@
 using System.Reflection;
-using Discord;
-using Discord.Net;
-using Discord.WebSocket;
-using FantasyCritic.Discord.Commands;
+using FantasyCritic.Discord.Bot;
 using FantasyCritic.Lib.DependencyInjection;
 using FantasyCritic.MySQL;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
 using NodaTime;
+using Dapper.NodaTime;
 
 namespace FantasyCritic.DiscordBot;
 
-using Dapper.NodaTime;
-using System;
-
 public class Program
 {
-    private DiscordSocketClient _client = null!;
-    private List<ICommand> _commandsList = new();
 
     public static Task Main() => new Program().MainAsync();
     public async Task MainAsync()
@@ -27,74 +19,20 @@ public class Program
             .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
             .Build();
 
+        // Bot Dependencies
         var botToken = configuration["BotToken"];
         var repositoryConfiguration = new RepositoryConfiguration(configuration["ConnectionString"], SystemClock.Instance);
-
         var userStore = new MySQLFantasyCriticUserStore(repositoryConfiguration);
         var masterGameRepo = new MySQLMasterGameRepo(repositoryConfiguration, userStore);
         var fantasyCriticRepo = new MySQLFantasyCriticRepo(repositoryConfiguration, userStore, masterGameRepo);
         var discordRepo = new MySQLDiscordRepo(repositoryConfiguration, fantasyCriticRepo);
         var clock = SystemClock.Instance;
 
-        _commandsList = new List<ICommand>
-        {
-            new GetLeagueCommand(discordRepo, fantasyCriticRepo, clock),
-            new GetLeagueLinkCommand(discordRepo, clock)
-        };
-
         DapperNodaTimeSetup.Register();
 
-        _client = new DiscordSocketClient();
-        _client.Log += Log;
-        _client.Ready += Client_Ready;
-        _client.SlashCommandExecuted += SlashCommandHandler;
-
-        await _client.LoginAsync(TokenType.Bot, botToken);
-        await _client.StartAsync();
+        var discordBotService = new DiscordBotService(botToken, fantasyCriticRepo, discordRepo, clock);
+        await discordBotService.InitializeBot();
 
         await Task.Delay(-1);
-    }
-
-    private static Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
-
-    public async Task Client_Ready()
-    {
-        foreach (var command in _commandsList)
-        {
-            var globalCommand = new SlashCommandBuilder()
-                .WithName(command.Name)
-                .WithDescription(command.Description);
-
-            if (command.Options.Any())
-            {
-                foreach (var option in command.Options)
-                {
-                    globalCommand.AddOption(option);
-                }
-            }
-
-            try
-            {
-                await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
-            }
-            catch (HttpException exception)
-            {
-                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
-                Console.WriteLine(json);
-            }
-        }
-    }
-
-    private async Task SlashCommandHandler(SocketSlashCommand command)
-    {
-        var commandToHandle = _commandsList.FirstOrDefault(c => c.Name == command.Data.Name);
-        if (commandToHandle != null)
-        {
-            await commandToHandle.HandleCommand(command);
-        }
     }
 }
