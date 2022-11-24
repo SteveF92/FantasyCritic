6d7e49dbee63572376883f6f8b4cbada9adab7b2
Stephen Fallon <sfallon92@gmail.com>
2022-11-24 13:03:47 -0500

    Implemented SQL function.

--- src/FantasyCritic.MySQL/MySQLDiscordSupplementalDataRepo.cs
+++ src/FantasyCritic.MySQL/MySQLDiscordSupplementalDataRepo.cs
@@ -11,8 +11,26 @@ public class MySQLDiscordSupplementalDataRepo : IDiscordSupplementalDataRepo
         _connectionString = configuration.ConnectionString;
     }
 
-    public Task<bool> GameInLeagueOrFormerlyInLeague(MasterGameYear masterGameYear, Guid leagueID)
+    public async Task<bool> GameInLeagueOrFormerlyInLeague(MasterGameYear masterGameYear, Guid leagueID)
     {
-        throw new NotImplementedException();
+        await using var connection = new MySqlConnection(_connectionString);
+        var queryObject = new
+        {
+            masterGameYear.MasterGame.MasterGameID,
+            masterGameYear.Year,
+            LeagueID = leagueID
+        };
+
+        string sql = "SELECT DISTINCT SubQuery.GameExists FROM " +
+                     "(SELECT 1 AS GameExists FROM tbl_league_publisher " +
+                     "JOIN tbl_league_publishergame ON tbl_league_publisher.PublisherID = tbl_league_publishergame.PublisherID " +
+                     "WHERE LeagueID = @LeagueID AND YEAR = @Year AND MasterGameID = @MasterGameID " +
+                     "UNION " +
+                     "SELECT 1 AS GameExists FROM tbl_league_publisher " +
+                     "JOIN tbl_league_formerpublishergame ON tbl_league_publisher.PublisherID = tbl_league_formerpublishergame.PublisherID " +
+                     "WHERE LeagueID = @LeagueID AND YEAR = @Year AND MasterGameID = @MasterGameID) AS SubQuery";
+
+        var result = await connection.QuerySingleOrDefaultAsync<int?>(sql, queryObject);
+        return result is not null;
     }
 }
