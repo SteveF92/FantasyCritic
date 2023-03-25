6d08e60ba599ab3e865124aee2072273303c2ff0
Stephen Fallon <sfallon92@gmail.com>
2023-03-25 13:39:26 -0400

    Most of the changes to convert to Vite

--- src/FantasyCritic.Web/ClientApp/.eslintrc.js
+++ src/FantasyCritic.Web/ClientApp/.eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-},
-  extends: [
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": ["off"],
-    "vue/valid-v-slot": ["error", {
-      "allowModifiers": true
-    }]
-  },
-  globals: {
-    "_": "writable"
-  }
+    root: true,
+    env: {
+        es2021: true,
+    },
+    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "vue/multi-word-component-names" : "off",
+        "vue/valid-v-slot": ["error", {
+        "allowModifiers": true
+        }]
+    },
+    globals: {
+        "_": "readonly",
+        "__dirname": "readonly"
+    }
 };
