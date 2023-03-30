4d9884b4545c96d29c2f212a23def8ef7e60234c
Stephen Fallon <sfallon92@gmail.com>
2023-03-29 22:35:37 -0400

    Updated eslint rules.

--- src/FantasyCritic.Web/ClientApp/.eslintrc.js
+++ src/FantasyCritic.Web/ClientApp/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     env: {
         es2021: true,
     },
-    extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+    extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
