b778c6cdd38dc903ae5dc64f2cac0f61255d827e
Stephen Fallon <sfallon92@gmail.com>
2023-04-01 13:52:31 -0400

    Attempting to set up the launch profiles better.

--- src/FantasyCritic.Web/ClientApp/vite.config.js
+++ src/FantasyCritic.Web/ClientApp/vite.config.js
@@ -15,7 +15,7 @@ const target = process.env.ASPNETCORE_HTTPS_PORT
   ? `https://localhost:${process.env.ASPNETCORE_HTTPS_PORT}`
   : process.env.ASPNETCORE_URLS
   ? process.env.ASPNETCORE_URLS.split(';')[0]
-  : 'http://localhost:44391';
+  : 'https://localhost:4554';
 
 export default defineConfig({
   plugins: [vue()],
