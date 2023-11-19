c530a8fb739d591f08b217250c49ae2428702e3e
Stephen Fallon <sfallon92@gmail.com>
2023-11-19 13:09:31 -0500

    Stubbing other functions. Does not build

--- src/FantasyCritic.Web/Controllers/API/ConferenceController.cs
+++ src/FantasyCritic.Web/Controllers/API/ConferenceController.cs
@@ -1,7 +1,6 @@
-using FantasyCritic.Lib.Domain.Requests;
 using FantasyCritic.Lib.Identity;
-using FantasyCritic.Lib.Services;
 using FantasyCritic.Web.Models.Requests.Conference;
+using FantasyCritic.Web.Models.Requests.League;
 using FantasyCritic.Web.Models.Requests.LeagueManager;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -34,6 +33,109 @@ public class ConferenceController : FantasyCriticController
             return BadRequest(requestValid.Error);
         }
 
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> EditConference([FromBody] EditConferenceRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetConference(int id)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> CreateInviteLink([FromBody] CreateInviteLinkRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> DeleteInviteLink([FromBody] DeleteInviteLinkRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> JoinWithInviteLink([FromBody] JoinWithInviteLinkRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> AddLeagueToConference([FromBody] AddLeagueToConferenceRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> PromoteNewConferenceManager([FromBody] PromoteNewConferenceManagerRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> ReassignLeagueManager([FromBody] ReassignLeagueManagerRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> AssignLeaguePlayers([FromBody] AssignLeaguePlayersRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> EditDraftStatusForConferenceYear([FromBody] EditDraftStatusForConferenceYearRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> CreateConferenceManagerMessage([FromBody] CreateConferenceManagerMessageRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> DeleteConferenceManagerMessage([FromBody] DeleteConferenceManagerMessageRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> DismissManagerMessage([FromBody] DismissManagerMessageRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
         throw new NotImplementedException();
     }
 }
