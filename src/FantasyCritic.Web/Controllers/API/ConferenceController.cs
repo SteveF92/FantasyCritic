aef907ee620b17eaf26ca94225a7bf6a3d401855
Stephen Fallon <sfallon92@gmail.com>
2023-11-19 13:05:18 -0500

    First conference request stubbed.

--- src/FantasyCritic.Web/Controllers/API/ConferenceController.cs
+++ src/FantasyCritic.Web/Controllers/API/ConferenceController.cs
@@ -1,4 +1,8 @@
+using FantasyCritic.Lib.Domain.Requests;
 using FantasyCritic.Lib.Identity;
+using FantasyCritic.Lib.Services;
+using FantasyCritic.Web.Models.Requests.Conference;
+using FantasyCritic.Web.Models.Requests.LeagueManager;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,4 +22,18 @@ public class ConferenceController : FantasyCriticController
         _logger = logger;
     }
 
-  }
+    [HttpPost]
+    [Authorize("Write")]
+    public async Task<IActionResult> CreateConference([FromBody] CreateConferenceRequest request)
+    {
+        var currentUser = await GetCurrentUserOrThrow();
+
+        var requestValid = request.IsValid();
+        if (requestValid.IsFailure)
+        {
+            return BadRequest(requestValid.Error);
+        }
+
+        throw new NotImplementedException();
+    }
+}
