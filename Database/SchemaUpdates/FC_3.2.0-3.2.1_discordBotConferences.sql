ca5c009860a8b4f408ad5ae348feb199f95deead
Stephen Fallon <sfallon92@gmail.com>
2024-01-15 22:50:15 -0500

    Removing explicit collation from SQL script.

--- Database/SchemaUpdates/FC_3.2.0-3.2.1_discordBotConferences.sql
+++ Database/SchemaUpdates/FC_3.2.0-3.2.1_discordBotConferences.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `fantasycritic`.`tbl_discord_conferencechannel` (
 	`GuildID` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
 	`ChannelID` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
-	`ConferenceID` CHAR(36) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`ConferenceID` CHAR(36) NOT NULL,
 	PRIMARY KEY (`GuildID`, `ChannelID`) USING BTREE,
 	INDEX `FK_ConferenceID` (`ConferenceID`) USING BTREE,
 	FOREIGN KEY (`ConferenceID`) REFERENCES `tbl_conference` (`ConferenceID`) ON UPDATE NO ACTION ON DELETE NO ACTION
