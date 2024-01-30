034f66e9e274c1978ddb5a6997c43ea20f686044
Stephen Fallon <sfallon92@gmail.com>
2024-01-29 22:33:38 -0500

    Fixing conditional drop messages.

--- src/FantasyCritic.Lib/Discord/Utilities/DiscordSharedMessageUtilities.cs
+++ src/FantasyCritic.Lib/Discord/Utilities/DiscordSharedMessageUtilities.cs
@@ -178,9 +178,18 @@ public static class DiscordSharedMessageUtilities
         if (bid.Successful!.Value)
         {
             message += $"- Won by {bid.Publisher.GetPublisherAndUserDisplayName()} with a bid of ${bid.BidAmount} {counterPickMessage}";
-            if (bid is { ConditionalDropResult.Result.IsSuccess: true, ConditionalDropPublisherGame: not null })
+            if (bid.ConditionalDropPublisherGame is not null)
             {
-                message += $"Dropped game '{bid.ConditionalDropPublisherGame.GameName}' conditionally.";
+                if (!bid.Outcome!.Contains("Attempted to conditionally drop"))
+                {
+                    message += $"\n\t Dropped game '{bid.ConditionalDropPublisherGame.GameName}' conditionally";
+                }
+                else
+                {
+                    int startIndex = bid.Outcome.IndexOf("Attempted to conditionally drop", StringComparison.Ordinal);
+                    string conditionalDropOutcome = bid.Outcome.Substring(startIndex).TrimEnd('.');
+                    message += $"\n\t {conditionalDropOutcome}";
+                }
             }
         }
         else
