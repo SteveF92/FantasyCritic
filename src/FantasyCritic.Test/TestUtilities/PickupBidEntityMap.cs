6363e9044780218e8fdda6ec1d654abd07f959f6
Stephen Fallon <sfallon92@gmail.com>
2024-06-23 13:57:11 -0400

    Fixed pickup bid timestamp mapping.

--- src/FantasyCritic.Test/TestUtilities/PickupBidEntityMap.cs
+++ src/FantasyCritic.Test/TestUtilities/PickupBidEntityMap.cs
@@ -1,21 +1,26 @@
 using System.Globalization;
 using CsvHelper.Configuration;
 using FantasyCritic.SharedSerialization.Database;
+using NodaTime.Text;
 
 namespace FantasyCritic.Test.TestUtilities;
 public sealed class PreAllowIneligibleSlotPickupBidEntityMap : ClassMap<PickupBidEntity>
 {
+    private static readonly InstantPattern pattern = InstantPattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
     public PreAllowIneligibleSlotPickupBidEntityMap()
     {
         AutoMap(CultureInfo.InvariantCulture);
         Map(m => m.AllowIneligibleSlot).Constant(true);
+        Map(m => m.Timestamp).Convert(args => pattern.Parse(args.Row.GetField("Timestamp")).GetValueOrThrow());
     }
 }
 
 public sealed class PickupBidEntityMap : ClassMap<PickupBidEntity>
 {
+    private static readonly InstantPattern pattern = InstantPattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
     public PickupBidEntityMap()
     {
         AutoMap(CultureInfo.InvariantCulture);
+        Map(m => m.Timestamp).Convert(args => pattern.Parse(args.Row.GetField("Timestamp")).GetValueOrThrow());
     }
 }
