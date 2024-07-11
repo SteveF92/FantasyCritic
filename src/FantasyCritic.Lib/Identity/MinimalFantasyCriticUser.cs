b86117fcb61ccd4f191b74e41245b1af86cf1043
Stephen Fallon <sfallon92@gmail.com>
2024-07-11 08:26:54 -0400

    Changing the way equality works for users, so that we can check equality regardless of type.

--- src/FantasyCritic.Lib/Identity/MinimalFantasyCriticUser.cs
+++ src/FantasyCritic.Lib/Identity/MinimalFantasyCriticUser.cs
@@ -1,14 +1,41 @@
 namespace FantasyCritic.Lib.Identity;
 
-public record MinimalFantasyCriticUser(Guid UserID, string DisplayName, string EmailAddress) : IMinimalFantasyCriticUser;
-public record VeryMinimalFantasyCriticUser(Guid UserID, string DisplayName) : IVeryMinimalFantasyCriticUser;
+public record MinimalFantasyCriticUser(Guid UserID, string DisplayName, string EmailAddress) : IMinimalFantasyCriticUser
+{
+    public virtual bool Equals(IVeryMinimalFantasyCriticUser? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return UserID.Equals(other.UserID);
+    }
+
+    public override int GetHashCode()
+    {
+        return UserID.GetHashCode();
+    }
+}
+
+public record VeryMinimalFantasyCriticUser(Guid UserID, string DisplayName) : IVeryMinimalFantasyCriticUser
+{
+    public virtual bool Equals(IVeryMinimalFantasyCriticUser? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return UserID.Equals(other.UserID);
+    }
+
+    public override int GetHashCode()
+    {
+        return UserID.GetHashCode();
+    }
+}
 
 public interface IMinimalFantasyCriticUser : IVeryMinimalFantasyCriticUser
 {
     string EmailAddress { get; }
 }
 
-public interface IVeryMinimalFantasyCriticUser
+public interface IVeryMinimalFantasyCriticUser : IEquatable<IVeryMinimalFantasyCriticUser>
 {
     Guid UserID { get; }
     string DisplayName { get; }
