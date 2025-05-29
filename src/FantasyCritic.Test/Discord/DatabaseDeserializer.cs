121308354dfc7aa4f270cc9092e5a13231b257f2
Stephen Fallon <sfallon92@gmail.com>
2025-05-28 22:17:33 -0400

    Translating settings from old database structure.

--- src/FantasyCritic.Test/Discord/DatabaseDeserializer.cs
+++ src/FantasyCritic.Test/Discord/DatabaseDeserializer.cs
@@ -1,19 +1,46 @@
 using System.Collections.Generic;
+using FantasyCritic.Lib.Discord.Handlers;
 using FantasyCritic.Lib.Discord.Models;
 using FantasyCritic.Lib.Domain;
 
 namespace FantasyCritic.Test.Discord;
 internal static class DatabaseDeserializer
 {
-    public static OriginalDatabaseStructure TransformToNewStructure(OriginalDatabaseStructure structure)
+    public static BaseGameNewsRelevanceHandler GetCombinedChannelGameSetting(OriginalDatabaseStructure structure, LeagueYear? leagueYear = null)
     {
-        return structure;
-    }
+        var translatedSetting = GameNewsSetting.GetOffSetting();
+        var hasGameChannel = structure.GameChannel is not null;
+        if (hasGameChannel)
+        {
+            translatedSetting = new GameNewsSetting(){
+                ShowJustReleasedAnnouncements = true,
+                ShowNewGameAnnouncements = true,
+                ShowAlreadyReleasedNews = true,
+                ShowWillReleaseInYearNews = true,
+                ShowMightReleaseInYearNews = structure.GameChannel!.GameNewsSetting == "All" ||
+                                             structure.GameChannel.GameNewsSetting == "Might",
+                ShowWillNotReleaseInYearNews = structure.GameChannel.GameNewsSetting == "All",
+                ShowScoreGameNews = true,
+                ShowEditedGameNews = true
+            };
+        }
 
-    public static CombinedChannelGameSetting GetCombinedChannelGameSetting(OriginalDatabaseStructure structure)
-    {
-        return new CombinedChannelGameSetting(structure.LeagueChannel?.SendLeagueMasterGameUpdates ?? false, structure.LeagueChannel?.SendNotableMisses ?? false,
-            GameNewsSetting.FromValue(structure.GameChannel?.GameNewsSetting ?? "Off"), structure.SkippedTags);
+        if (structure.LeagueChannel is not null)
+        {
+            var leagueYearList = new List<LeagueYear>();
+            if (leagueYear is not null)
+            {
+                leagueYearList.Add(leagueYear);
+            }
+            var notableMissSetting = structure.LeagueChannel.SendNotableMisses
+                ? NotableMissSetting.ScoreUpdates
+                : NotableMissSetting.None;
+            var showIneligibleGameNews = structure.GameChannel?.GameNewsSetting == "All";
+            return new LeagueGameNewsRelevanceHandler(structure.LeagueChannel.SendLeagueMasterGameUpdates, hasGameChannel, showIneligibleGameNews, notableMissSetting, translatedSetting, structure.SkippedTags,
+                new DiscordChannelKey(0, 0), leagueYearList);
+        }
+
+        return new GameNewsOnlyRelevanceHandler(translatedSetting, structure.SkippedTags, new DiscordChannelKey(0, 0));
     }
 }
 
