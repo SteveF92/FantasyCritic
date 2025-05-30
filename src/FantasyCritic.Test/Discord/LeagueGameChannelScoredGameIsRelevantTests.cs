de2f4afd94f78b6a2ee1c4c1b0e2f5a5988d8770
Stephen Fallon <sfallon92@gmail.com>
2025-05-29 21:51:41 -0400

    Flipping some tests.

--- src/FantasyCritic.Test/Discord/LeagueGameChannelScoredGameIsRelevantTests.cs
+++ src/FantasyCritic.Test/Discord/LeagueGameChannelScoredGameIsRelevantTests.cs
@@ -365,10 +365,9 @@ internal class LeagueGameChannelScoredGameIsRelevantTests : LeagueGameNewsChanne
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_On_Misses_Off_SettingOff_NoSkippedTags_HasGame_Score90() =>
         Assert.That(LeagueGames_On_Misses_Off_SettingOff_NoSkippedTags(GetTestLeagueYear(true)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
 
-    //This should not exist, there should not be a way for the user to have turned on notable miss settings, turn off game news, turn off league games
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_On_SettingOff_NoSkippedTags_HasGame_Score90() =>
-        Assert.That(LeagueGames_Off_Misses_On_SettingOff_NoSkippedTags(GetTestLeagueYear(true)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
+        Assert.That(LeagueGames_Off_Misses_On_SettingOff_NoSkippedTags(GetTestLeagueYear(true)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
 
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_Off_SettingOff_NoSkippedTags_HasGame_Score90() =>
@@ -382,10 +381,9 @@ internal class LeagueGameChannelScoredGameIsRelevantTests : LeagueGameNewsChanne
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_On_Misses_Off_SettingOff_SkipUNA_HasGame_Score90() =>
         Assert.That(LeagueGames_On_Misses_Off_SettingOff_SkipUNA(GetTestLeagueYear(true)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
 
-    //This should not exist, there should not be a way for the user to have turned on notable miss settings, turn off game news, turn off league games
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_On_SettingOff_SkipUNA_HasGame_Score90() =>
-        Assert.That(LeagueGames_Off_Misses_On_SettingOff_SkipUNA(GetTestLeagueYear(true)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
+        Assert.That(LeagueGames_Off_Misses_On_SettingOff_SkipUNA(GetTestLeagueYear(true)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
 
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_Off_SettingOff_SkipUNA_HasGame_Score90() =>
@@ -495,10 +493,9 @@ internal class LeagueGameChannelScoredGameIsRelevantTests : LeagueGameNewsChanne
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_On_Misses_Off_SettingOff_NoSkippedTags_DoesNotHaveGame_Score90() =>
         Assert.That(LeagueGames_On_Misses_Off_SettingOff_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
 
-    //This should not exist, there should not be a way for the user to have turned on notable miss settings, turn off game news, turn off league games
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_On_SettingOff_NoSkippedTags_DoesNotHaveGame_Score90() =>
-        Assert.That(LeagueGames_Off_Misses_On_SettingOff_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
+        Assert.That(LeagueGames_Off_Misses_On_SettingOff_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
 
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_Off_SettingOff_NoSkippedTags_DoesNotHaveGame_Score90() =>
@@ -515,7 +512,7 @@ internal class LeagueGameChannelScoredGameIsRelevantTests : LeagueGameNewsChanne
     //This should not exist, there should not be a way for the user to have turned on notable miss settings, turn off game news, turn off league games
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_On_SettingOff_SkipUNA_DoesNotHaveGame_Score90() =>
-        Assert.That(LeagueGames_Off_Misses_On_SettingOff_SkipUNA(GetTestLeagueYear(false)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
+        Assert.That(LeagueGames_Off_Misses_On_SettingOff_SkipUNA(GetTestLeagueYear(false)).ScoredGameIsRelevant(Eligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
 
     [Test]
     public void ScoredGame_Eligible_ReleasedToday_LeagueGames_Off_Misses_Off_SettingOff_SkipUNA_DoesNotHaveGame_Score90() =>
@@ -723,11 +720,11 @@ internal class LeagueGameChannelScoredGameIsRelevantTests : LeagueGameNewsChanne
 
     [Test]
     public void ScoredGame_Ineligible_ReleasedToday_LeagueGames_Off_Misses_On_SettingMightReleaseInYear_NoSkippedTags_DoesNotHaveGame_Score75() =>
-        Assert.That(LeagueGames_Off_Misses_On_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 75m, CurrentDateForTesting), Is.True);
+        Assert.That(LeagueGames_Off_Misses_On_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 75m, CurrentDateForTesting), Is.False);
 
     [Test]
     public void ScoredGame_Ineligible_ReleasedToday_LeagueGames_Off_Misses_Off_SettingMightReleaseInYear_NoSkippedTags_DoesNotHaveGame_Score75() =>
-        Assert.That(LeagueGames_Off_Misses_Off_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 75m, CurrentDateForTesting), Is.True);
+        Assert.That(LeagueGames_Off_Misses_Off_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 75m, CurrentDateForTesting), Is.False);
 
     [Test]
     public void ScoredGame_Ineligible_ReleasedToday_LeagueGames_On_Misses_On_SettingMightReleaseInYear_SkipUNA_DoesNotHaveGame_Score75() =>
@@ -979,11 +976,11 @@ internal class LeagueGameChannelScoredGameIsRelevantTests : LeagueGameNewsChanne
 
     [Test]
     public void ScoredGame_Ineligible_ReleasedToday_LeagueGames_Off_Misses_On_SettingMightReleaseInYear_NoSkippedTags_DoesNotHaveGame_Score90() =>
-        Assert.That(LeagueGames_Off_Misses_On_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
+        Assert.That(LeagueGames_Off_Misses_On_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
 
     [Test]
     public void ScoredGame_Ineligible_ReleasedToday_LeagueGames_Off_Misses_Off_SettingMightReleaseInYear_NoSkippedTags_DoesNotHaveGame_Score90() =>
-        Assert.That(LeagueGames_Off_Misses_Off_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.True);
+        Assert.That(LeagueGames_Off_Misses_Off_SettingMightReleaseInYear_NoSkippedTags(GetTestLeagueYear(false)).ScoredGameIsRelevant(Ineligible_ReleasedToday, null, 90m, CurrentDateForTesting), Is.False);
 
     [Test]
     public void ScoredGame_Ineligible_ReleasedToday_LeagueGames_On_Misses_On_SettingMightReleaseInYear_SkipUNA_DoesNotHaveGame_Score90() =>
