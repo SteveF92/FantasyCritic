b43a3e15d87d0c029c1785f59f64e90d45949ce7
Stephen Fallon <sfallon92@gmail.com>
2025-12-08 21:56:36 -0500

    Fixing another stored procedure.

--- Database/SchemaUpdates/FC_5.1.0-5.2.0_defaultLeagueYear.sql
+++ Database/SchemaUpdates/FC_5.1.0-5.2.0_defaultLeagueYear.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.28 - MySQL Community Server - GPL
--- Server OS:                    Win64
+-- Host:                         fantasy-critic-rds.cldutembgs4w.us-east-1.rds.amazonaws.com
+-- Server version:               8.0.31 - Source distribution
+-- Server OS:                    Linux
 -- HeidiSQL Version:             12.3.0.6589
 -- --------------------------------------------------------
 
@@ -332,6 +332,132 @@ BEGIN
 	FROM tbl_league_formerpublishergame
 	JOIN tbl_league_publisher ON (tbl_league_formerpublishergame.PublisherID = tbl_league_publisher.PublisherID)
 	WHERE tbl_league_publisher.LeagueID = P_LeagueID
+	  AND tbl_league_publisher.Year = P_Year;
+	
+	-- Master Game Data
+	
+	SELECT tbl_mastergame.*,
+	       tbl_user.DisplayName AS AddedByUserDisplayName
+	FROM tbl_mastergame
+	JOIN tbl_user ON tbl_user.UserID = tbl_mastergame.AddedByUserID;
+	
+	
+	SELECT *
+	FROM tbl_mastergame_tag;
+	
+	
+	SELECT *
+	FROM tbl_mastergame_subgame;
+	
+	
+	SELECT *
+	FROM tbl_mastergame_hastag;
+	
+	
+	SELECT tbl_caching_mastergameyear.*,
+	       tbl_user.DisplayName AS AddedByUserDisplayName
+	FROM tbl_caching_mastergameyear
+	JOIN tbl_user ON tbl_user.UserID = tbl_caching_mastergameyear.AddedByUserID
+	WHERE tbl_caching_mastergameyear.`Year` = P_Year;
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure fantasycritic.sp_getleagueyearsforconferenceyear
+DROP PROCEDURE IF EXISTS `sp_getleagueyearsforconferenceyear`;
+DELIMITER //
+CREATE PROCEDURE `sp_getleagueyearsforconferenceyear`(
+	IN `P_ConferenceID` CHAR(36),
+	IN `P_Year` INT
+)
+BEGIN
+	-- League
+	
+	SELECT *
+	FROM vw_league
+	WHERE ConferenceID = P_ConferenceID
+	  AND IsDeleted = 0;
+	
+	
+	SELECT tbl_league.LeagueID, 
+	tbl_league_year.`YEAR`,
+	tbl_meta_supportedyear.Finished AS "SupportedYearIsFinished",
+	tbl_league_year.PlayStatus
+	FROM tbl_league_year
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_year.LeagueID
+	JOIN tbl_meta_supportedyear ON tbl_meta_supportedyear.`Year` = tbl_league_year.`Year`
+	WHERE ConferenceID = P_ConferenceID;
+	
+	-- League Year
+	
+	SELECT *
+	FROM tbl_league_year
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_year.LeagueID
+	WHERE ConferenceID = P_ConferenceID
+	  AND YEAR = P_Year;
+	
+	
+	SELECT *
+	FROM tbl_league_yearusestag
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_yearusestag.LeagueID
+	WHERE ConferenceID = P_ConferenceID
+	  AND YEAR = P_Year;
+	
+	
+	SELECT *
+	FROM tbl_league_specialgameslot
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_specialgameslot.LeagueID
+	WHERE ConferenceID = P_ConferenceID
+	  AND YEAR = P_Year;
+	
+	
+	SELECT *
+	FROM tbl_league_eligibilityoverride
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_eligibilityoverride.LeagueID
+	WHERE ConferenceID = P_ConferenceID
+	  AND YEAR = P_Year;
+	
+	
+	SELECT tbl_league_tagoverride.*
+	FROM tbl_league_tagoverride
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_tagoverride.LeagueID
+	JOIN tbl_mastergame_tag ON tbl_league_tagoverride.TagName = tbl_mastergame_tag.Name
+	WHERE ConferenceID = P_ConferenceID
+	  AND YEAR = P_Year;
+	
+	-- Publishers
+	
+	SELECT tbl_user.*
+	FROM tbl_user
+	JOIN tbl_league_hasuser ON (tbl_user.UserID = tbl_league_hasuser.UserID)
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_hasuser.LeagueID
+	WHERE tbl_league.ConferenceID = P_ConferenceID
+	UNION
+	SELECT tbl_user.*
+	FROM tbl_user
+	JOIN tbl_league ON (tbl_user.UserID = tbl_league.LeagueManager)
+	WHERE tbl_league.ConferenceID = P_ConferenceID;
+	
+	
+	SELECT *
+	FROM tbl_league_publisher
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_publisher.LeagueID
+	WHERE tbl_league.ConferenceID = P_ConferenceID
+	  AND tbl_league_publisher.Year = P_Year;
+	
+	
+	SELECT tbl_league_publishergame.*
+	FROM tbl_league_publishergame
+	JOIN tbl_league_publisher ON (tbl_league_publishergame.PublisherID = tbl_league_publisher.PublisherID)
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_publisher.LeagueID
+	WHERE tbl_league.ConferenceID = P_ConferenceID
+	  AND tbl_league_publisher.Year = P_Year;
+	
+	
+	SELECT tbl_league_formerpublishergame.*
+	FROM tbl_league_formerpublishergame
+	JOIN tbl_league_publisher ON (tbl_league_formerpublishergame.PublisherID = tbl_league_publisher.PublisherID)
+	JOIN tbl_league on tbl_league.LeagueID = tbl_league_publisher.LeagueID
+	WHERE tbl_league.ConferenceID = P_ConferenceID
 	  AND tbl_league_publisher.Year = P_Year;
 	
 	-- Master Game Data
