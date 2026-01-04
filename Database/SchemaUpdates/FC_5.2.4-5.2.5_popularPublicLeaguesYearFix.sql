e0daf1cbd1ff1f26bd65222ddd8f6805b69d904c
Stephen Fallon <sfallon92@gmail.com>
2026-01-03 22:10:31 -0500

    Fixing SQL script

--- Database/SchemaUpdates/FC_5.2.4-5.2.5_popularPublicLeaguesYearFix.sql
+++ Database/SchemaUpdates/FC_5.2.4-5.2.5_popularPublicLeaguesYearFix.sql
@@ -1,3 +1,24 @@
+-- --------------------------------------------------------
+-- Host:                         fantasy-critic-rds.cldutembgs4w.us-east-1.rds.amazonaws.com
+-- Server version:               8.0.31 - Source distribution
+-- Server OS:                    Linux
+-- HeidiSQL Version:             12.3.0.6589
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for procedure fantasycritic.sp_gethomepagedata
+DELIMITER //
+CREATE PROCEDURE `sp_gethomepagedata`(
+	IN `P_UserID` CHAR(36)
+)
 BEGIN 
 	DECLARE userEmailAddress VARCHAR(255);
 	DECLARE maxProcessDate DATE;
@@ -161,4 +182,11 @@ BEGIN
 	  AND YEAR = highestRoyaleYear
 	  AND QUARTER = highestRoyaleQuarter;
 
-END
+END//
+DELIMITER ;
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
