caa994226f6fb9cd8141494fcb89bdbb9653e353
Stephen Fallon <sfallon92@gmail.com>
2026-01-04 21:18:36 -0500

    Updating mysql collation.

---
+++ Database/SchemaUpdates/FC_5.2.6-6.0.0_updateCollation.sql
@@ -0,0 +1,9 @@
+ALTER DATABASE fantasycritic
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_0900_ai_ci;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+--Use HeidiSQL to update collation and charset
+
+SET FOREIGN_KEY_CHECKS=1;
