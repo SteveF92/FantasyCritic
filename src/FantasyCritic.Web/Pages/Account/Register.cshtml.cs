de82eb2e4a6ccfecb6c85e1f07b5dc6ef89a2778
Stephen Fallon <sfallon92@gmail.com>
2026-01-25 00:36:44 -0500

    Added a "confirm email" box to prevent people from giving the wrong email address.

--- src/FantasyCritic.Web/Pages/Account/Register.cshtml.cs
+++ src/FantasyCritic.Web/Pages/Account/Register.cshtml.cs
@@ -50,6 +50,12 @@ public class RegisterModel : PageModel
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [EmailAddress]
+        [Display(Name = "Confirm Email")]
+        [Compare("Email", ErrorMessage = "The email addresses given do not match.")]
+        public string ConfirmEmail { get; set; }
+
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
@@ -57,7 +63,7 @@ public class RegisterModel : PageModel
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
