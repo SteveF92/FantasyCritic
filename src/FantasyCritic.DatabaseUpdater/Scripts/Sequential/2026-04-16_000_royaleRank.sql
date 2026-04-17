8d1966d7646190805b3ffa8116c15d805f5f0200
Stephen Fallon <sfallon92@gmail.com>
2026-04-17 15:16:38 -0400

    Better ranking.

--- src/FantasyCritic.DatabaseUpdater/Scripts/Sequential/2026-04-16_000_royaleRank.sql
+++ src/FantasyCritic.DatabaseUpdater/Scripts/Sequential/2026-04-16_000_royaleRank.sql
@@ -1,7 +1,11 @@
 ALTER TABLE `tbl_royale_publisher`
 	ADD COLUMN `Ranking` INT NULL AFTER `Budget`;
 
+UPDATE tbl_royale_publisher rp SET rp.Ranking = NULL;
 UPDATE tbl_royale_publisher rp
+JOIN tbl_royale_supportedquarter rsq
+    ON rp.`Year` = rsq.`Year`
+   AND rp.`Quarter` = rsq.`Quarter`
 LEFT JOIN (
     SELECT
         ranked.`Year`,
@@ -21,21 +25,19 @@ LEFT JOIN (
         JOIN tbl_royale_publishergame rpg
             ON rp2.PublisherID = rpg.PublisherID
            AND rpg.FantasyPoints IS NOT NULL
-        JOIN tbl_royale_supportedquarter rsq
-            ON rp2.`Year` = rsq.`Year`
-           AND rp2.`Quarter` = rsq.`Quarter`
-        WHERE rsq.Finished = 1
+        JOIN tbl_royale_supportedquarter rsq2
+            ON rp2.`Year` = rsq2.`Year`
+           AND rp2.`Quarter` = rsq2.`Quarter`
+        WHERE rsq2.Finished = 1
         GROUP BY
             rp2.`Year`,
             rp2.`Quarter`,
             rp2.PublisherID
+        HAVING SUM(rpg.FantasyPoints) > 0
     ) ranked
 ) r
     ON rp.`Year` = r.`Year`
    AND rp.`Quarter` = r.`Quarter`
    AND rp.PublisherID = r.PublisherID
-JOIN tbl_royale_supportedquarter rsq
-    ON rp.`Year` = rsq.`Year`
-   AND rp.`Quarter` = rsq.`Quarter`
 SET rp.Ranking = r.CalculatedRank
 WHERE rsq.Finished = 1;
