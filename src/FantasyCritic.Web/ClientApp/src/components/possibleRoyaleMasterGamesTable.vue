9a23f9cd58c204e4655737a10f9b55945293a14a
Stephen Fallon <sfallon92@gmail.com>
2026-04-19 23:04:50 -0400

    Avoiding a javascript console error

--- src/FantasyCritic.Web/ClientApp/src/components/possibleRoyaleMasterGamesTable.vue
+++ src/FantasyCritic.Web/ClientApp/src/components/possibleRoyaleMasterGamesTable.vue
@@ -37,7 +37,7 @@ export default {
     MasterGamePopover
   },
   props: {
-    value: { type: Object, required: true },
+    value: { type: Object },
     possibleGames: { type: Array, required: true }
   },
   data() {
