467844ab997ef69f06a89d0465f32238284623c1
Stephen Fallon <sfallon92@gmail.com>
2026-04-21 00:38:01 -0400

    Updating announcement date.

--- src/FantasyCritic.Web/ClientApp/src/data/siteAnnouncements.ts
+++ src/FantasyCritic.Web/ClientApp/src/data/siteAnnouncements.ts
@@ -9,8 +9,8 @@ export const SITE_ANNOUNCEMENTS: SiteAnnouncement[] = [
     id: 'royale-groups-and-charts',
     title: 'Royale Groups, Charts, and More!',
     body: "The biggest site update in months! Critic's Royale received a lot of attention this time, with new features for forming groups, seeing a player's past performance, and charts to view a quarter in more detail.",
-    postedAt: '2026-04-19',
-    sortDate: '2026-04-19',
+    postedAt: '2026-04-21',
+    sortDate: '2026-04-21',
     variant: 'secondary',
     href: 'https://www.youtube.com/watch?v=wOjiejePfRU',
     linkLabel: 'Watch the DevLog!'
