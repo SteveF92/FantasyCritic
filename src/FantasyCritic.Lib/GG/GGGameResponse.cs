c74290ebd3f4c5b9c857df085d3a33c560a81ec0
Stephen Fallon <sfallon92@gmail.com>
2026-06-10 21:13:20 -0400

    Fixing issues with GG data parsing. This was caused by the switch to system.text.json

--- src/FantasyCritic.Lib/GG/GGGameResponse.cs
+++ src/FantasyCritic.Lib/GG/GGGameResponse.cs
@@ -5,7 +5,7 @@ namespace FantasyCritic.Lib.GG;
 public class GGGameResponse
 {
     public string? CoverPath { get; set; }
-    public string? ID { get; set; }
+    public int? Id { get; set; }
     public string? Name { get; set; }
     public string? Slug { get; set; }
     public string? Token { get; set; }
