9b2b50fa4952eae02c95070f06779349c1851e6c
Stephen Fallon <sfallon92@gmail.com>
2026-06-27 21:59:28 -0400

    Minor change to wording in the undo last action modal.

--- src/FantasyCritic.Web/ClientApp/src/components/modals/undoLastDraftActionModal.vue
+++ src/FantasyCritic.Web/ClientApp/src/components/modals/undoLastDraftActionModal.vue
@@ -3,7 +3,7 @@
     <p>
       Do you wish to undo the last draft action?
       <br />
-      This will reverse the most recent action — either removing the last drafted game, or restoring a skipped turn.
+      This will remove the game that was added most recently from it's publisher. If the last publisher's turn was skipped, this will revert the draft to their turn.
       <br />
       Only do this if there was a mistake.
     </p>
